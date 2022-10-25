2ef0ee2a6069104f9a836df7e751181e118b4402
Egli Marc <egli@puzzle.ch>
2022-10-25 14:42:28 +0200

    finish team entity

---
+++ src/test/java/ch/puzzle/burningokr/models/entities/Team.java
@@ -0,0 +1,61 @@
+package ch.puzzle.burningokr.models.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "team")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 15)
+    @Column(name = "name")
+    private String name;
+
+    public Team() {}
+    private Team(Builder builder) {
+        setName(builder.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return Objects.equals(id, team.id) && Objects.equals(name, team.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public static final class Builder {
+        private @NotBlank @Size(min = 3, max = 15) String name;
+
+        public Builder() {
+        }
+
+        public Builder name(@NotBlank @Size(min = 3, max = 15) String val) {
+            name = val;
+            return this;
+        }
+
+        public Team build() {
+            return new Team(this);
+        }
+    }
+}

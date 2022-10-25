3ae75f61d78da6c08d1e718579a87205f86190e6
Egli Marc <egli@puzzle.ch>
2022-10-25 14:30:10 +0200

    basic files

---
+++ src/main/java/ch/puzzle/burningokr/BurningOkrApplication.java
@@ -0,0 +1,13 @@
+package ch.puzzle.burningokr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurningOkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurningOkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/puzzle/burningokr/BurningOkrApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.puzzle.burningokr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurningOkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

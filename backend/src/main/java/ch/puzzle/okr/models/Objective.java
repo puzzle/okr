c25411b862fa22d1a082dd1eaaf91861c090da8d
Jonas Schmid <schmid@puzzle.ch>
2022-10-27 08:19:12 +0200

    add OkrApplication and entities

---
+++ backend/src/main/java/ch/puzzle/okr/models/Objective.java
@@ -0,0 +1,126 @@
+package ch.puzzle.okr.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "objective", indexes = {
+        @Index(name = "idx_objective_title", columnList = "title")
+})
+public class Objective {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "sequence_objective")
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 250)
+    private String title;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "quarter_id")
+    private Quarter quarter;
+
+    @NotBlank@Size(min = 2, max = 1024 * 4)
+    private String description;
+
+    @NotNull
+    private Double progress;
+
+    @NotBlank
+    @Size(min = 2, max = 20)
+    private String createdBy;
+
+    @NotNull
+    private LocalDateTime createdOn;
+
+    public Objective() {
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Double getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Double progress) {
+        this.progress = progress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Quarter getQuarter() {
+        return quarter;
+    }
+
+    public void setQuarter(Quarter quarter) {
+        this.quarter = quarter;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}

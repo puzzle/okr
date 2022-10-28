27063ebabdd99c0938b30f2ddfc7b8014c8cd9ab
Lias <kleisa@puzzle.ch>
2022-10-28 12:56:59 +0200

    Add controller and repository for KeyResult

---
+++ backend/src/main/java/ch/puzzle/okr/repository/KeyResultRepository.java
@@ -0,0 +1,7 @@
+package ch.puzzle.okr.repository;
+
+import ch.puzzle.okr.models.KeyResult;
+import org.springframework.data.repository.CrudRepository;
+
+public interface KeyResultRepository extends CrudRepository<KeyResult, Long> {
+}

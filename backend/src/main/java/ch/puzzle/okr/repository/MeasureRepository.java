3a6458f030a034131e154972f1e9be7ac16f934b
Lias <kleisa@puzzle.ch>
2022-10-28 13:07:52 +0200

    Add controller and repository for Measure

---
+++ backend/src/main/java/ch/puzzle/okr/repository/MeasureRepository.java
@@ -0,0 +1,7 @@
+package ch.puzzle.okr.repository;
+
+import ch.puzzle.okr.models.Measure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeasureRepository extends CrudRepository<Measure, Long> {
+}

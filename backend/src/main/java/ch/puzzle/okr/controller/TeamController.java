698a20b05010e74a301b197c3b12012a811a7b9d
Jonas Schmid <schmid@puzzle.ch>
2022-10-31 07:34:28 +0100

    add a Team mapper

--- backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
@@ -1,7 +1,8 @@
 package ch.puzzle.okr.controller;
 
 import ch.puzzle.okr.common.BusinessException;
-import ch.puzzle.okr.models.Team;
+import ch.puzzle.okr.dto.TeamDto;
+import ch.puzzle.okr.mapper.TeamMapper;
 import ch.puzzle.okr.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,15 +21,21 @@ public class TeamController {
     @Autowired
     TeamService teamService;
 
+    @Autowired
+    TeamMapper teamMapper;
+
     @GetMapping
-    public List<Team> getAllTeams() {
-        return teamService.getAllTeams();
+    public List<TeamDto> getAllTeams() {
+        return teamService.getAllTeams().stream()
+                .map(teamMapper::toDto)
+                .toList();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getTeams(@PathVariable long id) {
         try {
-            return ResponseEntity.status(HttpStatus.OK).body(teamService.getTeamById(id));
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(teamMapper.toDto(teamService.getTeamById(id)));
         } catch (BusinessException e) {
             return ResponseEntity.status(e.getCode()).body(e.getClientMessage());
         }
---
+++ backend/src/main/java/ch/puzzle/okr/mapper/TeamMapper.java
@@ -0,0 +1,19 @@
+package ch.puzzle.okr.mapper;
+
+import ch.puzzle.okr.dto.TeamDto;
+import ch.puzzle.okr.models.Team;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TeamMapper {
+    public TeamDto toDto(Team team) {
+        return new TeamDto(team.getId(), team.getName());
+    }
+
+    public Team toTeam(TeamDto teamDto) {
+        return Team.Builder.builder()
+                .withId(teamDto.getId())
+                .withName(teamDto.getName())
+                .build();
+    }
+}

e8ef8888043177f869a655aa8f167c6fb91894c0
MarcEgli <marc.egli.2005@gmail.com>
2022-11-01 15:48:20 +0100

    Replace @beforeEach with constructor injection

--- backend/src/test/java/ch/puzzle/okr/mapper/ObjectiveMapperTest.java
+++ backend/src/test/java/ch/puzzle/okr/mapper/ObjectiveMapperTest.java
@@ -1,25 +1,24 @@
 package ch.puzzle.okr.mapper;
 
-import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.dto.objectives.GetObjectiveDTO;
-import org.junit.jupiter.api.BeforeEach;
+import ch.puzzle.okr.models.Objective;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 @ContextConfiguration
 @SpringBootConfiguration
 class ObjectiveMapperTest {
-    private ObjectiveMapper objectiveMapper;
+    private final ObjectiveMapper objectiveMapper;
 
-    @BeforeEach
-    void setUp() {
-        this.objectiveMapper = new ObjectiveMapper();
+    @Autowired
+    public ObjectiveMapperTest(ObjectiveMapper objectiveMapper) {
+        this.objectiveMapper = objectiveMapper;
     }
 
     @Test

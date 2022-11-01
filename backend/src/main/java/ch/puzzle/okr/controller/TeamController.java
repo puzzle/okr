502576bd75375460f6a375cb5d8e32ec5a0b9033
Jonas Schmid <schmid@puzzle.ch>
2022-11-01 16:10:57 +0100

    reorganize imports, remove @Autowire, update swaggerUI docs

--- backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
@@ -2,21 +2,18 @@ package ch.puzzle.okr.controller;
 
 import ch.puzzle.okr.dto.TeamDto;
 import ch.puzzle.okr.mapper.TeamMapper;
-import ch.puzzle.okr.service.TeamService;
 import ch.puzzle.okr.models.Team;
-import ch.puzzle.okr.repository.TeamRepository;
+import ch.puzzle.okr.service.TeamService;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/teams")
@@ -24,7 +21,6 @@ public class TeamController {
     private final TeamService teamService;
     private final TeamMapper teamMapper;
 
-    @Autowired
     public TeamController(TeamService teamService, TeamMapper teamMapper) {
         this.teamService = teamService;
         this.teamMapper = teamMapper;
@@ -34,8 +30,6 @@ public class TeamController {
             @ApiResponse(responseCode = "200", description = "Returned all teams",
                     content = {@Content(mediaType = "application/json",
                             schema = @Schema(implementation = Team.class))}),
-            @ApiResponse(responseCode = "404", description = "Did not find any teams",
-                    content = @Content)
     })
     @GetMapping
     public List<TeamDto> getAllTeams() {

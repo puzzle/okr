97fce224c050735340c0a9e94cd472b441b094db
Lias Kleisa <kleisa@puzzle.ch>
2022-11-02 08:10:02 +0100

    Implement rest interface for user

--- backend/src/main/java/ch/puzzle/okr/models/User.java
+++ backend/src/main/java/ch/puzzle/okr/models/User.java
@@ -33,6 +33,18 @@ public class User {
     public User() {
     }
 
+    private User(Builder builder) {
+        id = builder.id;
+        setUsername(builder.username);
+        setFirstname(builder.firstname);
+        setLastname(builder.lastname);
+        setEmail(builder.email);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -63,5 +75,50 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+
+    public static final class Builder {
+        private @NotNull Long id;
+        private @NotBlank @Size(min = 2, max = 20) String username;
+        private @NotBlank @Size(min = 2, max = 50) String firstname;
+        private @NotBlank @Size(min = 2, max = 50) String lastname;
+        private @Email @Size(min = 2, max = 250) String email;
+
+        public Builder() {
+        }
+
+        public static User.Builder builder() {
+            return new User.Builder();
+        }
+
+        public Builder withId(@NotNull Long val) {
+            id = val;
+            return this;
+        }
+
+        public Builder withUsername(@NotBlank @Size(min = 2, max = 20) String val) {
+            username = val;
+            return this;
+        }
+
+        public Builder withFirstname(@NotBlank @Size(min = 2, max = 50) String val) {
+            firstname = val;
+            return this;
+        }
+
+        public Builder withLastname(@NotBlank @Size(min = 2, max = 50) String val) {
+            lastname = val;
+            return this;
+        }
+
+        public Builder withEmail(@Email @Size(min = 2, max = 250) String val) {
+            email = val;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
---
+++ backend/src/main/java/ch/puzzle/okr/service/UserService.java
@@ -0,0 +1,28 @@
+package ch.puzzle.okr.service;
+
+import ch.puzzle.okr.models.User;
+import ch.puzzle.okr.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User with id %d not found", id))
+        );
+    }
+}

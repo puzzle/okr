9d66c018c7f22cdd5e36929e28d9f112bc0a7e69
Lias Kleisa <kleisa@puzzle.ch>
2022-11-02 09:20:49 +0100

    Add unit test for usercontroller

--- backend/src/main/java/ch/puzzle/okr/controller/UserController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/UserController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,18 +35,6 @@ public class UserController {
         return userService.getAllUsers().stream()
                 .map(userMapper::toDto)
                 .toList();
-    }
-
-
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Returned a user with a specified ID.",
-                    content = {@Content(mediaType = "application/json",
-                            schema = @Schema(implementation = User.class))}),
-            @ApiResponse(responseCode = "404", description = "Did not find a user with a specified ID.", content = @Content)
-    })
-    @GetMapping("/{id}")
-    public UserDto getUserById(@PathVariable long id) {
-        return userMapper.toDto(userService.getUserById(id));
     }
 
 }
--- backend/src/main/java/ch/puzzle/okr/service/UserService.java
+++ backend/src/main/java/ch/puzzle/okr/service/UserService.java
@@ -2,9 +2,7 @@ package ch.puzzle.okr.service;
 
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,9 +18,5 @@ public class UserService {
     public List<User> getAllUsers() {
         return (List<User>) userRepository.findAll();
     }
-    public User getUserById(long id) {
-        return userRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User with id %d not found", id))
-        );
-    }
+
 }

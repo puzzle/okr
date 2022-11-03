2c3ae737976bc9ccc21ed9b10e7eab284083ccd4
peggimann <peggimann@puzzle.com>
2022-11-03 16:18:01 +0100

    implement create Meassure

---
+++ backend/src/main/java/ch/puzzle/okr/dto/MeasureDto.java
@@ -0,0 +1,81 @@
+package ch.puzzle.okr.dto;
+
+import java.time.LocalDateTime;
+
+public class MeasureDto {
+    private java.lang.Long id;
+    private Long keyResultId;
+    private Integer value;
+    private String changeInfo;
+    private String initiatives;
+
+    private Long createdById;
+
+    private LocalDateTime createdOn;
+
+    public MeasureDto(Long id, Long keyResultId, Integer value, String changeInfo, String initiatives, Long createdById, LocalDateTime createdOn) {
+        this.id = id;
+        this.keyResultId = keyResultId;
+        this.value = value;
+        this.changeInfo = changeInfo;
+        this.initiatives = initiatives;
+        this.createdById = createdById;
+        this.createdOn = createdOn;
+    }
+
+    public java.lang.Long getId() {
+        return id;
+    }
+
+    public void setId(java.lang.Long id) {
+        this.id = id;
+    }
+
+    public Long getKeyResultId() {
+        return keyResultId;
+    }
+
+    public void setKeyResultId(Long keyResultId) {
+        this.keyResultId = keyResultId;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getChangeInfo() {
+        return changeInfo;
+    }
+
+    public void setChangeInfo(String changeInfo) {
+        this.changeInfo = changeInfo;
+    }
+
+    public String getInitiatives() {
+        return initiatives;
+    }
+
+    public void setInitiatives(String initiatives) {
+        this.initiatives = initiatives;
+    }
+
+    public Long getCreatedById() {
+        return createdById;
+    }
+
+    public void setCreatedById(Long createdById) {
+        this.createdById = createdById;
+    }
+
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(LocalDateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+}

0f03c2182e75fafbc91108113cea5053b9cb9252
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-03 16:06:37 +0100

    rework ObjectiveService

--- backend/src/main/java/ch/puzzle/okr/service/ObjectiveService.java
+++ backend/src/main/java/ch/puzzle/okr/service/ObjectiveService.java
@@ -1,28 +1,22 @@
 package ch.puzzle.okr.service;
 
-import ch.puzzle.okr.mapper.ObjectiveMapper;
-import ch.puzzle.okr.dto.objectives.GetObjectiveDTO;
+import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.repository.ObjectiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Service
 public class ObjectiveService {
     private final ObjectiveRepository objectiveRepository;
-    private final ObjectiveMapper objectiveMapper;
 
     @Autowired
-    public ObjectiveService(ObjectiveRepository objectiveRepository, ObjectiveMapper objectiveMapper) {
+    public ObjectiveService(ObjectiveRepository objectiveRepository) {
         this.objectiveRepository = objectiveRepository;
-        this.objectiveMapper = objectiveMapper;
     }
 
-    public List<GetObjectiveDTO> getAllObjectives() {
-        return StreamSupport.stream(this.objectiveRepository.findAll().spliterator(), false)
-                        .map(this.objectiveMapper::entityToGetObjectiveDto).toList();
+    public List<Objective> getAllObjectives() {
+        return (List<Objective>) objectiveRepository.findAll();
     }
 }

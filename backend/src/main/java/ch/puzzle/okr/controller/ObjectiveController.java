1853f0503bd3a08bce871ea08c9ebf36f4861262
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-03 16:05:19 +0100

    rework ObjectiveController

--- backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
@@ -1,9 +1,13 @@
 package ch.puzzle.okr.controller;
 
-import ch.puzzle.okr.dto.objectives.GetObjectiveDTO;
+import ch.puzzle.okr.dto.ObjectiveDTO;
+import ch.puzzle.okr.mapper.ObjectiveMapper;
+import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.service.ObjectiveService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +16,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/objectives")
-@ApiOperation("Objectives API")
 public class ObjectiveController {
     private final ObjectiveService objectiveService;
 
-    @Autowired
-    public ObjectiveController(ObjectiveService objectiveService) {
+    private final ObjectiveMapper objectiveMapper;
+
+    public ObjectiveController(ObjectiveService objectiveService, ObjectiveMapper objectiveMapper) {
         this.objectiveService = objectiveService;
+        this.objectiveMapper = objectiveMapper;
     }
 
-    @ApiOperation(value = "Get all objectives", notes = "Returns List of ObjectiveDTOs")
-    @APIResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returned all Objectives",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Objective.class))}),
     })
     @GetMapping
-    public ResponseEntity<List<GetObjectiveDTO>> getObjectiveDtoList() {
-        return ResponseEntity.ok(this.objectiveService.getAllObjectives());
+    public List<ObjectiveDTO> getAllObjectives() {
+        return objectiveService.getAllObjectives().stream()
+                .map(objectiveMapper::toDto)
+                .toList();
     }
 }

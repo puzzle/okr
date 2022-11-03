c7736d343bb2a4682bc37bb92b83d3da952bdb1d
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-03 16:05:56 +0100

    rework ObjectiveMapper

--- backend/src/main/java/ch/puzzle/okr/mapper/ObjectiveMapper.java
+++ backend/src/main/java/ch/puzzle/okr/mapper/ObjectiveMapper.java
@@ -1,18 +1,18 @@
 package ch.puzzle.okr.mapper;
 
 import ch.puzzle.okr.models.Objective;
-import ch.puzzle.okr.dto.objectives.GetObjectiveDTO;
+import ch.puzzle.okr.dto.ObjectiveDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ObjectiveMapper {
 
-    public GetObjectiveDTO entityToGetObjectiveDto(Objective objective) {
-        return new GetObjectiveDTO(
-                1, objective.getTeam().getName(),
-                objective.getTitle(), objective.getDescription(),
-                1, objective.getOwner().getUsername(),
-                objective.getQuarter().toString(),
-                objective.getCreatedOn());
+    public ObjectiveDTO toDto(Objective objective) {
+        return new ObjectiveDTO(
+                objective.getId(), objective.getTitle(), objective.getOwner().getId(),
+                objective.getOwner().getFirstname(), objective.getOwner().getLastname(),
+                objective.getTeam().getId(), objective.getTeam().getName(),
+                objective.getQuarter().getNumber(), objective.getQuarter().getYear(),
+                objective.getDescription(), objective.getProgress());
     }
 }

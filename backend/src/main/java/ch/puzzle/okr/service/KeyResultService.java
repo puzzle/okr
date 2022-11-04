8473cbbc2c5773b6003da1d3bea5cc33910cf6b1
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-04 10:52:00 +0100

    implemenz getAllKeyResultsByObjective method in keyresult service

--- backend/src/main/java/ch/puzzle/okr/service/KeyResultService.java
+++ backend/src/main/java/ch/puzzle/okr/service/KeyResultService.java
@@ -1,4 +1,29 @@
 package ch.puzzle.okr.service;
 
+import ch.puzzle.okr.models.KeyResult;
+import ch.puzzle.okr.models.Objective;
+import ch.puzzle.okr.repository.KeyResultRepository;
+import ch.puzzle.okr.repository.ObjectiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
 public class KeyResultService {
+    KeyResultRepository keyResultRepository;
+    ObjectiveRepository objectiveRepository;
+
+    public KeyResultService(KeyResultRepository keyResultRepository, ObjectiveRepository objectiveRepository) {
+        this.keyResultRepository = keyResultRepository;
+        this.objectiveRepository = objectiveRepository;
+    }
+
+    public List<KeyResult> getAllKeyResultsByObjective(long objectiveId) {
+        Objective objective = objectiveRepository.findById(objectiveId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Objective with id %d not found", objectiveId))
+        );
+        return keyResultRepository.findByObjective(objective);
+    }
 }

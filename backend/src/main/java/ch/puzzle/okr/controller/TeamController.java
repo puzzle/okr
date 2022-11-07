a5c83c9be125a3f87747e9e78383f9800d22da01
megli2 <egli@puzzle.ch>
2022-11-07 09:43:28 +0100

    finish controller tests for put mapping in TeamControllerIT.java

--- backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/TeamController.java
@@ -8,10 +8,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -50,9 +49,10 @@ public class TeamController {
     }
 
     @PutMapping("/{id}")
-    public TeamDto updateTeam(@PathVariable long id, @RequestBody TeamDto teamDto) {
-         return teamMapper.toDto(teamService.updateTeam(id, teamDto));
+    public ResponseEntity<Team> updateTeam(@PathVariable long id, @RequestBody TeamDto teamDto) {
+         return ResponseEntity.status(HttpStatus.OK).body(teamService.updateTeam(id, teamDto));
     }
+
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Saved new team to db",
                     content = {@Content(mediaType = "application/json",

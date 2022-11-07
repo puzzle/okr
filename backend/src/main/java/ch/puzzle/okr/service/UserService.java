bd6aa7c03749c9bfcfe21efe3d4262807b73037f
Lias Kleisa <kleisa@puzzle.ch>
2022-11-07 11:54:37 +0100

    Add getOwnerById method in UserService

--- backend/src/main/java/ch/puzzle/okr/service/UserService.java
+++ backend/src/main/java/ch/puzzle/okr/service/UserService.java
@@ -2,7 +2,9 @@ package ch.puzzle.okr.service;
 
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -17,6 +19,17 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+
+    public User getOwnerById(Long ownerId) {
+        if (ownerId == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing attribute owner id");
+        }
+
+        return userRepository.findById(ownerId).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, (String.format("Owner with id %d not found", ownerId)))
+        );
     }
 
 }

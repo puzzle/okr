be3b0f9b714e1014e8a7be43d7f29d06b6f2f56a
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-07 17:28:04 +0100

    remove unused method in user service

--- backend/src/main/java/ch/puzzle/okr/service/UserService.java
+++ backend/src/main/java/ch/puzzle/okr/service/UserService.java
@@ -2,9 +2,7 @@ package ch.puzzle.okr.service;
 
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.repository.UserRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,11 +17,5 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return (List<User>) userRepository.findAll();
-    }
-
-    public User getUserById(Long id){
-        return this.userRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Team with id %d not found", id))
-        );
     }
 }

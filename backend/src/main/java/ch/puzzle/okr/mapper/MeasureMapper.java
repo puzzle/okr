8cc03d3e361bf14986ffed13d0f6e7e611a7e86e
peggimann <peggimann@puzzle.com>
2022-11-07 09:54:48 +0100

    move logic from MeasureMapper to Measure Service

--- backend/src/main/java/ch/puzzle/okr/mapper/MeasureMapper.java
+++ backend/src/main/java/ch/puzzle/okr/mapper/MeasureMapper.java
@@ -1,41 +1,29 @@
 package ch.puzzle.okr.mapper;
 
 import ch.puzzle.okr.dto.MeasureDto;
-import ch.puzzle.okr.models.KeyResult;
 import ch.puzzle.okr.models.Measure;
-import ch.puzzle.okr.models.User;
-import ch.puzzle.okr.repository.KeyResultRepository;
-import ch.puzzle.okr.repository.UserRepository;
-import org.springframework.http.HttpStatus;
+import ch.puzzle.okr.repository.MeasureRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
+import ch.puzzle.okr.service.MeasureService;
 
 @Component
 public class MeasureMapper {
-    KeyResultRepository keyResultRepository;
-    UserRepository userRepository;
 
+    MeasureRepository measureRepository;
     public MeasureDto toDto(Measure measure) {
         return new MeasureDto(measure.getId(), measure.getKeyResult().getId(), measure.getValue(), measure.getChangeInfo(),
                 measure.getInitiatives(), measure.getCreatedBy().getId(), measure.getCreatedOn());
     }
 
     public Measure toMeasure(MeasureDto measureDto) {
-        Long keyResultId = measureDto.getKeyResultId();
-        Long userId = measureDto.getCreatedById();
-        KeyResult keyresult = keyResultRepository.findById(keyResultId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Keyresult with id %d not found", keyResultId))
-        );
-        User user = userRepository.findById(userId).orElseThrow(
-                ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User with id %d not found", userId))
-        );
+        MeasureService measureService = new MeasureService(measureRepository);
         return Measure.Builder.builder()
                 .withId(measureDto.getId())
-                .withKeyResult(keyresult)
+                .withKeyResult(measureService.mapKeyResult(measureDto))
                 .withValue(measureDto.getValue())
                 .withChangeInfo(measureDto.getChangeInfo())
                 .withInitiatives(measureDto.getInitiatives())
-                .withCreatedBy(user)
+                .withCreatedBy(measureService.mapUser(measureDto))
                 .withCreatedOn(measureDto.getCreatedOn())
                 .build();
     }

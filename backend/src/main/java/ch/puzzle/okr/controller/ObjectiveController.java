8486f6e6c2afd19f6eaf4b8759fc860cc2cbbc5a
Lias Kleisa <kleisa@puzzle.ch>
2022-11-07 11:46:15 +0100

    Rename objectiveDto

--- backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
@@ -8,14 +8,15 @@ import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.service.ObjectiveService;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

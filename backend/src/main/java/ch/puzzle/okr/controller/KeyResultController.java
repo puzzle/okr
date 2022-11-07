43ff889cb09b54dbd23a62d6ae395e80f4d5fda0
Niklas Jäggi <jaeggi@puzzle.ch>
2022-11-07 15:42:10 +0100

    add put method to controller

--- backend/src/main/java/ch/puzzle/okr/controller/KeyResultController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/KeyResultController.java
@@ -1,6 +1,7 @@
 package ch.puzzle.okr.controller;
 
 import ch.puzzle.okr.dto.KeyResultDto;
+import ch.puzzle.okr.mapper.KeyResultMapper;
 import ch.puzzle.okr.models.KeyResult;
 import ch.puzzle.okr.repository.KeyResultRepository;
 import ch.puzzle.okr.service.KeyResultService;
@@ -17,9 +18,12 @@ public class KeyResultController {
 
     KeyResultService keyResultService;
 
-    public KeyResultController(KeyResultRepository keyResultRepository, KeyResultService keyResultService) {
+    KeyResultMapper keyResultMapper;
+
+    public KeyResultController(KeyResultRepository keyResultRepository, KeyResultService keyResultService, KeyResultMapper keyResultMapper) {
         this.keyResultRepository = keyResultRepository;
         this.keyResultService = keyResultService;
+        this.keyResultMapper = keyResultMapper;
     }
 
     @GetMapping
@@ -30,5 +34,11 @@ public class KeyResultController {
     @PostMapping
     public KeyResult createKeyResult(@RequestBody KeyResultDto keyResultDto) {
         return this.keyResultService.createKeyResult(keyResultDto);
+    }
+
+    @PutMapping("/{id}")
+    public KeyResult updateKeyResult(@PathVariable long id, @RequestBody KeyResultDto keyResultDto) {
+        keyResultDto.setId(id);
+        return this.keyResultService.updateKeyResult(keyResultMapper.toKeyResult(keyResultDto));
     }
 }

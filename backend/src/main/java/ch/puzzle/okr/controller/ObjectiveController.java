334599c03d1bd4492475d25d2f3b3d590f42df78
megli2 <egli@puzzle.ch>
2022-11-08 16:05:06 +0100

    optimize imports

--- backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/ObjectiveController.java
@@ -13,12 +13,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;

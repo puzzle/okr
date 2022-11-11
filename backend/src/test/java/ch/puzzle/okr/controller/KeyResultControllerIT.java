d3a61cebf85496fe7c835dd369b1baaeb7162589
Lias Kleisa <kleisa@puzzle.ch>
2022-11-11 09:24:04 +0100

    Fix not running test class for KeyResultControllerIT

--- backend/src/test/java/ch/puzzle/okr/controller/KeyResultControllerIT.java
+++ backend/src/test/java/ch/puzzle/okr/controller/KeyResultControllerIT.java
@@ -13,8 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,8 +27,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(KeyResultController.class)
 class KeyResultControllerIT {
     @MockBean
     KeyResultRepository keyResultRepository;

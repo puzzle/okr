59718a84be15ebacdc248c729ab6bd8b0c181275
megli2 <egli@puzzle.ch>
2022-11-21 07:17:55 +0100

    create dtos for team-detail component

--- frontend/src/app/team-detail/team-detail.component.ts
+++ frontend/src/app/team-detail/team-detail.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Objectives {
+  id: number;
+  title: String;
+  ownerId: number;
+  ownerFirstname: String;
+  ownerLastname: String;
+  teamId: number;
+  teamName: String;
+  quarterId: number;
+  quarterNumber: number;
+  quarterYear: number;
+  description: String;
+  progress: number;
+}
+
+export interface TeamObjectives {
+  id: number;
+  name: string;
+  objectives: Objectives[];
+}
+
 @Component({
   selector: 'app-team-detail',
   templateUrl: './team-detail.component.html',

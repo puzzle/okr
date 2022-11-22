003ca072bedcef40edcab90fdb50fc8aed06d705
megli2 <egli@puzzle.ch>
2022-11-22 15:22:03 +0100

    use query to calculate progress and rewrite tests

--- backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
+++ backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
@@ -2,10 +2,20 @@ package ch.puzzle.okr.repository;
 
 import java.util.List;
 import ch.puzzle.okr.models.Objective;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ObjectiveRepository extends CrudRepository<Objective, Long> {
-    List<Objective> findByTeamId(long id);
+
+    @Query(
+            value = "select AVG((CAST(coalesce(measure_value, 0) as decimal)/CAST(target_value as decimal))*100) from\n" +
+                    "(select max(created_on), max(value) as measure_value, max(target_value) as target_value\n" +
+                    "from (select key_result.id, target_value, value, m.created_on, objective_id\n" +
+                    "      from key_result LEFT JOIN measure m on key_result.id = m.key_result_id) as sub where objective_id = :objective_id group by id) as sub2",
+            nativeQuery = true
+    )
+    Double getProgressOfObjective(@Param("objective_id") Long objectiveId);
 }

989849800849690cbe592454f9a20685250b5846
megli2 <egli@puzzle.ch>
2022-11-28 09:45:29 +0100

    calculate progress in one method and fix tests because of this change

--- backend/src/test/java/ch/puzzle/okr/service/ProgressServiceTestIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/ProgressServiceTestIT.java
@@ -1,8 +1,9 @@
 package ch.puzzle.okr.service;
 
 import ch.puzzle.okr.OkrApplication;
-import ch.puzzle.okr.models.Objective;
-import org.junit.jupiter.api.Disabled;
+import ch.puzzle.okr.models.*;
+import ch.puzzle.okr.repository.KeyResultRepository;
+import ch.puzzle.okr.repository.MeasureRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,28 +21,60 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ProgressServiceTestIT {
     @Autowired
     private ProgressService progressService;
+    @Autowired
+    private KeyResultRepository keyResultRepository;
+    @Autowired
+    private ObjectiveService objectiveService;
+    @Autowired
+    private MeasureRepository measureRepository;
 
     @Test
-    @Disabled
     void checkUpdateProgressMethod() {
         Objective objective = Objective.Builder.builder().withId(1L).build();
-        Double percentValue = this.progressService.getObjectiveProgressInPercent(objective);
-        assertEquals(55D, percentValue);
+        Quarter quarter = Quarter.Builder.builder().withId(1L).build();
+        User user = User.Builder.builder().withId(1L).build();
+        KeyResult keyResult = KeyResult.Builder.builder().withObjective(objective)
+                .withTargetValue(100L).withTitle("HelloWorld")
+                .withDescription("Very Important").withBasisValue(50L)
+                .withQuarter(quarter).withUnit(Unit.PERCENT)
+                .withExpectedEvolution(ExpectedEvolution.DECREASE)
+                .withOwner(user).withCreatedBy(user)
+                .withCreatedOn(LocalDateTime.now()).build();
+
+        this.keyResultRepository.save(keyResult);
+        this.progressService.updateObjectiveProgress(1L);
+
+        Objective updatedObjective = this.objectiveService.getObjective(1L);
+        assertEquals(44D, updatedObjective.getProgress());
     }
 
     @Test
-    @Disabled
     void checkProgressMethodWithNoMeasures() {
-        Objective objective = Objective.Builder.builder().withId(2L).build();
-        Double percentValue = this.progressService.getObjectiveProgressInPercent(objective);
-        assertEquals(0D, percentValue);
+        Objective objective = Objective.Builder.builder().withId(1L).build();
+        KeyResult keyResult = KeyResult.Builder.builder().withId(1L).withObjective(objective).build();
+        User user = User.Builder.builder().withId(1L).build();
+        Measure measure = Measure.Builder.builder().withValue(90)
+                .withKeyResult(keyResult).withCreatedBy(user).withCreatedOn(LocalDateTime.now())
+                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives").build();
+
+        this.measureRepository.save(measure);
+        this.progressService.updateObjectiveProgress(1L);
+        Objective updatedObjective = this.objectiveService.getObjective(1L);
+        assertEquals(62.5D, updatedObjective.getProgress());
     }
 
     @Test
-    @Disabled
     void checkProgressMethodWithNoKeyResults() {
-        Objective objective = Objective.Builder.builder().withId(3L).build();
-        Double percentValue = this.progressService.getObjectiveProgressInPercent(objective);
-        assertEquals(0D, percentValue);
+        Objective objective = Objective.Builder.builder().withId(2L).build();
+        KeyResult keyResult = KeyResult.Builder.builder().withId(5L).withObjective(objective).build();
+        User user = User.Builder.builder().withId(1L).build();
+        Measure measure = Measure.Builder.builder().withValue(90)
+                .withKeyResult(keyResult).withCreatedBy(user).withCreatedOn(LocalDateTime.now())
+                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives").build();
+
+        this.measureRepository.save(measure);
+        this.progressService.updateObjectiveProgress(2L);
+        Objective updatedObjective = this.objectiveService.getObjective(2L);
+        assertEquals(45D, updatedObjective.getProgress());
     }
 }

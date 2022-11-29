85691d95f26b6031e59aa03fb3b92ae98518db94
megli2 <egli@puzzle.ch>
2022-11-29 09:41:23 +0100

    optimize query with option to have progress calculation of objective without keyresults

--- backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
+++ backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface ObjectiveRepository extends CrudRepository<Objective, Long> {
 
-    @Query(value = "select coalesce(AVG((CAST(coalesce(value, 0) as decimal) / CAST(kr2.target_value as decimal))*100), 0) from key_result as kr2 LEFT JOIN"
+    @Query(value = "select coalesce(AVG((CAST(coalesce(value, 0) as decimal) / CAST(coalesce(kr2.target_value, 0) as decimal))*100), 0) from key_result as kr2 LEFT JOIN"
             + "(select m.id, m.created_on, m.value, key_result_id from measure as m, (select max(m.created_on) as created_on, kr.id"
             + "      from key_result as kr LEFT JOIN measure m on kr.id = m.key_result_id"
             + "      group by kr.id) as sub where m.key_result_id = sub.id and m.created_on = sub.created_on) as sub3 on sub3.key_result_id = kr2.id where kr2.objective_id = :objective_id", nativeQuery = true)

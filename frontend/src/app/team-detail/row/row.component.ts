9fd3acccfd357fb750d5443ae30aab03dfef929f
Yanick Minder <minder@puzzle.ch>
2022-11-29 12:55:28 +0100

    feature/305-key-result-row
    YM (T-305) 29.11.2022 12:55
    - Bar color changes based on value

--- frontend/src/app/team-detail/row/row.component.ts
+++ frontend/src/app/team-detail/row/row.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Menu } from '@angular/cdk/menu';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { MenuEntry } from 'src/app/models/MenuEntry';
-import { Objective } from '../../models/Objective';
 import { RowObject } from 'src/app/models/RowObject';
 
 @Component({
   selector: 'app-row',
   templateUrl: './row.component.html',
   styleUrls: ['./row.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class RowComponent implements OnInit {
   @Input() element!: RowObject;
   @Input() menuEntries!: MenuEntry[];
+
   constructor() {}
 
   ngOnInit(): void {}

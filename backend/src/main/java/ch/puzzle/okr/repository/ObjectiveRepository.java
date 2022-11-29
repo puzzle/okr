68ebbc2e1aea7cc00358939a16fe238bce1b77d4
megli2 <egli@puzzle.ch>
2022-11-29 17:08:13 +0100

    format ObjectiveRepository

--- backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
+++ backend/src/main/java/ch/puzzle/okr/repository/ObjectiveRepository.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Repository
 public interface ObjectiveRepository extends CrudRepository<Objective, Long> {
 
-    @Query(value = "select coalesce(AVG((CAST(m.value as decimal) / CAST(k.target_value as decimal))*100), 0)" +
-            "from key_result as k, measure as m, (select max(created_on) as created_on, key_result_id from measure group by key_result_id) as t where t.key_result_id = m.key_result_id and t.created_on = m.created_on and k.id = m.key_result_id and k.objective_id = :objective_id", nativeQuery = true)
+    @Query(value = "select coalesce(AVG((CAST(m.value as decimal) / CAST(k.target_value as decimal))*100), 0)"
+            + "from key_result as k, measure as m, (select max(created_on) as created_on, key_result_id from measure group by key_result_id) as t where t.key_result_id = m.key_result_id and t.created_on = m.created_on and k.id = m.key_result_id and k.objective_id = :objective_id", nativeQuery = true)
     Double getProgressOfObjective(@Param("objective_id") Long objectiveId);
 
     List<Objective> findByTeamId(long id);

f4f4926867615f32cc6dc24a3b37251d076fce55
Lias Kleisa <kleisa@puzzle.ch>
2022-11-30 10:35:44 +0100

    Add tests for team list component

--- frontend/src/app/team-module/team-list/team-list.component.spec.ts
+++ frontend/src/app/team-module/team-list/team-list.component.spec.ts
@@ -1,13 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TeamListComponent } from './team-list.component';
+import {of} from "rxjs";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {TeamService} from "../team.service";
+import {By} from "@angular/platform-browser";
 
 describe('TeamListComponent', () => {
   let component: TeamListComponent;
   let fixture: ComponentFixture<TeamListComponent>;
 
+  const teamServiceMock = {
+    getTeams: jest.fn(),
+  };
+
   beforeEach(async () => {
+    teamServiceMock.getTeams.mockReturnValue(
+      of([
+        { id: 1, name: 'Team1' },
+        { id: 2, name: 'Team2' },
+      ])
+    );
+
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: TeamService, useValue: teamServiceMock }],
       declarations: [TeamListComponent],
     }).compileComponents();
 
@@ -19,4 +36,48 @@ describe('TeamListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display a headline with right text', () => {
+    expect(fixture.nativeElement.querySelector('.headline-large').textContent).toEqual(
+      'Teams'
+    );
+  });
+
+  it('should display two teams', () => {
+    const teamRows = fixture.debugElement.queryAll(
+      By.css('.team-row')
+    );
+    expect(teamRows.length).toEqual(2);
+  });
+
+  it('should have right name on teams', () => {
+    const teamRows = fixture.debugElement.queryAll(
+      By.css('.team-row')
+    );
+    expect(teamRows[0].nativeElement.querySelector('p').textContent).toEqual("Team1");
+    expect(teamRows[1].nativeElement.querySelector('p').textContent).toEqual("Team2");
+  });
+
+  it('should have two edit buttons', () => {
+    const editButtons = fixture.debugElement.queryAll(
+      By.css('.edit-icon')
+    );
+    expect(editButtons.length).toEqual(2);
+  });
+
+  it('should have two delete buttons', () => {
+    const editButtons = fixture.debugElement.queryAll(
+      By.css('.delete-icon')
+    );
+    expect(editButtons.length).toEqual(2);
+  });
+
+  it('should have two lines under the teams', () => {
+    const dividers = fixture.debugElement.queryAll(
+      By.css('.divider')
+    );
+    expect(dividers.length).toEqual(2);
+  });
+
+
 });

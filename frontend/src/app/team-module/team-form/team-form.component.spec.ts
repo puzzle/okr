4663573db1660abddd12821b878f0171a2e95b17
Lias Kleisa <kleisa@puzzle.ch>
2022-12-05 13:12:47 +0100

    Write tests for team-form.component

--- frontend/src/app/team-module/team-form/team-form.component.spec.ts
+++ frontend/src/app/team-module/team-form/team-form.component.spec.ts
@@ -1,22 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TeamFormComponent } from './team-form.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
-describe('TeamFormComponent', () => {
-  let component: TeamFormComponent;
-  let fixture: ComponentFixture<TeamFormComponent>;
+let component: TeamFormComponent;
+let fixture: ComponentFixture<TeamFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TeamFormComponent],
-    }).compileComponents();
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+    declarations: [TeamFormComponent],
+  }).compileComponents();
 
-    fixture = TestBed.createComponent(TeamFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  fixture = TestBed.createComponent(TeamFormComponent);
+  component = fixture.componentInstance;
+  fixture.detectChanges();
+});
+
+describe('TeamFormComponent Creating', () => {
+  test('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
+  test('should isCreating set on true default', () => {
+    expect(component.isCreating).toEqual(true);
+  });
+
+  test('should have 1 input filed with right placeholder', () => {
+    const inputFileds = fixture.debugElement.queryAll(By.css('input'));
+    expect(inputFileds.length).toEqual(1);
+
+    expect(inputFileds[0].nativeElement.placeholder).toEqual('Team hinzufügen');
+  });
+});
+
+describe('TeamFormComponent Editing', () => {
+  beforeEach(async () => {
+    component.isCreating = false;
+  });
+
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should isCreating set on false', () => {
+    expect(component.isCreating).toEqual(false);
+  });
+
+  test('should have 1 input filed with right placeholder', () => {
+    const inputFileds = fixture.debugElement.queryAll(By.css('input'));
+    expect(inputFileds.length).toEqual(1);
+
+    expect(inputFileds[0].nativeElement.placeholder).toEqual('Team bearbeiten');
   });
 });

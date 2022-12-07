3e92ca260c6c47036549e6d9ad1b424c255ed77a
Lias Kleisa <kleisa@puzzle.ch>
2022-12-07 15:20:08 +0100

    Rename team-model to team

--- frontend/src/app/team/team.module.ts
+++ frontend/src/app/team/team.module.ts
@@ -6,9 +6,11 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { ObjectiveModule } from '../objective/objective.module';
 import { MatDividerModule } from '@angular/material/divider';
 import { TeamListComponent } from './team-list/team-list.component';
+import { TeamFormComponent } from './team-form/team-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [TeamDetailComponent, TeamListComponent],
+  declarations: [TeamDetailComponent, TeamListComponent, TeamFormComponent],
   providers: [TeamService],
   exports: [TeamDetailComponent, TeamListComponent],
   imports: [
@@ -16,6 +18,7 @@ import { TeamListComponent } from './team-list/team-list.component';
     MatExpansionModule,
     ObjectiveModule,
     MatDividerModule,
+    FormsModule,
   ],
 })
 export class TeamModule {}

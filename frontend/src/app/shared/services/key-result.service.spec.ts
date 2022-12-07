c0474d322d79172355b8b5709d7e7be16240a8d3
Lias Kleisa <kleisa@puzzle.ch>
2022-12-07 10:43:00 +0100

    Fix key-result.service.spec

--- frontend/src/app/shared/services/key-result.service.spec.ts
+++ frontend/src/app/shared/services/key-result.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { KeyResultService } from './key-result.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('KeyResultService', () => {
   let service: KeyResultService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(KeyResultService);
   });
 

66b5f7f3fc0531a42bda25c667a1bd1b8bd7d831
Lias Kleisa <kleisa@puzzle.ch>
2022-12-07 10:02:31 +0100

    Finish specs for team-form.component

--- frontend/src/app/team-module/team-form/team-form.component.spec.ts
+++ frontend/src/app/team-module/team-form/team-form.component.spec.ts
@@ -5,84 +5,116 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
+import { Team } from '../team.service';
+import { ActivatedRoute } from '@angular/router';
 
 let component: TeamFormComponent;
 let fixture: ComponentFixture<TeamFormComponent>;
+let team: Team = {
+  id: 1,
+  name: 'Team1',
+};
 
-beforeEach(async () => {
-  await TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
-    declarations: [TeamFormComponent],
-  }).compileComponents();
+describe('TeamFormComponent', () => {
+  describe('Create new Team', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+        declarations: [TeamFormComponent],
+      }).compileComponents();
 
-  fixture = TestBed.createComponent(TeamFormComponent);
-  component = fixture.componentInstance;
-  fixture.detectChanges();
-});
+      fixture = TestBed.createComponent(TeamFormComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-describe('TeamFormComponent Creating', () => {
-  test('should create', () => {
-    expect(component).toBeTruthy();
+    test('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    test('should isCreating set on true default', () => {
+      expect(component.isCreating).toEqual(true);
+    });
+
+    test('should have right title', () => {
+      const heading = fixture.nativeElement.querySelector('p').textContent;
+      expect(heading).toEqual('Team erstellen');
+    });
+
+    test('should have 1 input filed with right placeholder', () => {
+      const inputFileds = fixture.debugElement.queryAll(By.css('input'));
+      expect(inputFileds.length).toEqual(1);
+
+      expect(inputFileds[0].nativeElement.placeholder).toEqual(
+        'Team hinzufügen'
+      );
+      expect(inputFileds[0].nativeElement.value).toEqual('');
+    });
+
+    test('should have 2 buttons', () => {
+      const buttons = fixture.debugElement.queryAll(By.css('button'));
+      expect(buttons.length).toEqual(2);
+
+      expect(buttons[0].nativeElement.textContent).toEqual(' Abbrechen ');
+      expect(buttons[1].nativeElement.textContent).toEqual(' Speichern ');
+    });
   });
 
-  test('should isCreating set on true default', () => {
-    expect(component.isCreating).toEqual(true);
-  });
+  describe('Edit existing team', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+        declarations: [TeamFormComponent],
+        providers: [
+          {
+            provide: ActivatedRoute,
+            useValue: { snapshot: { params: { id: 1 } } },
+          },
+        ],
+      }).compileComponents();
 
-  test('should have right title', () => {
-    const heading = fixture.nativeElement.querySelector('p').textContent;
-    expect(heading).toEqual('Team erstellen');
-  });
+      fixture = TestBed.createComponent(TeamFormComponent);
+      component = fixture.componentInstance;
 
-  test('should have 1 input filed with right placeholder', () => {
-    const inputFileds = fixture.debugElement.queryAll(By.css('input'));
-    expect(inputFileds.length).toEqual(1);
+      window.history.pushState(team, '');
+      component.teamObject = team;
+      component.teamname = team.name;
 
-    expect(inputFileds[0].nativeElement.placeholder).toEqual('Team hinzufügen');
-  });
+      fixture.detectChanges();
+    });
 
-  test('should have 1 buttons', () => {
-    const buttons = fixture.debugElement.queryAll(By.css('button'));
-    expect(buttons.length).toEqual(2);
+    test('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-    expect(buttons[0].nativeElement.textContent).toEqual(' Abbrechen ');
-    expect(buttons[1].nativeElement.textContent).toEqual(' Speichern ');
-  });
-});
+    test('should isCreating set on false', () => {
+      console.log(component.isCreating);
+      expect(component.isCreating).toEqual(false);
+    });
 
-describe('TeamFormComponent Editing', () => {
-  beforeEach(async () => {
-    component.isCreating = false;
-    fixture.detectChanges();
-  });
+    test('should have right title', () => {
+      const heading = fixture.nativeElement.querySelector('p').textContent;
+      expect(heading).toEqual('Team Name bearbeiten');
+    });
 
-  test('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    test('should have 1 input filed with right placeholder', () => {
+      const inputFileds = fixture.debugElement.queryAll(By.css('input'));
+      expect(inputFileds.length).toEqual(1);
 
-  test('should isCreating set on false', () => {
-    expect(component.isCreating).toEqual(false);
-  });
+      expect(inputFileds[0].nativeElement.placeholder).toEqual(
+        'Team bearbeiten'
+      );
+      expect(inputFileds[0].nativeElement.value).toEqual('Team1');
+    });
 
-  test('should have right title', () => {
-    const heading = fixture.nativeElement.querySelector('p').textContent;
-    expect(heading).toEqual('Team Name bearbeiten');
-  });
+    test('should have 2 buttons', () => {
+      const buttons = fixture.debugElement.queryAll(By.css('button'));
+      expect(buttons.length).toEqual(2);
 
-  test('should have 1 input filed with right placeholder', () => {
-    const inputFileds = fixture.debugElement.queryAll(By.css('input'));
-    expect(inputFileds.length).toEqual(1);
-
-    expect(inputFileds[0].nativeElement.placeholder).toEqual('Team bearbeiten');
-  });
-
-  test('should have 1 buttons', () => {
-    const buttons = fixture.debugElement.queryAll(By.css('button'));
-    expect(buttons.length).toEqual(2);
-
-    expect(buttons[0].nativeElement.textContent).toEqual(' Abbrechen ');
-    expect(buttons[1].nativeElement.textContent).toEqual(
-      ' Änderungen speichern '
-    );
+      expect(buttons[0].nativeElement.textContent).toEqual(' Abbrechen ');
+      expect(buttons[1].nativeElement.textContent).toEqual(
+        ' Änderungen speichern '
+      );
+    });
   });
 });

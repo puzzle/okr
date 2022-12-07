15ada974925c15ef651743fc4e22878cf01a2dc5
Lias Kleisa <kleisa@puzzle.ch>
2022-12-07 12:47:04 +0100

    Write test for key-result-row

--- frontend/src/app/keyresult/key-result-row/key-result-row.component.spec.ts
+++ frontend/src/app/keyresult/key-result-row/key-result-row.component.spec.ts
@@ -7,171 +7,185 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { trigger } from '@angular/animations';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { Objective } from '../../shared/services/objective.service';
 import { KeyResultRowComponent } from './key-result-row.component';
 import { MenuEntry } from 'src/app/shared/types/menu-entry';
+import {
+  ExpectedEvolution,
+  KeyResultMeasure,
+  Unit,
+} from '../../shared/services/key-result.service';
+import { DatePipe } from '@angular/common';
+import { By } from '@angular/platform-browser';
 
 describe('KeyResultKeyResultRowComponent', () => {
   let component: KeyResultRowComponent;
   let fixture: ComponentFixture<KeyResultRowComponent>;
-
-  let objective: Objective = {
+  let keyResult: KeyResultMeasure = {
     id: 1,
-    title: 'Wir wollen unseren Umsatz verdoppeln',
+    objectiveId: 1,
+    title: 'Keyresult 1',
+    description: 'This is a description',
     ownerId: 2,
-    ownerFirstname: 'Ruedi',
-    ownerLastname: 'Grochde',
-    description: 'Sehr wichtig',
-    progress: 5,
+    ownerFirstname: 'Alice',
+    ownerLastname: 'Wunderland',
     quarterId: 1,
     quarterNumber: 3,
     quarterYear: 2022,
-    created: '',
+    expectedEvolution: ExpectedEvolution.INCREASE,
+    unit: Unit.PERCENT,
+    basicValue: 0,
+    targetValue: 100,
+    measure: {
+      id: 1,
+      keyResultId: 1,
+      value: 20,
+      changeInfo: 'Change Infos',
+      initiatives: 'Initatives',
+      createdBy: 2,
+      createdOn: new Date(),
+    },
   };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        BrowserDynamicTestingModule,
-        NoopAnimationsModule,
-        MatMenuModule,
-        MatExpansionModule,
-        MatIconModule,
-        RouterTestingModule,
-        MatProgressBarModule,
-      ],
-      declarations: [KeyResultRowComponent],
-    })
-      .overrideComponent(KeyResultRowComponent, {
-        set: {
-          animations: [trigger('entryModeTransition', [])],
-        },
+  describe('KeyResultRow with set measure', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          BrowserDynamicTestingModule,
+          NoopAnimationsModule,
+          MatMenuModule,
+          MatExpansionModule,
+          MatIconModule,
+          RouterTestingModule,
+          MatProgressBarModule,
+        ],
+        providers: [DatePipe],
+        declarations: [KeyResultRowComponent],
       })
-      .compileComponents();
+        .overrideComponent(KeyResultRowComponent, {
+          set: {
+            animations: [trigger('entryModeTransition', [])],
+          },
+        })
+        .compileComponents();
 
-    fixture = TestBed.createComponent(KeyResultRowComponent);
-    component = fixture.componentInstance;
-    component.keyResult = objective;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should have objetive title as class objectiveTitle', () => {
-    expect(
-      fixture.nativeElement.querySelector('mat-panel-title').textContent
-    ).toEqual(objective.title);
-  });
-
-  it('should have progress label with progress from objective', () => {
-    let progressLabel = fixture.nativeElement
-      .querySelector('#progressContainer')
-      .querySelector('.h5');
-    expect(progressLabel.textContent).toEqual(objective.progress + '%');
-  });
-
-  it('should have progress bar with progress from objective', () => {
-    let progressBar = fixture.nativeElement
-      .querySelector('#progressContainer')
-      .querySelector('mat-progress-bar');
-    expect(progressBar.getAttribute('ng-reflect-value')).toEqual(
-      objective.progress.toString()
-    );
-  });
-
-  // @ts-ignore
-  it.each([
-    [100, 'good', true],
-    [10, 'bad', true],
-    [50, 'medium', true],
-    [10, 'good', false],
-  ])(
-    'Progressbar test with value: %i, barname %s, result: %o',
-    (progress: number, barIdentifier: string, result: boolean) => {
       fixture = TestBed.createComponent(KeyResultRowComponent);
       component = fixture.componentInstance;
-      component.keyResult = { ...objective, progress: progress } as Objective;
+      component.keyResult = keyResult;
       fixture.detectChanges();
+    });
 
+    test('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    test('should have objetive title as class objectiveTitle', () => {
+      expect(
+        fixture.nativeElement.querySelector('mat-panel-title').textContent
+      ).toEqual('Keyresult 1');
+    });
+
+    test('should have right owner', () => {
+      const ownerTag = fixture.debugElement.query(By.css('.keyResultOwner'));
+      expect(ownerTag.nativeElement.textContent).toEqual('Alice Wunderland');
+    });
+
+    test('should have right last measure when measure is set', () => {
+      const ownerTag = fixture.debugElement.query(By.css('.measure-date'));
+      expect(ownerTag.nativeElement.textContent).toEqual(' 07.812.2022 ');
+    });
+
+    test('should have progress label with right calculated progress', () => {
+      let progressLabel = fixture.debugElement.query(By.css('.h6'));
+      expect(progressLabel.nativeElement.textContent).toEqual('20%');
+    });
+
+    test('should have progress bar with progress from objective', () => {
       let progressBar = fixture.nativeElement
         .querySelector('#progressContainer')
         .querySelector('mat-progress-bar');
+      expect(progressBar.getAttribute('ng-reflect-value')).toEqual('20');
+    });
 
-      let barPrefix = 'progress-bar-';
-      let classList = [...progressBar.classList]
-        .map((e) => e.toString())
-        .filter((e) => e.includes(barPrefix));
+    test('should have menu button with icon', () => {
+      expect(fixture.nativeElement.querySelector('button').textContent).toEqual(
+        'more_vert'
+      );
+    });
 
-      let expectedClass = barPrefix + barIdentifier;
-      let activeClass = classList[0];
-      expect(activeClass === expectedClass).toBe(result);
-    }
-  );
+    // @ts-ignore
+    test.each([
+      [
+        [
+          { displayName: 'Resultat hinzufügen', routeLine: 'result/add' },
+          { displayName: 'Ziel bearbeiten', routeLine: 'objective/edit' },
+          { displayName: 'Ziel duplizieren', routeLine: 'objective/duplicate' },
+          { displayName: 'Ziel löschen', routeLine: 'objective/delete' },
+        ] as MenuEntry[],
+      ],
+      [
+        [
+          { displayName: 'Resultat bearbeiten', routeLine: 'result/add' },
+          { displayName: 'Resultat duplizieren', routeLine: 'objective/edit' },
+          { displayName: 'Details einsehen', routeLine: 'result/add' },
+          { displayName: 'Resultat löschen', routeLine: 'result/add' },
+          { displayName: 'Messung hinzufügen', routeLine: 'result/add' },
+        ] as MenuEntry[],
+      ],
+    ])('should have menu items', (menuEntries: MenuEntry[]) => {
+      fixture = TestBed.createComponent(KeyResultRowComponent);
+      component = fixture.componentInstance;
+      component.keyResult = keyResult;
+      component.menuEntries = menuEntries;
+      fixture.detectChanges();
 
-  it('should have menu button with icon', () => {
-    expect(fixture.nativeElement.querySelector('button').textContent).toEqual(
-      'more_vert'
-    );
+      let button = fixture.debugElement.nativeElement.querySelector(
+        'button[mat-icon-button]'
+      );
+      button.click();
+      let matMenu: HTMLElement = document.querySelector('.mat-menu-content')!;
+      let children = Array.from(matMenu.children)
+        .map((e) => e.querySelector('span')!)
+        .map((e) => e.textContent);
+      let itemTexts = menuEntries.map((e) => e.displayName);
+      expect(children).toEqual(itemTexts);
+    });
   });
 
-  // @ts-ignore
-  it.each([
-    [
-      [
-        { displayName: 'Resultat hinzufügen', routeLine: 'result/add' },
-        { displayName: 'Ziel bearbeiten', routeLine: 'objective/edit' },
-        { displayName: 'Ziel duplizieren', routeLine: 'objective/duplicate' },
-        { displayName: 'Ziel löschen', routeLine: 'objective/delete' },
-      ] as MenuEntry[],
-    ],
-    [
-      [
-        { displayName: 'Resultat bearbeiten', routeLine: 'result/add' },
-        { displayName: 'Resultat duplizieren', routeLine: 'objective/edit' },
-        { displayName: 'Details einsehen', routeLine: 'result/add' },
-        { displayName: 'Resultat löschen', routeLine: 'result/add' },
-        { displayName: 'Messung hinzufügen', routeLine: 'result/add' },
-      ] as MenuEntry[],
-    ],
-  ])('should have menu items', (menuEntries: MenuEntry[]) => {
-    fixture = TestBed.createComponent(KeyResultRowComponent);
-    component = fixture.componentInstance;
-    component.keyResult = objective;
-    component.menuEntries = menuEntries;
-    fixture.detectChanges();
+  describe('KeyResultRow with empty measure', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          BrowserDynamicTestingModule,
+          NoopAnimationsModule,
+          MatMenuModule,
+          MatExpansionModule,
+          MatIconModule,
+          RouterTestingModule,
+          MatProgressBarModule,
+        ],
+        providers: [DatePipe],
+        declarations: [KeyResultRowComponent],
+      })
+        .overrideComponent(KeyResultRowComponent, {
+          set: {
+            animations: [trigger('entryModeTransition', [])],
+          },
+        })
+        .compileComponents();
 
-    let button = fixture.debugElement.nativeElement.querySelector(
-      'button[mat-icon-button]'
-    );
-    button.click();
-    let matMenu: HTMLElement = document.querySelector('.mat-menu-content')!;
-    let children = Array.from(matMenu.children)
-      .map((e) => e.querySelector('span')!)
-      .map((e) => e.textContent);
-    let itemTexts = menuEntries.map((e) => e.displayName);
-    expect(children).toEqual(itemTexts);
-  });
+      fixture = TestBed.createComponent(KeyResultRowComponent);
+      component = fixture.componentInstance;
 
-  // @ts-ignore
-  it.each([
-    [['Yanick Minder', '01.01.2022']],
-    [['Vorname Nachname', '12.12.2012']],
-  ])('test information', (informationArray: string[]) => {
-    fixture = TestBed.createComponent(KeyResultRowComponent);
-    component = fixture.componentInstance;
-    component.keyResult = objective;
-    component.information = informationArray;
-    fixture.detectChanges();
+      let emptyMeasureKeyResult = keyResult;
+      emptyMeasureKeyResult.measure = null!;
+      component.keyResult = emptyMeasureKeyResult;
 
-    let informationNodes: HTMLElement[] =
-      fixture.debugElement.nativeElement.querySelector('mat-panel-description')!
-        .children[0].children;
-    let informationStrings: string[] = Array.from(informationNodes).map(
-      (e) => e.textContent!
-    );
+      fixture.detectChanges();
+    });
 
-    expect(informationStrings).toEqual(informationArray);
+    test('should have right last measure when measure is set', () => {
+      const ownerTag = fixture.debugElement.query(By.css('.measure-null-date'));
+      expect(ownerTag.nativeElement.textContent).toEqual(' - ');
+    });
   });
 });

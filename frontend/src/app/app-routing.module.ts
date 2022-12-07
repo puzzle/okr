e0874e77c69ae5d411884f585d8ab22f42aed04e
Lias Kleisa <kleisa@puzzle.ch>
2022-12-07 11:12:47 +0100

    Remove dashboard module and adjust team module to feature/306/321/304

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard-module/dashboard/dashboard.component';
-import { TeamListComponent } from './team-module/team-list/team-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TeamListComponent } from './team/team-list/team-list.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, pathMatch: 'full' },
---
+++ frontend/src/app/team/team-detail/objective-row/objective-row.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Objective } from '../objective.service';
+
+@Component({
+  selector: 'app-objective-row',
+  templateUrl: './objective-row.component.html',
+  styleUrls: ['./objective-row.component.scss'],
+})
+export class ObjectiveRowComponent implements OnInit {
+  @Input() objective!: Objective;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public addResult() {
+    console.log('Keyresult hinzufügen');
+  }
+
+  public editGoal() {
+    console.log('Objective bearbeiten');
+  }
+
+  public duplicateGoal() {
+    console.log('Objective duplizieren');
+  }
+
+  public deleteGoal() {
+    console.log('Objective löschen');
+  }
+}

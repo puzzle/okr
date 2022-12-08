78916a6b5a4017daeed4d3a9144c055994d457d0
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-08 11:54:48 +0100

    add getNumberOrNullFunction and create userService to use in different branches

---
+++ frontend/src/app/shared/common.ts
@@ -0,0 +1,8 @@
+export function getNumberOrNull(str: string|null): number|null {
+    if (str === null
+        || str.toString().trim() === '') {
+        return null;
+    }
+    const number: number = parseInt(str, 10);
+    return Number.isNaN(number) ? null : number;
+}

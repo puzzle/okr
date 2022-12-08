549a67eeb0ec6bb384fba77e6776a80e8c46dc9e
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-08 11:56:35 +0100

    reformat code

--- frontend/src/app/shared/common.ts
+++ frontend/src/app/shared/common.ts
@@ -1,8 +1,7 @@
-export function getNumberOrNull(str: string|null): number|null {
-    if (str === null
-        || str.toString().trim() === '') {
-        return null;
-    }
-    const number: number = parseInt(str, 10);
-    return Number.isNaN(number) ? null : number;
+export function getNumberOrNull(str: string | null): number | null {
+  if (str === null || str.toString().trim() === '') {
+    return null;
+  }
+  const number: number = parseInt(str, 10);
+  return Number.isNaN(number) ? null : number;
 }

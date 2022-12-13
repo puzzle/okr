774abd26ed5717cc9638a1ad9df864c90f858dfc
Simon Roth <roth@puzzle.ch>
2022-12-13 16:47:19 +0100

    Add get requests for keyresult by id and objective of that keyresult

--- frontend/src/app/keyresult/key-result-overview/key-result-overview.component.spec.ts
+++ frontend/src/app/keyresult/key-result-overview/key-result-overview.component.spec.ts
@@ -1,22 +1,81 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeyResultOverviewComponent } from './key-result-overview.component';
+import {KeyResultMeasure} from "../../shared/services/key-result.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ObjectiveModule} from "../../objective/objective.module";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterTestingModule} from "@angular/router/testing";
+import {Objective} from "../../shared/services/objective.service";
+import {Observable, of} from "rxjs";
 
 describe('KeyresultOverviewComponent', () => {
   let component: KeyResultOverviewComponent;
   let fixture: ComponentFixture<KeyResultOverviewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let keyresult1: Observable<KeyResultMeasure> = of({
+    id: 1,
+    objectiveId: 1,
+    title: 'KeyResult 1',
+    description: 'Description of KeyResult 1',
+    ownerId: 1,
+    ownerFirstname: 'Rudi',
+    ownerLastname: 'Voller',
+    quarterId: 1,
+    quarterNumber: 3,
+    quarterYear: 2022,
+    expectedEvolution: 'CONSTANT',
+    unit: 'NUMBER',
+    basicValue: 10,
+    targetValue: 50,
+    measure: {
+      id: 1,
+      keyResultId: 1,
+      value: 15,
+      changeInfo: 'Changeinfo 1',
+      initiatives: 'Initiatives 2',
+      createdBy: 1,
+      createdOn: new Date('2022-12-07T00:00:00'),
+    }
+  })
+
+  let objective: Observable<Objective> = of({
+    id: 1,
+    teamId: 1,
+    teamName: 'Team Name',
+    title: 'title',
+    ownerLastname: 'Alice',
+    ownerFirstname: 'Wunderland',
+    description: 'description',
+    quarterYear: 2022,
+    quarterNumber: 4,
+    quarterId: 1,
+    progress: 10,
+    ownerId: 1,
+    created: '01.01.2022',
+  });
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ObjectiveModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+      ],
       declarations: [KeyResultOverviewComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(KeyResultOverviewComponent);
     component = fixture.componentInstance;
+
+    component.keyResult$ = keyresult1;
+    component.objective$ = objective;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
 });
--- frontend/src/app/keyresult/key-result-overview/key-result-overview.component.ts
+++ frontend/src/app/keyresult/key-result-overview/key-result-overview.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { KeyResultService } from '../../shared/services/key-result.service';
-import { ActivatedRoute } from '@angular/router';
+import {KeyResultMeasure, KeyResultService} from '../../shared/services/key-result.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from "rxjs";
+import {Objective, ObjectiveService} from "../../shared/services/objective.service";
 
 @Component({
-  selector: 'app-keyresult-overview',
+  selector: 'app-key-result-overview',
   templateUrl: './key-result-overview.component.html',
   styleUrls: ['./key-result-overview.component.scss'],
 })
 export class KeyResultOverviewComponent implements OnInit {
-  constructor() {}
+  public keyResult$!: Observable<KeyResultMeasure>;
+  public objective$!: Observable<Objective>;
 
-  ngOnInit(): void {}
+  constructor(private keyResultService: KeyResultService,
+              private objectiveService: ObjectiveService,
+              private route: ActivatedRoute,) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.keyResult$ = this.keyResultService.getKeyResultById(params['id'])
+    });
+    this.keyResult$.subscribe(key_result => this.objective$ = this.objectiveService.getObjectiveById(key_result.objectiveId))
+  }
 }

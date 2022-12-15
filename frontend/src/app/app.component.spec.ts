6d2372ebd7ce56e2e9d57d6fa3850a6e2c6bc411
Jonas Schmid <schmid@puzzle.ch>
2022-12-15 10:15:35 +0100

    fix Puzzle Shell Navigation

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(() => {
@@ -14,12 +15,16 @@ describe('AppComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   test('should create the app', () => {
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  test('should preventLinkAction return always false', () => {
+    expect(component.preventLinkAction()).toBeFalsy();
   });
 
   test('should render OKRs navigation item', () => {
@@ -27,5 +32,46 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('pzsh-nav-item')?.textContent).toContain(
       'OKRs'
     );
+  });
+
+  describe('isOverview + isTeam', () => {
+    test("should handle '' url ", () => {
+      component.currentUrl = '';
+      expect(component.isOverview()).toEqual(true);
+      expect(component.isTeam()).toEqual(null);
+    });
+
+    test("should handle '/' url ", () => {
+      component.currentUrl = '/';
+      expect(component.isOverview()).toEqual(true);
+      expect(component.isTeam()).toEqual(null);
+    });
+
+    test("should handle '/objectives/new' url ", () => {
+      component.currentUrl = '/objectives/new';
+      expect(component.isOverview()).toEqual(true);
+      expect(component.isTeam()).toEqual(null);
+    });
+
+    test("should handle '/teams' url ", () => {
+      component.currentUrl = '/teams';
+      expect(component.isOverview()).toEqual(null);
+      expect(component.isTeam()).toEqual(true);
+    });
+
+    test("should handle '/team' url ", () => {
+      component.currentUrl = '/team';
+      expect(component.isOverview()).toEqual(null);
+      expect(component.isTeam()).toEqual(true);
+    });
+  });
+
+  describe('convertFalseToNull', () => {
+    test('should convert true value to true', () => {
+      expect(component.convertFalseToNull(true)).toEqual(true);
+    });
+    test('should convert false value to null', () => {
+      expect(component.convertFalseToNull(false)).toEqual(null);
+    });
   });
 });

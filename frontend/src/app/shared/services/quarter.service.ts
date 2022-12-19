faf8b8d6466c25a83121e6e9bf1d273ea7843590
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-19 14:59:18 +0100

    implement new backend endpoint for quarters

--- frontend/src/app/shared/services/quarter.service.ts
+++ frontend/src/app/shared/services/quarter.service.ts
@@ -14,28 +14,6 @@ export class QuarterService {
   constructor(private httpClient: HttpClient) {}
 
   public getQuarters(): Observable<Quarter[]> {
-    // return this.httpClient.get<Quarter[]>('api/v1/quarters');
-    return of([
-      {
-        id: 1,
-        label: 'GJ 22/23-Q1',
-      },
-      {
-        id: 2,
-        label: 'GJ 22/23-Q2',
-      },
-      {
-        id: 3,
-        label: 'GJ 22/23-Q3',
-      },
-      {
-        id: 4,
-        label: 'GJ 22/23-Q4',
-      },
-      {
-        id: 5,
-        label: 'GJ 23/24-Q1',
-      },
-    ]);
+    return this.httpClient.get<Quarter[]>('api/v1/quarters');
   }
 }

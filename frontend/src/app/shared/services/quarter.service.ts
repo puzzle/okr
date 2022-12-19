c4212b4324ff3311e0ff2974657b8dd9321232eb
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-19 10:43:20 +0100

    return dummy data in quarter service to not be dependant on backend impkementation of quarter

--- frontend/src/app/shared/services/quarter.service.ts
+++ frontend/src/app/shared/services/quarter.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Objective } from './objective.service';
 
 export interface Quarter {
@@ -14,6 +14,28 @@ export class QuarterService {
   constructor(private httpClient: HttpClient) {}
 
   public getQuarters(): Observable<Quarter[]> {
-    return this.httpClient.get<Quarter[]>('api/v1/quarters');
+    // return this.httpClient.get<Quarter[]>('api/v1/quarters');
+    return of([
+      {
+        id: 1,
+        label: 'GJ 22/23-Q1',
+      },
+      {
+        id: 2,
+        label: 'GJ 22/23-Q2',
+      },
+      {
+        id: 3,
+        label: 'GJ 22/23-Q3',
+      },
+      {
+        id: 4,
+        label: 'GJ 22/23-Q4',
+      },
+      {
+        id: 5,
+        label: 'GJ 23/24-Q1',
+      },
+    ]);
   }
 }

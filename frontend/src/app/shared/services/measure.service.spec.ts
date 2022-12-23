a9698fe048e1f6f4a15cd5b1c02b71b6a661bb2a
Lias Kleisa <kleisa@puzzle.ch>
2022-12-23 15:57:29 +0100

    Add test for measureservice

--- frontend/src/app/shared/services/measure.service.spec.ts
+++ frontend/src/app/shared/services/measure.service.spec.ts
@@ -1,19 +1,55 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MeasureService } from './measure.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Measure, MeasureService } from './measure.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+const respons = [
+  {
+    id: 1,
+    keyResultId: 1,
+    value: 20,
+    changeInfo: 'Changeinfo',
+    initiatives: 'Initiatives',
+    createdBy: 1,
+    createdOn: new Date(),
+    measureDate: new Date(),
+  },
+];
 
 describe('MeasureService', () => {
   let service: MeasureService;
+  let httpTestingController: HttpTestingController;
+  const URL = '/api/v1/measures';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(MeasureService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('should get measure by id', (done) => {
+    service.getMeasureById(1).subscribe({
+      next(response: Measure) {
+        expect(response).toEqual(respons);
+        done();
+      },
+      error(error) {
+        done(error);
+      },
+    });
+
+    const req = httpTestingController.expectOne(`${URL}/1`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(respons);
+    httpTestingController.verify();
   });
 });

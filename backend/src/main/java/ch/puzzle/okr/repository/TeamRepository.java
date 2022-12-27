c2e193adde87dc0c23204590adc25fe496cbb5fa
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-27 16:21:53 +0100

    run backend formatter again

--- backend/src/main/java/ch/puzzle/okr/repository/TeamRepository.java
+++ backend/src/main/java/ch/puzzle/okr/repository/TeamRepository.java
@@ -8,7 +8,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TeamRepository extends CrudRepository<Team, Long> {
-    //This methods overrides default CrudRepository findAllById, this affects that if given list in parameter is null every value is returned instead of an empty list
+    // This methods overrides default CrudRepository findAllById, this affects that if given list in parameter is null
+    // every value is returned instead of an empty list
     @Query(value = "select t from Team as t where coalesce(:teamIds, null) is null or t.id in (:teamIds)")
     List<Team> findAllById(@Param("teamIds") List<Long> teamIds);
 }

55174d42862c1e7956c170d911e28517bd94ae9e
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-28 16:59:34 +0100

    implement svg service specs

--- frontend/src/app/shared/services/svg/svg.service.spec.ts
+++ frontend/src/app/shared/services/svg/svg.service.spec.ts
@@ -9,7 +9,7 @@ import {
 describe('SvgService', () => {
   let service: SvgService;
   let httpTestingController: HttpTestingController;
-  const URL = '';
+  const URL = '../../assets/images';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -21,5 +21,20 @@ describe('SvgService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  test('should get svg', (done) => {
+    service.getSvg('progress_bar.svg').subscribe({
+      next() {
+        done();
+      },
+      error(error) {
+        done(error);
+      },
+    });
+
+    const req = httpTestingController.expectOne(`${URL}/progress_bar.svg`);
+    expect(req.request.method).toEqual('GET');
+    httpTestingController.verify();
   });
 });

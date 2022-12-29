d0a9c2a17f83f4e64e38f56467c640dac2779a9a
Lias Kleisa <kleisa@puzzle.ch>
2022-12-29 10:53:25 +0100

    Finish specs for measure service

--- frontend/src/app/shared/services/measure.service.spec.ts
+++ frontend/src/app/shared/services/measure.service.spec.ts
@@ -8,7 +8,6 @@ import {
 import * as measureData from '../testing/mock-data/measure.json';
 
 const respons = measureData.measure;
-const badRespons = 'Did not find the Measure with requested id';
 
 describe('MeasureService', () => {
   let service: MeasureService;
@@ -57,6 +56,73 @@ describe('MeasureService', () => {
     const req = httpTestingController.expectOne(`${URL}/34567`);
     expect(req.request.method).toEqual('GET');
     req.flush(null, { status: 404, statusText: 'Not Found' });
+    httpTestingController.verify();
+  });
+
+  test('should create measure', (done) => {
+    let measure: Measure = {
+      id: null,
+      measureDate: new Date('2022-12-24'),
+      createdOn: new Date('2023-01-18'),
+      createdBy: 0,
+      changeInfo: 'Changeinfo',
+      initiatives: 'Initiatives',
+      value: 42,
+      keyResultId: 1,
+    };
+    service.saveMeasure(measure, true).subscribe({
+      next(response: Measure) {
+        expect(response).toBe(respons);
+        done();
+      },
+      error(error) {
+        done(error);
+      },
+    });
+
+    const req = httpTestingController.expectOne(`${URL}`);
+    expect(req.request.method).toEqual('POST');
+    req.flush(respons);
+    expect(req.request.body.changeInfo).toContain('Changeinfo');
+    expect(req.request.body.value).toEqual(42);
+    expect(req.request.body.measureDate).toEqual(new Date('2022-12-24'));
+    httpTestingController.verify();
+  });
+
+  test('should update Measure', (done) => {
+    let measure: Measure = {
+      id: 33,
+      measureDate: new Date('2022-12-24'),
+      createdOn: new Date('2022-12-28'),
+      createdBy: 0,
+      changeInfo: 'Changeinfo',
+      initiatives: 'Initiatives',
+      value: 42,
+      keyResultId: 1,
+    };
+    service.saveMeasure(measure, false).subscribe({
+      next(response: Measure) {
+        expect(response).toBe(respons);
+        done();
+      },
+      error(error) {
+        done(error);
+      },
+    });
+
+    const req = httpTestingController.expectOne(`${URL}/33`);
+    expect(req.request.method).toEqual('PUT');
+    req.flush(respons);
+    expect(req.request.body).toEqual({
+      id: 33,
+      measureDate: new Date('2022-12-24'),
+      createdOn: new Date('2022-12-28'),
+      createdBy: 0,
+      changeInfo: 'Changeinfo',
+      initiatives: 'Initiatives',
+      value: 42,
+      keyResultId: 1,
+    });
     httpTestingController.verify();
   });
 });

a16c22aeb1cd423856e3c669f91990727c8695d1
Lias Kleisa <kleisa@puzzle.ch>
2022-12-30 14:19:25 +0100

    Write tests for diagram component

--- frontend/src/app/keyresult/diagram/diagram.component.spec.ts
+++ frontend/src/app/keyresult/diagram/diagram.component.spec.ts
@@ -1,22 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DiagramComponent } from './diagram.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
+import {
+  KeyResultService,
+  Measure,
+} from '../../shared/services/key-result.service';
+import * as measureData from '../../shared/testing/mock-data/measure.json';
 
 describe('DiagramComponent', () => {
   let component: DiagramComponent;
   let fixture: ComponentFixture<DiagramComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let measures: Observable<Measure[]> = of(measureData.measures);
+
+  const mockKeyResultService = {
+    getMeasuresOfKeyResult: jest.fn(),
+  };
+
+  beforeEach(() => {
+    mockKeyResultService.getMeasuresOfKeyResult.mockReturnValue(measures);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: KeyResultService, useValue: mockKeyResultService },
+      ],
+
       declarations: [DiagramComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DiagramComponent);
     component = fixture.componentInstance;
+    component.keyResultId = 1;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    mockKeyResultService.getMeasuresOfKeyResult.mockReset();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get keyresult by id', () => {
+    expect(mockKeyResultService.getMeasuresOfKeyResult).toHaveBeenCalledTimes(
+      1
+    );
+    expect(mockKeyResultService.getMeasuresOfKeyResult).toHaveBeenCalledWith(1);
+  });
+
+  it('should set measures in component', () => {
+    expect(component.measures).toEqual(measures);
   });
 });

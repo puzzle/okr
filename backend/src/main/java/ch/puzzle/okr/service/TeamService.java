229a40ee240bf0bd2f73864f4666cad71d3fa728
Niklas Jäggi <jaeggi@puzzle.ch>
2022-12-30 14:14:23 +0100

    implement sorting for team endpoint too

--- backend/src/main/java/ch/puzzle/okr/service/TeamService.java
+++ backend/src/main/java/ch/puzzle/okr/service/TeamService.java
@@ -6,19 +6,30 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.text.Collator;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
 
 @Service
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    Collator collator = Collator.getInstance(Locale.GERMAN);
 
     public TeamService(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
 
     public List<Team> getAllTeams() {
-        return (List<Team>) teamRepository.findAll();
+        Team puzzleTeam = teamRepository.findByName("Puzzle ITC");
+        List<Team> teamList = (List<Team>) teamRepository.findAll();
+        return Stream
+                .concat(Stream.of(puzzleTeam),
+                        teamList.stream()
+                                .filter(team -> !"Puzzle ITC".equals(team.getName()))
+                                .sorted((team1, team2) -> collator.compare(team1.getName(), team2.getName())))
+                .toList();
     }
 
     public Team getTeamById(long id) {

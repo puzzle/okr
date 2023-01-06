1ee2b876ab5585abcba0165d8b78c658a31c7b65
Niklas Jäggi <jaeggi@puzzle.ch>
2023-01-06 17:17:19 +0100

    test with reload page and start implementing dashboard url changes when expanding panel

--- frontend/src/app/objective/objective-row/objective-row.component.ts
+++ frontend/src/app/objective/objective-row/objective-row.component.ts
@@ -11,7 +11,9 @@ import {
   KeyResultService,
 } from '../../shared/services/key-result.service';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import { Location } from '@angular/common';
+import { getNumberOrNull } from '../../shared/common';
 
 @Component({
   selector: 'app-objective-row',
@@ -23,15 +25,32 @@ export class ObjectiveRowComponent implements OnInit {
   @Input() objective!: Objective;
   keyResultList: Observable<KeyResultMeasure[]> = new BehaviorSubject([]);
   menuEntries!: MenuEntry[];
+  isSelected: boolean = false;
   constructor(
     private keyResultService: KeyResultService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
   ) {}
 
   public getKeyResults(id: number) {
+      this.router.navigate([],
+          {
+            relativeTo: this.route,
+            queryParams: { objectives: this.objective.id },
+            queryParamsHandling: 'merge'
+          });
     this.keyResultList = this.keyResultService.getKeyResultsOfObjective(id);
   }
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params['objectives'] !== undefined) {
+      const selectedObjectives: string[] = params['objectives'].split(',');
+       if (selectedObjectives.some(x => getNumberOrNull(x) === this.objective.id)) {
+         this.isSelected = true;
+       }
+      }
+     })
     this.menuEntries = [
       {
         displayName: 'KeyResult hinzufügen',

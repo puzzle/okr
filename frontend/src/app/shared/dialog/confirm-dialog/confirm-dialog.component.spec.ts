289e336e4937011c65ab87d72abbb99b567a19b8
Niklas Jäggi <jaeggi@puzzle.ch>
2023-01-09 16:48:59 +0100

    include dialog spec

--- frontend/src/app/shared/dialog/confirm-dialog/confirm-dialog.component.spec.ts
+++ frontend/src/app/shared/dialog/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmDialogComponent } from './confirm-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
-xdescribe('ConfirmDialog', () => {
+describe('ConfirmDialog', () => {
   let component: ConfirmDialogComponent;
   let fixture: ComponentFixture<ConfirmDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ConfirmDialogComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmDialogComponent);

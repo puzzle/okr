285d27bf7c81f955168fb5ae456762f9297cd9f4
Jonas Schmid <schmid@puzzle.ch>
2023-01-10 14:46:25 +0100

    add types instead of any

--- frontend/src/app/shared/testing/Loader.ts
+++ frontend/src/app/shared/testing/Loader.ts
@@ -1,14 +1,18 @@
 import { Measure } from '../services/measure.service';
 import * as measureData from './mock-data/measure.json';
 
+type NameOfMeasure = {
+  [name: string]: Measure;
+};
+
+const measureDataObject: NameOfMeasure = measureData as any as NameOfMeasure;
+
 export function loadMeasure(name: string): Measure {
-  let measureDataObject: any = measureData;
   return overwriteObjectWithCorrectDate(measureDataObject[name]);
 }
 
-export function loadAllMeasure(loadWithString: boolean): any[] {
-  let measureDataObject: any = measureData;
-  let measureArray: any[] = [];
+export function loadAllMeasure(loadWithString: boolean): Measure[] {
+  let measureArray: Measure[] = [];
   for (let prop in measureDataObject) {
     if (loadWithString) {
       measureArray.push(measureDataObject[prop]);
@@ -19,7 +23,7 @@ export function loadAllMeasure(loadWithString: boolean): any[] {
   return measureArray;
 }
 
-function overwriteObjectWithCorrectDate(measure: any): any {
+function overwriteObjectWithCorrectDate(measure: Measure): Measure {
   return Object.assign({}, measure, {
     createdOn: new Date(measure.createdOn),
     measureDate: new Date(measure.measureDate),

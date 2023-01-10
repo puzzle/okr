372fec785dde1c5ffa180ea9a2a873a8716cd87d
megli2 <egli@puzzle.ch>
2023-01-10 10:12:01 +0100

    delete unnecessary convert function

--- frontend/src/app/shared/testing/Loader.ts
+++ frontend/src/app/shared/testing/Loader.ts
@@ -4,11 +4,7 @@ import * as measureData from './mock-data/measure.json';
 export function loadMeasure(name: string): Measure {
   let measureDataObject: any = measureData;
   return Object.assign({}, measureDataObject[name], {
-    createdOn: turnToIsoDate(new Date(measureDataObject[name].createdOn)),
-    measureDate: turnToIsoDate(new Date(measureDataObject[name].measureDate)),
+    createdOn: new Date(measureDataObject[name].createdOn),
+    measureDate: new Date(measureDataObject[name].measureDate),
   });
-}
-
-function turnToIsoDate(date: Date): Date {
-  return new Date(date.toISOString());
 }

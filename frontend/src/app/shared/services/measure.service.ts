1766e5c516984d35d01ffea4cac0eb2da024d5aa
Niklas Jäggi <jaeggi@puzzle.ch>
2023-01-19 11:36:39 +0100

    format frontend

--- frontend/src/app/shared/services/measure.service.ts
+++ frontend/src/app/shared/services/measure.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { KeyResultMeasure } from './key-result.service';
 
 export interface Measure {
   id: number | null;

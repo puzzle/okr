e0ef55acba64ed42cd511ad215dade514fdef585
Niklas Jäggi <jaeggi@puzzle.ch>
2023-01-19 11:17:29 +0100

    implement backend request, fix dialog and reload measure list in frontend

--- frontend/src/app/shared/components/shared/measure-row/measure-row.component.ts
+++ frontend/src/app/shared/components/shared/measure-row/measure-row.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, switchMap } from 'rxjs';
 import {
   KeyResultService,
   Measure,
@@ -10,6 +10,8 @@ import { DatePipe } from '@angular/common';
 import { ConfirmDialogComponent } from '../../../dialog/confirm-dialog/confirm-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MeasureService } from '../../../services/measure.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-measure-row',
@@ -17,27 +19,19 @@ import { MeasureService } from '../../../services/measure.service';
   styleUrls: ['./measure-row.component.scss'],
 })
 export class MeasureRowComponent implements OnInit {
-  measures$!: Observable<Measure[]>;
+  measures$: Subject<Measure[]> = new BehaviorSubject<Measure[]>([]);
 
   constructor(
     private keyresultService: KeyResultService,
     private route: ActivatedRoute,
     private datePipe: DatePipe,
     private dialog: MatDialog,
-    private measureService: MeasureService
+    private measureService: MeasureService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
-    this.measures$ = this.route.paramMap.pipe(
-      switchMap((params) => {
-        const keyResultId = getNumberOrNull(params.get('keyresultId'));
-        if (keyResultId) {
-          return this.keyresultService.getMeasuresOfKeyResult(keyResultId);
-        } else {
-          throw Error('KeyResult with Id ' + keyResultId + " doesn't exist");
-        }
-      })
-    );
+    this.reloadMeasures();
   }
 
   formatDate(date: string) {
@@ -45,13 +39,12 @@ export class MeasureRowComponent implements OnInit {
     return this.datePipe.transform(convertedDate, 'dd.MM.yyyy', 'CEST');
   }
 
-  delete() {
+  openDeleteDialog(id: number) {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       width: '400px',
       disableClose: true,
       data: {
-        title:
-          'Willst du dieses Key Result und die dazugehörigen Messungen wirklich löschen?',
+        title: 'Willst du diese Messung wirklich löschen?',
         confirmText: 'Bestätigen',
         closeText: 'Abbrechen',
       },
@@ -59,7 +52,44 @@ export class MeasureRowComponent implements OnInit {
     dialogRef.componentInstance.closeDialog.subscribe((confirm) => {
       if (confirm) {
         dialogRef.componentInstance.displaySpinner = true;
-        //implement delete + toaster
+        this.measureService.deleteMeasureById(id).subscribe({
+          next: () => {
+            dialogRef.componentInstance.displaySpinner = false;
+            dialogRef.close();
+            this.reloadMeasures();
+            this.toastr.success('', 'Messung gelöscht!', {
+              timeOut: 5000,
+            });
+          },
+          error: (e: HttpErrorResponse) => {
+            dialogRef.componentInstance.displaySpinner = false;
+            dialogRef.close();
+            this.toastr.error(
+              'Messung konnte nicht gelöscht werden!',
+              'Fehlerstatus: ' + e.status,
+              {
+                timeOut: 5000,
+              }
+            );
+          },
+        });
+      } else {
+        dialogRef.close();
+      }
+    });
+  }
+
+  reloadMeasures(): void {
+    this.route.paramMap.subscribe((params) => {
+      const keyResultId = getNumberOrNull(params.get('keyresultId'));
+      if (keyResultId) {
+        this.keyresultService
+          .getMeasuresOfKeyResult(keyResultId)
+          .subscribe((data) => {
+            this.measures$.next(data);
+          });
+      } else {
+        throw Error('KeyResult with Id ' + keyResultId + " doesn't exist");
       }
     });
   }

5c49eb0231bf79199a6e806f9b8700880b29e080
Yanick Minder <minder@puzzle.ch>
2023-01-31 10:01:08 +0100

    Change datatype of owner id in KeyResultMeasure so null is allowed

--- frontend/src/app/shared/services/key-result.service.ts
+++ frontend/src/app/shared/services/key-result.service.ts
@@ -18,7 +18,7 @@ export interface KeyResultMeasure {
   objectiveId: number;
   title: string;
   description: string;
-  ownerId: number;
+  ownerId: number | null;
   ownerFirstname: string;
   ownerLastname: string;
   expectedEvolution: string | null;
@@ -62,7 +62,7 @@ export class KeyResultService {
       description: '',
       expectedEvolution: null,
       unit: '',
-      ownerId: 0,
+      ownerId: null,
       ownerLastname: '',
       ownerFirstname: '',
       targetValue: 0,

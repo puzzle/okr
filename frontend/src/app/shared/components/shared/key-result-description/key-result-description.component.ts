4cee7164b351f36487d079befc87278b0e83e041
Yanick Minder <minder@puzzle.ch>
2023-02-01 14:31:34 +0100

    beautify frontend

--- frontend/src/app/shared/components/shared/key-result-description/key-result-description.component.ts
+++ frontend/src/app/shared/components/shared/key-result-description/key-result-description.component.ts
@@ -1,36 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import {
-  KeyResultMeasure,
-  KeyResultService,
-} from 'src/app/shared/services/key-result.service';
+import { Component, Input } from '@angular/core';
 import { Goal } from '../../../services/goal.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-key-result-description',
   templateUrl: './key-result-description.component.html',
   styleUrls: ['./key-result-description.component.scss'],
 })
-export class KeyResultDescriptionComponent implements OnInit {
+export class KeyResultDescriptionComponent {
   @Input() goal!: Goal;
-  keyResultMeasures$!: Observable<KeyResultMeasure>;
 
-  constructor(private keyResultService: KeyResultService) {}
-
-  ngOnInit() {
-    this.keyResultMeasures$ = this.keyResultService.getKeyResultById(
-      this.goal.keyresult.id
-    );
-  }
-
-  calculateEffectiveValue(
-    basicValue: number,
-    targetValue: number,
-    progress: number
-  ): number {
-    let range = targetValue - basicValue;
-    let percentValue = (range / 100) * progress;
-    let lowerBound = Math.min(basicValue, targetValue);
-    return Math.abs(percentValue + lowerBound);
-  }
+  constructor() {}
 }

d756a00f56deb7e2877f0a2e2fe19ca904238745
peggimann <peggimann@puzzle.com>
2023-03-13 17:07:15 +0100

    implement max and min evolution in frontend, fix style

--- frontend/src/app/shared/services/key-result.service.spec.ts
+++ frontend/src/app/shared/services/key-result.service.spec.ts
@@ -3,12 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { KeyResultMeasure, KeyResultService } from './key-result.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import * as keyresultData from '../testing/mock-data/keyresults.json';
+import spyOn = jest.spyOn;
+import any = jasmine.any;
+import { error } from '@angular/compiler-cli/src/transformers/util';
 
 const response = keyresultData.keyresults;
+const createKeyResultObject = keyresultData.createKeyResultObject;
 
 describe('KeyResultService', () => {
   let service: KeyResultService;
   let httpTestingController: HttpTestingController;
+  let httpClientTesting: HttpClientTestingModule;
   const URL = '/api/v1/objectives';
 
   beforeEach(() => {
@@ -17,10 +22,19 @@ describe('KeyResultService', () => {
     });
     service = TestBed.inject(KeyResultService);
     httpTestingController = TestBed.inject(HttpTestingController);
+    httpClientTesting = TestBed.inject(HttpClientTestingModule);
   });
 
   test('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  xtest('should set Keyresult.basicvalue to null', (done) => {
+    //TODO: implement test
+  });
+
+  xtest('should set Keyresult.basicvalue to null on create new Keyresult', (done) => {
+    //   TODO: implement test cases
   });
 
   test('should get Key Results of Objective', (done) => {

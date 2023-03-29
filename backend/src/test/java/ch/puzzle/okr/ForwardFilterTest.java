81f17ae04359666d24c2ce3f83a2310d36514fcb
Jonas Schmid <schmid@puzzle.ch>
2023-03-29 09:45:44 +0200

    add ForwardFilter, dynamic redirectUri

---
+++ backend/src/test/java/ch/puzzle/okr/ForwardFilterTest.java
@@ -0,0 +1,73 @@
+package ch.puzzle.okr;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ForwardFilterTest {
+    @InjectMocks
+    ForwardFilter forwardFilter;
+
+    @Mock
+    HttpServletRequest request;
+
+    @Mock
+    ServletResponse response;
+
+    @Mock
+    FilterChain filterChain;
+
+    @Mock
+    RequestDispatcher requestDispatcher;
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "/", "/api/", "/index.html", "/runtime.", "/polyfills.", "/main.", "/scripts.", "/styles.",
+            "/favicon.ico", "/3rdpartylicenses.txt", "/assets/" })
+    void shouldNotFilterTheRootPath(String requestUri) throws ServletException, IOException {
+        // given
+        when(request.getRequestURI()).thenReturn(requestUri);
+        doNothing().when(filterChain).doFilter(Mockito.eq(request), Mockito.eq(response));
+
+        // when
+        forwardFilter.doFilter(request, response, filterChain);
+
+        // then
+        verify(filterChain, times(1)).doFilter(Mockito.eq(request), Mockito.eq(response));
+        verify(request, never()).getRequestDispatcher(anyString());
+    }
+
+    @Test
+    void shouldFilterAuthPath() throws ServletException, IOException {
+        // given
+        when(request.getRequestURI()).thenReturn("/auth");
+        when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+        doNothing().when(requestDispatcher).forward(Mockito.eq(request), Mockito.eq(response));
+
+        // when
+        forwardFilter.doFilter(request, response, filterChain);
+
+        // then
+        verify(filterChain, never()).doFilter(Mockito.eq(request), Mockito.eq(response));
+        verify(request, times(1)).getRequestDispatcher(anyString());
+    }
+}
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,9 @@ export const environment = {
   oauth: {
     issuer: 'https://sso.puzzle.ch/auth/realms/pitc',
     strictDiscoveryDocumentValidation: false,
-    redirectUri: 'https://okr.ocp-internal.cloudscale.puzzle.ch/auth/keycloakopenid/callback',
+    // redirectUri: 'http://localhost:8080/auth/keycloakopenid/callback',
+    // redirectUri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/auth/keycloakopenid/callback${window.location.search}`,
+    redirectUri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/auth/keycloakopenid/callback`,
     scope: 'profile offline_access openid',
     clientId: 'pitc_okr_prod',
     responseType: 'code',

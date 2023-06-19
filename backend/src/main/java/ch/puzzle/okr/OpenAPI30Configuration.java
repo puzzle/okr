afaa5547f4bdd81903bb5e847acdd74a91bd44cb
peggimann <peggimann@puzzle.com>
2023-06-19 16:22:16 +0200

    format backend

--- backend/src/main/java/ch/puzzle/okr/OpenAPI30Configuration.java
+++ backend/src/main/java/ch/puzzle/okr/OpenAPI30Configuration.java
@@ -12,15 +12,9 @@ public class OpenAPI30Configuration {
     @Bean
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
-                                .name(securitySchemeName)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")));
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                        .name(securitySchemeName).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
-    
+
 }

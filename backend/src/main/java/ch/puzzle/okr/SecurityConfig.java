06b8a74d5be8e28785559fac77def40c7bb53a76
peggimann <peggimann@puzzle.com>
2023-06-23 14:50:07 +0200

    format backend

--- backend/src/main/java/ch/puzzle/okr/SecurityConfig.java
+++ backend/src/main/java/ch/puzzle/okr/SecurityConfig.java
@@ -15,21 +15,18 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .headers(headers -> headers
-                                .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-                                .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN))
-                                .permissionsPolicy(permissions -> permissions.policy("accelerometer=(), ambient-light-sensor=(), autoplay=(), " +
-                                        "battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), " +
-                                        "execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=()," +
-                                        " geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), " +
-                                        "midi=(), navigation-override=(), payment=(), picture-in-picture=()," +
-                                        " publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(self), " +
-                                        "usb=(), web-share=(), xr-spatial-tracking=()"))
-                                .and().xssProtection().block(false)
-                                .and().httpStrictTransportSecurity().includeSubDomains(true).maxAgeInSeconds(31536000)
-                                .and().defaultsDisabled().contentTypeOptions()
-                );
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                .referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN))
+                .permissionsPolicy(
+                        permissions -> permissions.policy("accelerometer=(), ambient-light-sensor=(), autoplay=(), "
+                                + "battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), "
+                                + "execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(),"
+                                + " geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), "
+                                + "midi=(), navigation-override=(), payment=(), picture-in-picture=(),"
+                                + " publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(self), "
+                                + "usb=(), web-share=(), xr-spatial-tracking=()"))
+                .and().xssProtection().block(false).and().httpStrictTransportSecurity().includeSubDomains(true)
+                .maxAgeInSeconds(31536000).and().defaultsDisabled().contentTypeOptions());
 
         http.antMatcher("/api/**")
                 .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())

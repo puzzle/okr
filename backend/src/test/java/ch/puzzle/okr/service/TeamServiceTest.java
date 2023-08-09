330ad27e5a5ef59ee3b1dfe2287c4b5bec764f8e
Yanick Minder <minder@puzzle.ch>
2023-08-09 16:13:38 +0200

    make validation generic and implemnt teamValidationService

--- backend/src/test/java/ch/puzzle/okr/service/TeamServiceTest.java
+++ backend/src/test/java/ch/puzzle/okr/service/TeamServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;

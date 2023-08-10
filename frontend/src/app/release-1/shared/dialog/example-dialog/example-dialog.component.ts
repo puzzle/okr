652becc7a88eec827899f2f020070c6f6863e579
megli2 <egli@puzzle.ch>
2023-08-10 09:48:46 +0200

    implement example-dialog with all necessary html elements to test

---
+++ frontend/src/app/release-1/shared/dialog/example-dialog/example-dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example-dialog',
+  templateUrl: './example-dialog.component.html',
+  styleUrls: ['./example-dialog.component.scss']
+})
+export class ExampleDialogComponent implements OnInit {
+  cars = ["Volvo", "Saab", "Mercedes", "Audi"]
+  selected = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

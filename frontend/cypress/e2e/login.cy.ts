023eb6ddd65d9c29a26a7e7026eb64a9a906835a
Lias Kleisa <kleisa@puzzle.ch>
2023-08-14 15:16:10 +0200

    Rename login e2e spec

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -3,10 +3,16 @@ describe('OKR Login', () => {
     cy.loginWithCredentials('johnny', '123'); // Call the custom login command before each test
   });
 
-  it('Logs in with user johnny and has right overview', () => {
+  it('Logs in with user johnny and has right overview and teams', () => {
     cy.contains('Objectives und Key Results');
     cy.contains('Overview');
     cy.contains('Team');
+    cy.contains('Puzzle ITC');
+    cy.contains('/BBT');
+    cy.wait(500);
+    cy.get('pzsh-nav-item:last').click();
+    cy.contains('Teams');
+    cy.contains('Team erstellen');
     cy.contains('Puzzle ITC');
     cy.contains('/BBT');
   });

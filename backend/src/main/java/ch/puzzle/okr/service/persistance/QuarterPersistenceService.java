d5937a310a3f2e7813a011e84be099461b232823
Manuel Möri <moeri@puzzle.ch>
2023-08-21 10:33:50 +0200

    Add @Service annotation to QuarterPersistenceService

--- backend/src/main/java/ch/puzzle/okr/service/persistance/QuarterPersistenceService.java
+++ backend/src/main/java/ch/puzzle/okr/service/persistance/QuarterPersistenceService.java
@@ -2,7 +2,9 @@ package ch.puzzle.okr.service.persistance;
 
 import ch.puzzle.okr.models.Quarter;
 import ch.puzzle.okr.repository.QuarterRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class QuarterPersistenceService extends PersistenceBase<Quarter, Long> {
     protected QuarterPersistenceService(QuarterRepository repository) {
         super(repository);

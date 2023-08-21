4d2325e689e2be74dcdaa082e6e633ec7d0069b2
Jannik Pulfer <pulfer@puzzle.ch>
2023-08-21 15:56:32 +0200

    Add grey background to whole page, add mat-menu for three-dot-menu and its styling

--- frontend/src/app/release-2/objective/objective.module.ts
+++ frontend/src/app/release-2/objective/objective.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ObjectiveColumnComponent } from './objective-column/objective-column.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [ObjectiveColumnComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatMenuModule],
 })
 export class ObjectiveModule {}

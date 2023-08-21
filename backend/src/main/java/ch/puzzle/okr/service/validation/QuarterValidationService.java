663a695fc20265323c1d522e875274c0e537dd6a
Manuel Möri <moeri@puzzle.ch>
2023-08-21 10:30:45 +0200

    Modify validateOnCreate method in QuarterValidationService

--- backend/src/main/java/ch/puzzle/okr/service/validation/QuarterValidationService.java
+++ backend/src/main/java/ch/puzzle/okr/service/validation/QuarterValidationService.java
@@ -1,14 +1,20 @@
 package ch.puzzle.okr.service.validation;
 
 import ch.puzzle.okr.models.Quarter;
+import ch.puzzle.okr.service.persistance.QuarterPersistenceService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuarterValidationService extends ValidationBase<Quarter, Long> {
 
+    public QuarterValidationService(QuarterPersistenceService quarterPersistenceService) {
+        super(quarterPersistenceService);
+    }
     @Override
     public void validateOnCreate(Quarter model) {
-
+        throwExceptionIfModelIsNull(model);
+        throwExceptionWhenIdIsNotNull(model.getId());
+        validate(model);
     }
 
     @Override

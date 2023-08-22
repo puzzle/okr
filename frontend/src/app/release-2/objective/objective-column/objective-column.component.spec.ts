4abab351be339302fb03666a09476b1ca54b66a1
Jannik Pulfer <pulfer@puzzle.ch>
2023-08-22 15:06:09 +0200

    Add test for objective-column component to test if three-dot menu opens

--- frontend/src/app/release-2/objective/objective-column/objective-column.component.spec.ts
+++ frontend/src/app/release-2/objective/objective-column/objective-column.component.spec.ts
@@ -1,22 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ObjectiveColumnComponent } from './objective-column.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuHarness } from '@angular/material/menu/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { trigger } from '@angular/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ObjectiveColumnComponent', () => {
   let component: ObjectiveColumnComponent;
   let fixture: ComponentFixture<ObjectiveColumnComponent>;
-
+  let loader: HarnessLoader;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ObjectiveColumnComponent],
+      imports: [MatMenuModule, MatCardModule, NoopAnimationsModule],
     }).compileComponents();
-
     fixture = TestBed.createComponent(ObjectiveColumnComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('Open mat-menu button after click on three-dot menu-button', async () => {
+    const menu = await loader.getHarness(MatMenuHarness.with({ selector: '.three-dot-menu' }));
+    expect(await menu.isOpen()).toBe(false);
+    await menu.open();
+    expect(await menu.isOpen()).toBe(true);
   });
 });

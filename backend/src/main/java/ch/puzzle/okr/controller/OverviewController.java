9b8efbd3c7adfa36d90de4540de6ebc527047585
megli2 <egli@puzzle.ch>
2023-08-23 11:54:29 +0200

    delete unused imports in overview controller

--- backend/src/main/java/ch/puzzle/okr/controller/OverviewController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/OverviewController.java
@@ -1,20 +1,8 @@
 package ch.puzzle.okr.controller;
 
-import ch.puzzle.okr.dto.OverviewDto;
 import ch.puzzle.okr.service.OverviewService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/overview")

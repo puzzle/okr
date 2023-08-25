2b3c33521e1b5a682eedd6a5f7de45de68a2ee5b
Jannik Pulfer <pulfer@puzzle.ch>
2023-08-25 08:46:37 +0200

    Rename blue-status-indicator to ongoing-status-indicator, remove version text, add function that sets the source of the indicator based on the state

--- frontend/src/app/release-2/objective/objective-column/objective-column.component.spec.ts
+++ frontend/src/app/release-2/objective/objective-column/objective-column.component.spec.ts
@@ -3,13 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ObjectiveColumnComponent } from './objective-column.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
-import { MatMenuHarness } from '@angular/material/menu/testing';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { style, trigger } from '@angular/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { By } from '@angular/platform-browser';
-import { getStylesConfig } from '@angular-devkit/build-angular/src/webpack/configs';
 
 describe('ObjectiveColumnComponent', () => {
   let component: ObjectiveColumnComponent;
--- frontend/src/app/release-2/objective/objective-column/objective-column.component.ts
+++ frontend/src/app/release-2/objective/objective-column/objective-column.component.ts
@@ -9,6 +9,7 @@ import { KeyResultMeasure } from '../../../release-1/shared/services/key-result.
 export class ObjectiveColumnComponent implements OnInit {
   objectiveTitle: string = '';
   keyResults: String[] = []; //ToDo in KeyResult ticket
+  state: String = 'DRAFT';
 
   constructor(private overviewService: OverviewService) {}
 
@@ -23,6 +24,21 @@ export class ObjectiveColumnComponent implements OnInit {
     //Add JSON string since there is no keyResult model yet
     for (let keyResult of objectiveWithKeyresults.keyresults) {
       this.keyResults.push(JSON.stringify(keyResult));
+    }
+
+    this.state = objectiveWithKeyresults.state;
+  }
+
+  getCorrectStateSrc() {
+    switch (this.state) {
+      case 'ONGOING':
+        return '/assets/ongoing-status-indicator.svg';
+      case 'DISSATISFIED':
+        return '/assets/dissatisfied-status-indicator.svg';
+      case 'SATISFIED':
+        return '/assets/very-satisfied-status-indicator.svg';
+      default:
+        return '/assets/draft-status-indicator.svg';
     }
   }
 

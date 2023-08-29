84c1f6e85bebed8a1ac7fe1fc2cefddce9d1a147
megli2 <egli@puzzle.ch>
2023-08-29 10:14:19 +0200

    implement dynamic margin of banner to topbar depending on height of topbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, map, Observable } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
@@ -16,6 +16,9 @@ import { RouteService } from './shared/services/route.service';
 export class AppComponent implements OnInit {
   currentUrl: string = '/';
   isEnvStaging$: Observable<boolean>;
+
+  @ViewChild('okrTopbar')
+  okrTopbar: ElementRef | undefined;
 
   constructor(
     private router: Router,

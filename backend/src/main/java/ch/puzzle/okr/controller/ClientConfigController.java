98fd2ea57acce01496a01aad562f0d6eba514702
peggimann <peggimann@puzzle.com>
2023-08-31 08:25:01 +0200

    use rest annotation in controller

--- backend/src/main/java/ch/puzzle/okr/controller/ClientConfigController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/ClientConfigController.java
@@ -4,13 +4,13 @@ import ch.puzzle.okr.service.ClientConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
-@Controller
+@RestController
 @RequestMapping("/config")
 public class ClientConfigController {
 

841cf01789387a9798b90b9138756dc62a33cfc6
Christoph Gächter <gaechter@puzzle.ch>
2023-08-31 16:28:30 +0200

    use actual quarter when quarterId is not given

--- backend/src/main/java/ch/puzzle/okr/service/business/OverviewBusinessService.java
+++ backend/src/main/java/ch/puzzle/okr/service/business/OverviewBusinessService.java
@@ -3,21 +3,30 @@ package ch.puzzle.okr.service.business;
 import ch.puzzle.okr.models.Overview;
 import ch.puzzle.okr.service.persistence.OverviewPersistenceService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class OverviewBusinessService {
 
     private final OverviewPersistenceService overviewPersistenceService;
+    private final QuarterBusinessService quarterBusinessService;
 
-    public OverviewBusinessService(OverviewPersistenceService overviewPersistenceService) {
+    public OverviewBusinessService(OverviewPersistenceService overviewPersistenceService,
+            QuarterBusinessService quarterBusinessService) {
         this.overviewPersistenceService = overviewPersistenceService;
+        this.quarterBusinessService = quarterBusinessService;
     }
 
     public List<Overview> getOverviewByQuarterIdAndTeamIds(Long quarterId, List<Long> teamIds) {
-        // TODO get current quarter if quarterId is not set
-        // TODO get current team (of current user) if teamIds is empty
+        if (Objects.isNull(quarterId)) {
+            quarterId = quarterBusinessService.getActiveQuarter().getId();
+        }
+        if (CollectionUtils.isEmpty(teamIds)) {
+            // TODO get current team (of current user) if teamIds is empty
+        }
         return overviewPersistenceService.getOverviewByQuarterIdAndTeamIds(quarterId, teamIds);
     }
 }

68575b7fccfe565e2426a64f79aff85dfb24fb82
Jannik Pulfer <pulfer@puzzle.ch>
2023-09-01 12:51:31 +0200

    Create keyresult component and format

---
+++ frontend/src/app/keyresult/keyresult.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KeyresultComponent } from './keyresult.component';
+
+describe('KeyresultComponent', () => {
+  let component: KeyresultComponent;
+  let fixture: ComponentFixture<KeyresultComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [KeyresultComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(KeyresultComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/keyresult/keyresult.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyresult',
+  templateUrl: './keyresult.component.html',
+  styleUrls: ['./keyresult.component.scss'],
+})
+export class KeyresultComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

723af73fb1d63d3011eb82147a708f310cc06272
megli2 <egli@puzzle.ch>
2023-09-01 13:28:55 +0200

    implement height of application-header

--- frontend/src/app/application-header/application-header.component.ts
+++ frontend/src/app/application-header/application-header.component.ts
@@ -7,17 +7,26 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class ApplicationHeaderComponent implements OnInit {
   lastScrollTop: number = 0;
+  @ViewChild('okrTopbar')
+  okrTopbar: ElementRef | undefined;
+  @ViewChild('okrBanner')
+  okrBanner: ElementRef | undefined;
 
   constructor() {}
 
   ngOnInit(): void {
+    let comp = document.getElementById('componentDiv');
+    comp!.style.height = '208px';
     window.addEventListener('scroll', this.changeHeaderAppearance);
   }
 
   changeHeaderAppearance() {
     let scrollTop: number = window.scrollY || document.documentElement.scrollTop;
     let navbar = document.getElementById('okrBanner');
+    let comp = document.getElementById('componentDiv');
     if (navbar != null) {
+      comp!.style.height = scrollTop > this.lastScrollTop ? '48px' : '208px';
+
       navbar.style.top = scrollTop > this.lastScrollTop ? '-220px' : '48px';
     }
     this.lastScrollTop = scrollTop;

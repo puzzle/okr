2791f7f286ec9cc473388696f627a9dc4c91b963
Yanick Minder <minder@puzzle.ch>
2023-09-01 13:01:55 +0200

    solve problems related to rebase

--- frontend/src/app/model/ObjectiveMin.ts
+++ frontend/src/app/model/ObjectiveMin.ts
@@ -1,10 +1,11 @@
-import { Quarter } from '../release-1/shared/services/quarter.service';
 import { KeyresultMin } from './KeyresultMin';
+import { QuarterMin } from './QuarterMin';
+import { State } from '../shared/types/enums/State';
 
 export interface ObjectiveMin {
   id: number;
   title: string;
-  state: string;
-  quarter: Quarter;
+  state: State;
+  quarter: QuarterMin;
   keyresults: KeyresultMin[];
 }

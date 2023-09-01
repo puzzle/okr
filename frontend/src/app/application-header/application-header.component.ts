295ccd88c073ea025b724b3bbcb8f76c50f06eae
megli2 <egli@puzzle.ch>
2023-09-01 13:52:22 +0200

    clean up code to be ready for first review

--- frontend/src/app/application-header/application-header.component.ts
+++ frontend/src/app/application-header/application-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-application-header',
@@ -7,28 +7,17 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class ApplicationHeaderComponent implements OnInit {
   lastScrollTop: number = 0;
-  @ViewChild('okrTopbar')
-  okrTopbar: ElementRef | undefined;
-  @ViewChild('okrBanner')
-  okrBanner: ElementRef | undefined;
 
   constructor() {}
 
   ngOnInit(): void {
-    let comp = document.getElementById('componentDiv');
-    comp!.style.height = '208px';
     window.addEventListener('scroll', this.changeHeaderAppearance);
   }
 
   changeHeaderAppearance() {
     let scrollTop: number = window.scrollY || document.documentElement.scrollTop;
     let navbar = document.getElementById('okrBanner');
-    let comp = document.getElementById('componentDiv');
-    if (navbar != null) {
-      comp!.style.height = scrollTop > this.lastScrollTop ? '48px' : '208px';
-
-      navbar.style.top = scrollTop > this.lastScrollTop ? '-220px' : '48px';
-    }
+    navbar!.style.top = scrollTop > this.lastScrollTop ? '-220px' : '48px';
     this.lastScrollTop = scrollTop;
   }
 }

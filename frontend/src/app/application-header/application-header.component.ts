c6b478ce37a82b96e2e8369f48cde1e6f37d684e
megli2 <egli@puzzle.ch>
2023-09-01 11:15:18 +0200

    implement new behaviour of okr header

--- frontend/src/app/application-header/application-header.component.ts
+++ frontend/src/app/application-header/application-header.component.ts
@@ -6,12 +6,29 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./application-header.component.scss'],
 })
 export class ApplicationHeaderComponent implements OnInit {
-  @ViewChild('okrTopbar')
-  okrTopbar: ElementRef | undefined;
   @ViewChild('okrBanner')
   okrBanner: ElementRef | undefined;
+  @ViewChild('okrTopbar')
+  okrTopbar: ElementRef | undefined;
+  lastScrollTop: number = 0;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let navbar = document.getElementById('okrBanner');
+    window.addEventListener('scroll', this.scrollFunction);
+  }
+
+  scrollFunction() {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
+    let navbar = document.getElementById('okrBanner');
+    if (navbar != null) {
+      if (scrollTop > this.lastScrollTop) {
+        navbar.style.top = '-220px';
+      } else {
+        navbar.style.top = '48px';
+      }
+    }
+    this.lastScrollTop = scrollTop;
+  }
 }

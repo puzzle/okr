027e0d9960b9cdab84c280f12bd0cec61b6a8fbb
Jannik Pulfer <pulfer@puzzle.ch>
2023-09-01 10:50:00 +0200

    Remove unused import of MatCardModule in objective.module.ts

--- frontend/src/app/objective/objective.module.ts
+++ frontend/src/app/objective/objective.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { ObjectiveColumnComponent } from './objectiveColumn/objective-column.component';
 
 @NgModule({
   declarations: [ObjectiveColumnComponent],
-  imports: [CommonModule, MatCardModule, MatMenuModule, MatButtonModule],
+  imports: [CommonModule, MatMenuModule, MatButtonModule],
 })
 export class ObjectiveModule {}

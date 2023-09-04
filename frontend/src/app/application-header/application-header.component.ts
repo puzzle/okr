7ca760c4ac7b1fff29560fdafd9463790d9a7395
megli2 <egli@puzzle.ch>
2023-09-04 12:52:01 +0200

    handle visibility over resize observer

--- frontend/src/app/application-header/application-header.component.ts
+++ frontend/src/app/application-header/application-header.component.ts
@@ -1,23 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-application-header',
   templateUrl: './application-header.component.html',
   styleUrls: ['./application-header.component.scss'],
 })
-export class ApplicationHeaderComponent implements OnInit {
-  lastScrollTop: number = 0;
+export class ApplicationHeaderComponent implements OnInit, AfterViewInit, OnDestroy {
+  lastScrollPosition: number = 0;
+  PUZZLE_TOP_BAR_HEIGHT: number = 48;
+  puzzleBanner: HTMLElement | null = null;
+  eventListener: EventListener | null = null;
+
+  resizeObserver: ResizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    this.removeScrollEventListener(this.puzzleBanner!, this.puzzleBanner!.offsetHeight);
+    this.setScrollEventListener(this.puzzleBanner!, entries[0].contentRect.height);
+  });
 
   constructor() {}
 
-  ngOnInit(): void {
-    window.addEventListener('scroll', this.changeHeaderAppearance);
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    //Define puzzleBanner reference and add resizeObserver
+    this.puzzleBanner = document.getElementById('okrBanner')!;
+    this.resizeObserver.observe(this.puzzleBanner);
   }
 
-  changeHeaderAppearance() {
+  ngOnDestroy(): void {
+    this.removeScrollEventListener(this.puzzleBanner!, this.puzzleBanner!.offsetHeight);
+  }
+
+  changeHeaderAppearance(banner: HTMLElement, bannerHeight: number) {
+    //Move Banner to negative Top if scrolled down, move to visible top if scrolled up
     let scrollTop: number = window.scrollY || document.documentElement.scrollTop;
-    let navbar = document.getElementById('okrBanner');
-    navbar!.style.top = scrollTop > this.lastScrollTop ? '-220px' : '48px';
-    this.lastScrollTop = scrollTop;
+    banner!.style.top =
+      scrollTop > this.lastScrollPosition
+        ? '-' + (bannerHeight + this.PUZZLE_TOP_BAR_HEIGHT) + 'px'
+        : this.PUZZLE_TOP_BAR_HEIGHT + 'px';
+    this.lastScrollPosition = scrollTop;
+  }
+
+  setScrollEventListener(banner: HTMLElement, bannerHeight: number) {
+    this.eventListener = () => this.changeHeaderAppearance(banner, bannerHeight);
+    window.addEventListener('scroll', this.eventListener);
+  }
+
+  removeScrollEventListener(banner: HTMLElement, bannerHeight: number) {
+    window.removeEventListener('scroll', this.eventListener!);
   }
 }

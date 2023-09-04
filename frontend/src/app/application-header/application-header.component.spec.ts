db1b96f444e54ccc61e453c30941fca089067de6
megli2 <egli@puzzle.ch>
2023-09-04 13:45:33 +0200

    fix application-header test by bypassing mock class to window

--- frontend/src/app/application-header/application-header.component.spec.ts
+++ frontend/src/app/application-header/application-header.component.spec.ts
@@ -2,7 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplicationHeaderComponent } from './application-header.component';
 
+class ResizeObserverMock {
+  observe() {}
+
+  unobserve() {}
+}
+
 describe('ApplicationHeaderComponent', () => {
+  //@ts-ignore
+  global.ResizeObserver = ResizeObserverMock;
   let component: ApplicationHeaderComponent;
   let fixture: ComponentFixture<ApplicationHeaderComponent>;
 

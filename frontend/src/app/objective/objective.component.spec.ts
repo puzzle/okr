e645f474b142ec576893ae5ae9cd43964ef617f4
Yanick Minder <minder@puzzle.ch>
2023-09-05 08:54:53 +0200

    remove objectiveWrapper and harness

--- frontend/src/app/objective/objective.component.spec.ts
+++ frontend/src/app/objective/objective.component.spec.ts
@@ -10,43 +10,29 @@ import { State } from '../shared/types/enums/State';
 import { RouterTestingModule } from '@angular/router/testing';
 import { OverviewService } from '../shared/services/overview.service';
 import { objective } from '../shared/testData';
-import { ObjectiveMin } from '../shared/types/model/ObjectiveMin';
-import { Component } from '@angular/core';
-import { ObjectiveHarness } from '../shared/harness/objective-harness';
-
-@Component({
-  selector: 'app-team-wrapper',
-  template: "<app-objective-column [objective]='objective'> </app-objective-column>",
-})
-// @ts-ignore
-export class ObjectiveWrapper {
-  objective!: ObjectiveMin;
-}
+import { MatMenuHarness } from '@angular/material/menu/testing';
 
 const overviewServiceMock = {
   getObjectiveWithKeyresults: jest.fn(),
 };
 describe('ObjectiveColumnComponent', () => {
-  let component: ObjectiveWrapper;
-  let fixture: ComponentFixture<ObjectiveWrapper>;
+  let component: ObjectiveComponent;
+  let fixture: ComponentFixture<ObjectiveComponent>;
   let loader: HarnessLoader;
-  let harness: ObjectiveHarness;
   beforeEach(async () => {
     overviewServiceMock.getObjectiveWithKeyresults.mockReset();
 
     await TestBed.configureTestingModule({
-      declarations: [ObjectiveComponent, ObjectiveWrapper],
+      declarations: [ObjectiveComponent],
       imports: [MatMenuModule, MatCardModule, NoopAnimationsModule, RouterTestingModule],
       providers: [{ provide: OverviewService, useValue: overviewServiceMock }],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ObjectiveWrapper);
+    fixture = TestBed.createComponent(ObjectiveComponent);
     component = fixture.componentInstance;
 
     loader = TestbedHarnessEnvironment.loader(fixture);
     component.objective = objective;
-    harness = await loader.getHarness(ObjectiveHarness);
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -54,11 +40,14 @@ describe('ObjectiveColumnComponent', () => {
   });
 
   test('Mat-menu should open and close', async () => {
-    expect(await harness.isMenuOpen()).toBe(false);
-    await harness.setMenuState(true);
-    expect(await harness.isMenuOpen()).toBe(true);
-    await harness.setMenuState(false);
-    expect(await harness.isMenuOpen()).toBe(false);
+    fixture.detectChanges();
+
+    const menu = await loader.getHarness(MatMenuHarness.with({ selector: '.three-dot-menu' }));
+    expect(await menu.isOpen()).toBeFalsy();
+    await menu.open();
+    expect(await menu.isOpen()).toBeTruthy();
+    await menu.close();
+    expect(await menu.isOpen()).toBeFalsy();
   });
 
   test.each([

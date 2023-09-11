c043390a49765cd3f477a828fafcd7feecbd91bb
Lias Kleisa <kleisa@puzzle.ch>
2023-09-11 14:18:18 +0200

    Fix OverviewPersistenceService tests

--- backend/src/test/java/ch/puzzle/okr/service/persistence/OverviewPersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/persistence/OverviewPersistenceServiceIT.java
@@ -22,11 +22,10 @@ public class OverviewPersistenceServiceIT {
     private OverviewMapper overviewMapper;
 
     @Test
-    @Disabled
     void getOverviewByQuarterIdAndTeamIds_ShouldReturnOverviews() {
         List<Overview> overviews = overviewPersistenceService.getOverviewByQuarterIdAndTeamIds(2L, List.of(5L, 6L, 8L));
 
-        assertEquals(15, overviews.size());
+        assertEquals(13, overviews.size());
         overviews.forEach(overview -> assertTrue(matchOverviewId(overview.getOverviewId())));
     }
 
@@ -36,7 +35,6 @@ public class OverviewPersistenceServiceIT {
 
     private static Stream<OverviewId> getExpectedOverviewIds() {
         return Stream.of(OverviewId.of(5L, 3L, 3L, 9L), //
-                OverviewId.of(5L, 3L, 3L, 9L), //
                 OverviewId.of(5L, 3L, 4L, 8L), //
                 OverviewId.of(5L, 3L, 5L, 7L), //
                 OverviewId.of(5L, 4L, 6L, 5L), //
@@ -45,6 +43,7 @@ public class OverviewPersistenceServiceIT {
                 OverviewId.of(6L, 8L, 18L, 20L), //
                 OverviewId.of(6L, 8L, 19L, 19L), //
                 OverviewId.of(6L, 9L, 15L, 18L), //
+                OverviewId.of(6L, 9L, 16L, 13L), //
                 OverviewId.of(6L, 9L, 16L, 17L), //
                 OverviewId.of(6L, 9L, 17L, 16L), //
                 OverviewId.of(6L, 10L, -1L, -1L));

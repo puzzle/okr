da84238ea97b542cbaaca8fecc3ff7e0699fc1dd
megli2 <egli@puzzle.ch>
2023-09-11 12:48:50 +0200

    fix compilation errors in CheckInControllerIT

--- backend/src/test/java/ch/puzzle/okr/controller/CheckInControllerIT.java
+++ backend/src/test/java/ch/puzzle/okr/controller/CheckInControllerIT.java
@@ -1,14 +1,17 @@
 package ch.puzzle.okr.controller;
 
 import ch.puzzle.okr.dto.checkIn.CheckInDto;
-import ch.puzzle.okr.mapper.MeasureMapper;
-import ch.puzzle.okr.models.Measure;
+import ch.puzzle.okr.dto.checkIn.CheckInMetricDto;
+import ch.puzzle.okr.mapper.checkIn.CheckInMapper;
 import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.models.User;
-import ch.puzzle.okr.service.business.CheckInBusinessService;
+import ch.puzzle.okr.models.checkIn.CheckIn;
+import ch.puzzle.okr.models.checkIn.CheckInMetric;
+import ch.puzzle.okr.models.checkIn.CheckInOrdinal;
+import ch.puzzle.okr.models.checkIn.Zone;
 import ch.puzzle.okr.models.keyresult.KeyResultMetric;
 import ch.puzzle.okr.models.keyresult.KeyResultOrdinal;
-import ch.puzzle.okr.service.business.MeasureBusinessService;
+import ch.puzzle.okr.service.business.CheckInBusinessService;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +29,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
@@ -42,23 +44,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CheckInController.class)
 class CheckInControllerIT {
     static Objective objective = Objective.Builder.builder().withId(1L).build();
-    static Measure measure = Measure.Builder.builder().withId(5L)
+    static CheckIn checkIn = CheckInMetric.Builder.builder().withValue(30D).withCheckInType("metric").withConfidence(5)
+            .withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
             .withCreatedBy(User.Builder.builder().withId(1L).withFirstname("Frank").build())
-            .withCreatedOn(LocalDateTime.MAX)
             .withKeyResult(KeyResultMetric.Builder.builder().withBaseline(3.0).withStretchGoal(6.0).withId(8L)
                     .withObjective(objective).build())
-            .withValue(30D).withChangeInfo("ChangeInfo").withInitiatives("Initiatives").build();
-    static Measure anotherMeasure = Measure.Builder.builder().withId(4L)
+            .build();
+    static CheckIn anotherCheckIn = CheckInOrdinal.Builder.builder().withValue(Zone.COMMIT).withId(4L)
             .withCreatedBy(User.Builder.builder().withId(2L).withFirstname("Robert").build())
             .withCreatedOn(LocalDateTime.MAX)
             .withKeyResult(
                     KeyResultOrdinal.Builder.builder().withCommitZone("Baum").withTargetZone("Wald").withId(9L).build())
-            .withValue(35D).withChangeInfo("ChangeInfo").build();
-    static List<Measure> measureList = Arrays.asList(measure, anotherMeasure);
-    static CheckInDto checkInDto = new CheckIn(5L, 8L, 30D, "changeInfo", "Initiatives", 1L, LocalDateTime.MAX,
-            Instant.parse("2022-08-12T01:01:00.00Z"));
-    static CheckInDto anotherCheckInDto = new CheckInDto(4L, 9L, 35D, "changeInfo", "Initiatives", 2L,
-            LocalDateTime.MAX, Instant.parse("2022-08-12T01:01:00.00Z"));
+            .withChangeInfo("ChangeInfo").build();
+    static List<CheckIn> checkInList = Arrays.asList(checkIn, anotherCheckIn);
+
+    static CheckInDto checkInDto = new CheckInMetricDto(5L, "Changeinfo1", "Initiatives1", 6,
+            KeyResultMetric.Builder.builder().withBaseline(3.0).withStretchGoal(6.0).withId(8L).withObjective(objective)
+                    .build(),
+            User.Builder.builder().withId(1L).withFirstname("Frank").build(), LocalDateTime.MAX, LocalDateTime.MAX,
+            "metric", 46D);
+    static CheckInDto anotherCheckInDto = new CheckInMetricDto(4L, "Changeinfo2", "Initiatives2", 5,
+            KeyResultMetric.Builder.builder().withBaseline(3.0).withStretchGoal(6.0).withId(8L).withObjective(objective)
+                    .build(),
+            User.Builder.builder().withId(1L).withFirstname("Frank").build(), LocalDateTime.MAX, LocalDateTime.MAX,
+            "metric", 30D);
     @Autowired
     private MockMvc mvc;
     @MockBean
@@ -68,21 +77,24 @@ class CheckInControllerIT {
 
     @BeforeEach
     void setUp() {
-        BDDMockito.given(checkInMapper.toDto(measure)).willReturn(checkInDto);
-        BDDMockito.given(checkInMapper.toDto(anotherMeasure)).willReturn(anotherCheckInDto);
+        BDDMockito.given(checkInMapper.toDto(checkIn)).willReturn(checkInDto);
+        BDDMockito.given(checkInMapper.toDto(anotherCheckIn)).willReturn(anotherCheckInDto);
     }
 
     @Test
-    void shouldReturnMeasureWhenCreatingNewMeasure() throws Exception {
-        CheckInDto testMeasure = new CheckInDto(5L, 5L, 30D, "changeInfo", "initiatives", 1L, LocalDateTime.now(),
-                Instant.parse("2022-08-12T01:01:00.00Z"));
+    void shouldReturnCheckInWhenCreatingNewCheckIn() throws Exception {
+        CheckInDto testCheckIn = new CheckInMetricDto(5L, "changeInfo", "initiatives", 6,
+                KeyResultMetric.Builder.builder().withBaseline(3.0).withStretchGoal(6.0).withId(8L)
+                        .withObjective(objective).build(),
+                User.Builder.builder().withId(1L).withFirstname("Frank").build(), LocalDateTime.MAX, LocalDateTime.MAX,
+                "metric", 30D);
 
-        BDDMockito.given(checkInBusinessService.saveMeasure(any(), any())).willReturn(measure);
-        BDDMockito.given(checkInMapper.toDto(any())).willReturn(testMeasure);
-        BDDMockito.given(checkInMapper.toMeasure(any())).willReturn(measure);
+        BDDMockito.given(checkInBusinessService.saveCheckIn(any(), any())).willReturn(checkIn);
+        BDDMockito.given(checkInMapper.toDto(any())).willReturn(testCheckIn);
+        BDDMockito.given(checkInMapper.toCheckIn(any())).willReturn(checkIn);
 
         mvc.perform(post("/api/v2/checkIns").contentType(MediaType.APPLICATION_JSON).content(
-                "{\"keyResultId\": 5 , \"value\": 30, \"changeInfo\": \"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": 1, \"measureDate \": \"2022-08-12T01:01:00\"}")
+                "{\"keyResultId\": 5 , \"value\": 30, \"changeInfo\": \"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": 1, \"checkInDate \": \"2022-08-12T01:01:00\"}")
                 .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful()).andExpect(jsonPath("$.id", Is.is(5)))
                 .andExpect(jsonPath("$.keyResultId", Is.is(5))).andExpect(jsonPath("$.value", Is.is(30D)))
@@ -90,39 +102,42 @@ class CheckInControllerIT {
     }
 
     @Test
-    void shouldReturnResponseStatusExceptionWhenCreatingMeasureNullName() throws Exception {
-        BDDMockito.given(checkInBusinessService.saveMeasure(any(), any()))
+    void shouldReturnResponseStatusExceptionWhenCreatingCheckInNullName() throws Exception {
+        BDDMockito.given(checkInBusinessService.saveCheckIn(any(), any()))
                 .willThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST, "The given user is null"));
 
         mvc.perform(post("/api/v2/checkIns").contentType(MediaType.APPLICATION_JSON).content(
-                "{\"keyResultId\": 5 , \"value\": 30, \"changeInfo\": \"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": null, \"measureDate \": null}")
+                "{\"keyResultId\": 5 , \"value\": 30, \"changeInfo\": \"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": null, \"checkInDate \": null}")
                 .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
     @Test
-    void shouldReturnCorrectMeasure() throws Exception {
-        CheckInDto testMeasure = new CheckInDto(5L, 5L, 30D, "changeInfo", "initiatives", 1L, LocalDateTime.now(),
-                Instant.parse("2022-08-12T01:01:00.00Z"));
-        BDDMockito.given(checkInBusinessService.updateMeasure(anyLong(), any(), any())).willReturn(measure);
-        BDDMockito.given(checkInMapper.toDto(any())).willReturn(testMeasure);
-        BDDMockito.given(checkInMapper.toMeasure(any())).willReturn(measure);
+    void shouldReturnCorrectCheckIn() throws Exception {
+        CheckInDto testCheckIn = new CheckInMetricDto(5L, "changeInfo", "initiatives", 6,
+                KeyResultMetric.Builder.builder().withBaseline(3.0).withStretchGoal(6.0).withId(8L)
+                        .withObjective(objective).build(),
+                User.Builder.builder().withId(1L).withFirstname("Frank").build(), LocalDateTime.MAX, LocalDateTime.MAX,
+                "metric", 30D);
+        BDDMockito.given(checkInBusinessService.updateCheckIn(anyLong(), any(), any())).willReturn(checkIn);
+        BDDMockito.given(checkInMapper.toDto(any())).willReturn(testCheckIn);
+        BDDMockito.given(checkInMapper.toCheckIn(any())).willReturn(checkIn);
 
         mvc.perform(put("/api/v2/checkIns/1").contentType(MediaType.APPLICATION_JSON)
                 .with(SecurityMockMvcRequestPostProcessors.csrf())
                 .content("{\"keyResultId\": 1, \"value\": 30, \"changeInfo\": "
-                        + "\"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": null, \"measureDate \": null}"))
+                        + "\"changeInfo\", \"initiatives \": \"initiatives\", \"createdById \": null, \"checkInDate \": null}"))
                 .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$.value", Is.is(30D)))
                 .andExpect(jsonPath("$.createdById", Is.is(1)))
-                .andExpect(jsonPath("$.measureDate", Is.is("2022-08-12T01:01:00Z")))
+                .andExpect(jsonPath("$.checkInDate", Is.is("2022-08-12T01:01:00Z")))
                 .andExpect(jsonPath("$.initiatives", Is.is("initiatives")));
     }
 
     @Test
     void shouldReturnNotFound() throws Exception {
-        BDDMockito.given(checkInBusinessService.updateMeasure(anyLong(), any(), any()))
+        BDDMockito.given(checkInBusinessService.updateCheckIn(anyLong(), any(), any()))
                 .willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND));
-        mvc.perform(put("/api/v1/measures/3").contentType(MediaType.APPLICATION_JSON)
+        mvc.perform(put("/api/v1/checkIns/3").contentType(MediaType.APPLICATION_JSON)
                 .with(SecurityMockMvcRequestPostProcessors.csrf())
                 .content("{\"keyResultId\": 5 , \"value\": 30, \"changeInfo\": "
                         + "\"changeInfo\", \"initiatives \": \"initiatives\", " + "\"createdById \": null}"))
@@ -131,7 +146,7 @@ class CheckInControllerIT {
 
     @Test
     void shouldReturnBadRequest() throws Exception {
-        BDDMockito.given(checkInBusinessService.updateMeasure(anyLong(), any(), any()))
+        BDDMockito.given(checkInBusinessService.updateCheckIn(anyLong(), any(), any()))
                 .willThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST));
         mvc.perform(put("/api/v2/checkIns/3").contentType(MediaType.APPLICATION_JSON)
                 .with(SecurityMockMvcRequestPostProcessors.csrf())

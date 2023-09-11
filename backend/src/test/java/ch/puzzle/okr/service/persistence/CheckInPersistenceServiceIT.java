b96b2480aac7e97f6f3f5cc364d1f2f070ef1ca4
megli2 <egli@puzzle.ch>
2023-09-11 12:27:16 +0200

    fix compilation errors in checkInPersistenceServiceIT

--- backend/src/test/java/ch/puzzle/okr/service/persistence/CheckInPersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/persistence/CheckInPersistenceServiceIT.java
@@ -1,8 +1,9 @@
 package ch.puzzle.okr.service.persistence;
 
-import ch.puzzle.okr.models.Measure;
 import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.models.User;
+import ch.puzzle.okr.models.checkIn.CheckIn;
+import ch.puzzle.okr.models.checkIn.CheckInMetric;
 import ch.puzzle.okr.models.keyresult.KeyResultMetric;
 import ch.puzzle.okr.test.SpringIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,124 +19,117 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringIntegrationTest
 public class CheckInPersistenceServiceIT {
-    Measure createdMeasure;
+    CheckIn createdCheckIn;
     @Autowired
     private CheckInPersistenceService checkInPersistenceService;
 
-    private static Measure createMeasure(Long id) {
-        return Measure.Builder.builder().withId(id)
+    private static CheckIn createCheckIn(Long id) {
+        return CheckInMetric.Builder.builder().withValue(30D).withId(id)
                 .withCreatedBy(User.Builder.builder().withId(1L).withFirstname("Frank").build())
                 .withCreatedOn(LocalDateTime.MAX)
                 .withKeyResult(KeyResultMetric.Builder.builder().withBaseline(1.0).withStretchGoal(13.0).withId(8L)
                         .withObjective(Objective.Builder.builder().withId(1L).build()).build())
-                .withValue(30D).withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
-                .withMeasureDate(Instant.parse("2021-11-03T00:00:00.00Z")).build();
+                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives").withModifiedOn(LocalDateTime.MAX)
+                .withConfidence(5).withCheckInType("metric").build();
     }
 
     @AfterEach
     void tearDown() {
         try {
-            if (createdMeasure != null) {
-                checkInPersistenceService.findById(createdMeasure.getId());
-                checkInPersistenceService.deleteById(createdMeasure.getId());
+            if (createdCheckIn != null) {
+                checkInPersistenceService.findById(createdCheckIn.getId());
+                checkInPersistenceService.deleteById(createdCheckIn.getId());
             }
         } catch (ResponseStatusException ex) {
-            // created measure already deleted
+            // created CheckIn already deleted
         } finally {
-            createdMeasure = null;
+            createdCheckIn = null;
         }
     }
 
     @Test
-    void saveMeasure_ShouldSaveNewMeasure() {
-        Measure measure = createMeasure(null);
+    void saveCheckIn_ShouldSaveNewCheckIn() {
+        CheckIn checkIn = createCheckIn(null);
 
-        createdMeasure = checkInPersistenceService.save(measure);
+        createdCheckIn = checkInPersistenceService.save(checkIn);
 
-        assertNotNull(createdMeasure.getId());
-        assertEquals(measure.getMeasureDate(), createdMeasure.getMeasureDate());
-        assertEquals(measure.getValue(), createdMeasure.getValue());
-        assertEquals(measure.getCreatedBy(), createdMeasure.getCreatedBy());
-        assertEquals(measure.getCreatedOn(), createdMeasure.getCreatedOn());
-        assertEquals(measure.getInitiatives(), createdMeasure.getInitiatives());
-        assertEquals(measure.getChangeInfo(), createdMeasure.getChangeInfo());
+        assertNotNull(createdCheckIn.getId());
+        assertEquals(checkIn.getModifiedOn(), createdCheckIn.getModifiedOn());
+        assertEquals(((CheckInMetric) checkIn).getValue(), ((CheckInMetric) createdCheckIn).getValue());
+        assertEquals(checkIn.getCreatedBy(), createdCheckIn.getCreatedBy());
+        assertEquals(checkIn.getCreatedOn(), createdCheckIn.getCreatedOn());
+        assertEquals(checkIn.getInitiatives(), createdCheckIn.getInitiatives());
+        assertEquals(checkIn.getChangeInfo(), createdCheckIn.getChangeInfo());
     }
 
     @Test
     void updateKeyResult_ShouldUpdateKeyResult() {
-        Measure measure = createMeasure(null);
-        createdMeasure = checkInPersistenceService.save(measure);
-        createdMeasure.setChangeInfo("Updated Measure");
+        CheckIn CheckIn = createCheckIn(null);
+        createdCheckIn = checkInPersistenceService.save(CheckIn);
+        createdCheckIn.setChangeInfo("Updated CheckIn");
 
-        Measure updateMeasure = checkInPersistenceService.save(createdMeasure);
+        CheckIn updateCheckIn = checkInPersistenceService.save(createdCheckIn);
 
-        assertEquals(createdMeasure.getId(), updateMeasure.getId());
-        assertEquals("Updated Measure", updateMeasure.getChangeInfo());
+        assertEquals(createdCheckIn.getId(), updateCheckIn.getId());
+        assertEquals("Updated CheckIn", updateCheckIn.getChangeInfo());
     }
 
     @Test
     void updateKeyResult_ShouldThrowExceptionWhenKeyResultNotFound() {
-        Measure measure = createMeasure(321L);
+        CheckIn CheckIn = createCheckIn(321L);
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> checkInPersistenceService.save(measure));
+                () -> checkInPersistenceService.save(CheckIn));
 
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
-        assertEquals("Measure with id 321 not found", exception.getReason());
+        assertEquals("CheckIn with id 321 not found", exception.getReason());
     }
 
     @Test
-    void getAllMeasure_ShouldReturnListOfAllMeasures() {
-        List<Measure> measures = checkInPersistenceService.findAll();
+    void getAllCheckIn_ShouldReturnListOfAllCheckIns() {
+        List<CheckIn> CheckIns = checkInPersistenceService.findAll();
 
-        assertEquals(20, measures.size());
+        assertEquals(20, CheckIns.size());
     }
 
     @Test
-    void getMeasureById_ShouldReturnMeasureProperly() {
-        Measure measure = checkInPersistenceService.findById(20L);
+    void getCheckInById_ShouldReturnCheckInProperly() {
+        CheckIn checkIn = checkInPersistenceService.findById(20L);
 
-        assertEquals(20L, measure.getId());
-        assertEquals(0.5, measure.getValue(), 0.01);
+        assertEquals(20L, checkIn.getId());
+        assertEquals(0.5, ((CheckInMetric) checkIn).getValue(), 0.01);
         assertEquals(
                 "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores ",
-                measure.getChangeInfo());
+                checkIn.getChangeInfo());
     }
 
     @Test
-    void getMeasureById_ShouldThrowExceptionWhenMeasureNotFound() {
+    void getCheckInById_ShouldThrowExceptionWhenCheckInNotFound() {
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
                 () -> checkInPersistenceService.findById(321L));
 
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
-        assertEquals("Measure with id 321 not found", exception.getReason());
+        assertEquals("CheckIn with id 321 not found", exception.getReason());
     }
 
     @Test
-    void getMeasuretById_ShouldThrowExceptionWhenMeasureIdIsNull() {
+    void getCheckIntById_ShouldThrowExceptionWhenCheckInIdIsNull() {
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
                 () -> checkInPersistenceService.findById(null));
 
         assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
-        assertEquals("Missing attribute measure id", exception.getReason());
+        assertEquals("Missing attribute CheckIn id", exception.getReason());
     }
 
     @Test
-    void getMeasuresByKeyResultIdAndMeasureDate_ShouldReturnMeasuresProperly() {
-        List<Measure> measures = checkInPersistenceService.getMeasuresByKeyResultIdAndMeasureDate(8L,
-                Instant.parse("2023-07-24T20:00:00.00Z"));
-        assertEquals(1, measures.size());
-    }
-
-    @Test
-    void deleteMeasureById_ShouldDeleteMeasure() {
-        Measure measure = createMeasure(null);
-        createdMeasure = checkInPersistenceService.save(measure);
-        checkInPersistenceService.deleteById(createdMeasure.getId());
+    void deleteCheckInById_ShouldDeleteCheckIn() {
+        CheckIn CheckIn = createCheckIn(null);
+        createdCheckIn = checkInPersistenceService.save(CheckIn);
+        checkInPersistenceService.deleteById(createdCheckIn.getId());
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> checkInPersistenceService.findById(createdMeasure.getId()));
+                () -> checkInPersistenceService.findById(createdCheckIn.getId()));
         assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
-        assertEquals(String.format("Measure with id %d not found", createdMeasure.getId()), exception.getReason());
+        assertEquals(String.format("CheckIn with id %d not found", createdCheckIn.getId()), exception.getReason());
     }
 }

8f1a8ff64bb1c3e222e2e82c6674eae4c2358f70
megli2 <egli@puzzle.ch>
2023-09-11 12:12:32 +0200

    fix compilation errors of CheckInBusinessServiceTest.java

--- backend/src/test/java/ch/puzzle/okr/service/business/CheckInBusinessServiceTest.java
+++ backend/src/test/java/ch/puzzle/okr/service/business/CheckInBusinessServiceTest.java
@@ -1,7 +1,8 @@
 package ch.puzzle.okr.service.business;
 
-import ch.puzzle.okr.models.Measure;
 import ch.puzzle.okr.models.User;
+import ch.puzzle.okr.models.checkIn.CheckIn;
+import ch.puzzle.okr.models.checkIn.CheckInMetric;
 import ch.puzzle.okr.models.keyresult.KeyResult;
 import ch.puzzle.okr.models.keyresult.KeyResultOrdinal;
 import ch.puzzle.okr.service.persistence.CheckInPersistenceService;
@@ -14,10 +15,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,15 +33,13 @@ class CheckInBusinessServiceTest {
     CheckInValidationService validator = Mockito.mock(CheckInValidationService.class);
     @MockBean
     UserBusinessService userBusinessService = Mockito.mock(UserBusinessService.class);
-
+    KeyResult ordinalKeyResult;
     @InjectMocks
     private CheckInBusinessService checkInBusinessService;
-
-    private Measure measure;
-    private Measure measureWithId;
-    private List<Measure> measures = new ArrayList<>();
-    private Measure falseMeasure;
-    KeyResult ordinalKeyResult;
+    private CheckIn checkIn;
+    private CheckIn checkInWithId;
+    private List<CheckIn> checkIns = new ArrayList<>();
+    private CheckIn falseCheckIn;
     private User user;
 
     @BeforeEach
@@ -49,63 +47,49 @@ class CheckInBusinessServiceTest {
         this.ordinalKeyResult = KeyResultOrdinal.Builder.builder().withCommitZone("Baum").withStretchZone("Wald")
                 .withId(7L).withTitle("Keyresult Ordinal").build();
 
-        this.measure = Measure.Builder.builder()
-                .withCreatedBy(User.Builder.builder().withId(1L).withFirstname("Frank").build())
-                .withCreatedOn(LocalDateTime.MAX).withKeyResult(this.ordinalKeyResult).withValue(30D)
-                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
-                .withMeasureDate(Instant.parse("2021-11-03T00:00:00.00Z")).build();
-        this.measureWithId = Measure.Builder.builder().withId(1L)
-                .withCreatedBy(User.Builder.builder().withId(1L).withFirstname("Frank").build())
-                .withCreatedOn(LocalDateTime.MAX).withKeyResult(this.ordinalKeyResult).withValue(30D)
-                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
-                .withMeasureDate(Instant.parse("2021-11-03T00:00:00.00Z")).build();
+        this.checkIn = CheckInMetric.Builder.builder().withValue(30D).withCheckInType("Metric").withConfidence(5)
+                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives").withCreatedBy(user)
+                .withKeyResult(ordinalKeyResult).build();
+        this.checkInWithId = this.checkIn = CheckInMetric.Builder.builder().withValue(30D).withId(1L)
+                .withCheckInType("Metric").withConfidence(5).withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
+                .withCreatedBy(user).withKeyResult(ordinalKeyResult).build();
         this.user = User.Builder.builder().withEmail("Email").withFirstname("Firstname").withLastname("Lastname")
                 .build();
-        this.measures.add(measureWithId);
+        this.checkIns.add(checkInWithId);
     }
 
     @Test
-    void shouldReturnMeasure() {
-        Mockito.when(checkInPersistenceService.save(any())).thenReturn(measure);
+    void shouldReturnCheckIn() {
+        Mockito.when(checkInPersistenceService.save(any())).thenReturn(checkIn);
         Mockito.when(userBusinessService.getUserByAuthorisationToken(any())).thenReturn(user);
 
-        Measure returnMeasure = this.checkInBusinessService.saveMeasure(measure, any());
-        assertEquals(returnMeasure.getValue(), 30);
-        assertEquals(returnMeasure.getChangeInfo(), "ChangeInfo");
-        assertEquals(returnMeasure.getInitiatives(), "Initiatives");
-        assertEquals(returnMeasure.getKeyResult().getId(), 7);
-        assertEquals(returnMeasure.getCreatedBy().getFirstname(), "Frank");
-    }
-
-    @Test
-    void shouldThrowErrorWhenInDbIsSameMeasureDateWithSameKeyResultId() {
-        Mockito.when(checkInPersistenceService.getMeasuresByKeyResultIdAndMeasureDate(any(), any()))
-                .thenReturn(measures);
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> this.checkInBusinessService.saveMeasure(measure));
-        assertEquals(400, exception.getRawStatusCode());
-        assertEquals("Only one Messung is allowed per day and Key Result!", exception.getReason());
+        CheckIn returnCheckIn = this.checkInBusinessService.saveCheckIn(checkIn, any());
+        assertEquals(((CheckInMetric) returnCheckIn).getValue(), 30D);
+        assertEquals(returnCheckIn.getChangeInfo(), "ChangeInfo");
+        assertEquals(returnCheckIn.getInitiatives(), "Initiatives");
+        assertEquals(returnCheckIn.getKeyResult().getId(), 7);
+        assertEquals(returnCheckIn.getCreatedBy().getFirstname(), "Frank");
     }
 
     @Test
     void shouldNotReturnException() {
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> checkInBusinessService.saveMeasure(falseMeasure));
+                () -> checkInBusinessService.saveCheckIn(falseCheckIn, Jwt.withTokenValue(user.toString()).build()));
         assertEquals(400, exception.getRawStatusCode());
-        assertEquals("Measure has already an Id", exception.getReason());
+        assertEquals("CheckIn has already an Id", exception.getReason());
     }
 
     @Test
     void shouldReturnCorrectEntity() {
-        Mockito.when(checkInPersistenceService.save(any())).thenReturn(measure);
+        Mockito.when(checkInPersistenceService.save(any())).thenReturn(checkIn);
         Mockito.when(userBusinessService.getUserByAuthorisationToken(any())).thenReturn(user);
 
-        Measure returnedMeasure = this.checkInBusinessService.updateMeasure(1L, measure, any());
+        CheckIn returnedCheckIn = this.checkInBusinessService.updateCheckIn(1L, checkIn, any());
 
-        assertEquals(measure.getId(), returnedMeasure.getId());
-        assertEquals(measure.getValue(), returnedMeasure.getValue());
-        assertEquals(measure.getChangeInfo(), returnedMeasure.getChangeInfo());
-        assertEquals(measure.getMeasureDate(), returnedMeasure.getMeasureDate());
+        assertEquals(checkIn.getId(), returnedCheckIn.getId());
+        assertEquals(((CheckInMetric) checkIn).getValue(), ((CheckInMetric) returnedCheckIn).getValue());
+        assertEquals(checkIn.getChangeInfo(), returnedCheckIn.getChangeInfo());
+        assertEquals(checkIn.getModifiedOn(), returnedCheckIn.getModifiedOn());
     }
 
     @Test
@@ -114,7 +98,7 @@ class CheckInBusinessServiceTest {
                 .thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST));
 
         ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> this.checkInBusinessService.updateMeasure(1L, measure, any()));
+                () -> this.checkInBusinessService.updateCheckIn(1L, checkIn, any()));
         assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
     }
 }

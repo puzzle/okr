088d20bf487adc6a619d5bbb67dd471db73cbffd
Yanick Minder <minder@puzzle.ch>
2023-09-11 12:56:06 +0200

    remove check if lastCheckIn is available

--- frontend/src/app/confidence/confidence.component.ts
+++ frontend/src/app/confidence/confidence.component.ts
@@ -1,21 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { KeyresultMin } from '../shared/types/model/KeyresultMin';
-import { CheckInMin } from '../shared/types/model/CheckInMin';
 
 @Component({
   selector: 'app-confidence',
   templateUrl: './confidence.component.html',
   styleUrls: ['./confidence.component.scss'],
 })
-export class ConfidenceComponent implements OnInit {
+export class ConfidenceComponent {
   min: number = 0;
   max: number = 10;
   value: number = 5;
   @Input() edit: boolean = true;
   @Input() keyResult!: KeyresultMin;
-  ngOnInit(): void {
-    if (this.keyResult.lastCheckIn == null) {
-      this.keyResult.lastCheckIn = { confidence: 5, value: 0, createdOn: new Date() } as CheckInMin;
-    }
-  }
 }

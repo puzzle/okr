14ddab9a627bd9ad1a6804bce1c6ff2e8c8f5683
Yanick Minder <minder@puzzle.ch>
2023-09-11 12:20:38 +0200

    use new styles root in angular.json and change imports

--- frontend/src/app/confidence/confidence.component.ts
+++ frontend/src/app/confidence/confidence.component.ts
@@ -11,7 +11,7 @@ export class ConfidenceComponent implements OnInit {
   min: number = 0;
   max: number = 10;
   value: number = 5;
-  showSlider: boolean = true;
+  @Input() showSlider: boolean = true;
   @Input() keyResult!: KeyresultMin;
 
   constructor() {}

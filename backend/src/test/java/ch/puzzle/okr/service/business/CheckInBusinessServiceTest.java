6deb8f69c224b380ca69c620c3d7cc0f593f4222
megli2 <egli@puzzle.ch>
2023-09-12 19:49:51 +0200

    implement CheckInBusinessService tests with pattern of KeyResultBusinessService tests

--- backend/src/test/java/ch/puzzle/okr/service/business/CheckInBusinessServiceTest.java
+++ backend/src/test/java/ch/puzzle/okr/service/business/CheckInBusinessServiceTest.java
@@ -1,9 +1,14 @@
 package ch.puzzle.okr.service.business;
 
+import ch.puzzle.okr.TestHelper;
+import ch.puzzle.okr.models.Unit;
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.models.checkIn.CheckIn;
 import ch.puzzle.okr.models.checkIn.CheckInMetric;
+import ch.puzzle.okr.models.checkIn.CheckInOrdinal;
+import ch.puzzle.okr.models.checkIn.Zone;
 import ch.puzzle.okr.models.keyresult.KeyResult;
+import ch.puzzle.okr.models.keyresult.KeyResultMetric;
 import ch.puzzle.okr.models.keyresult.KeyResultOrdinal;
 import ch.puzzle.okr.service.persistence.CheckInPersistenceService;
 import ch.puzzle.okr.service.validation.CheckInValidationService;
@@ -14,71 +19,155 @@ import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CheckInBusinessServiceTest {
+    TestHelper testHelper = new TestHelper();
     @MockBean
     CheckInPersistenceService checkInPersistenceService = Mockito.mock(CheckInPersistenceService.class);
     @MockBean
     CheckInValidationService validator = Mockito.mock(CheckInValidationService.class);
     @MockBean
     UserBusinessService userBusinessService = Mockito.mock(UserBusinessService.class);
-    KeyResult ordinalKeyResult;
     @InjectMocks
     private CheckInBusinessService checkInBusinessService;
-    private CheckIn checkIn;
-    private CheckIn checkInWithId;
-    private List<CheckIn> checkIns = new ArrayList<>();
-    private CheckIn falseCheckIn;
+
+    /* Test data */
+    KeyResult ordinalKeyResult;
+    KeyResult metricKeyResult;
+    private CheckIn checkInMetric;
+    private CheckIn checkInOrdinal;
     private User user;
+    private Jwt jwt;
 
     @BeforeEach
     void setUp() {
+        /* KeyResult definition */
         this.ordinalKeyResult = KeyResultOrdinal.Builder.builder().withCommitZone("Baum").withStretchZone("Wald")
                 .withId(7L).withTitle("Keyresult Ordinal").build();
+        this.metricKeyResult = KeyResultMetric.Builder.builder().withBaseline(10D).withStretchGoal(50D)
+                .withUnit(Unit.CHF.toString()).withId(8L).withTitle("Keyresult Metric").build();
 
-        this.checkIn = CheckInMetric.Builder.builder().withValue(30D).withCheckInType("Metric").withConfidence(5)
-                .withChangeInfo("ChangeInfo").withInitiatives("Initiatives").withCreatedBy(user)
-                .withKeyResult(ordinalKeyResult).build();
-        this.checkInWithId = this.checkIn = CheckInMetric.Builder.builder().withValue(30D).withId(1L)
-                .withCheckInType("Metric").withConfidence(5).withChangeInfo("ChangeInfo").withInitiatives("Initiatives")
-                .withCreatedBy(user).withKeyResult(ordinalKeyResult).build();
+        /* CheckIn definition */
+        this.checkInMetric = CheckInMetric.Builder.builder().withValue(30D).withId(1L).withCheckInType("metric")
+                .withConfidence(5).withChangeInfo("ChangeInfo1").withInitiatives("Initiatives1").withCreatedBy(user)
+                .withKeyResult(metricKeyResult).build();
+        this.checkInOrdinal = CheckInOrdinal.Builder.builder().withValue(Zone.COMMIT.toString())
+                .withCheckInType("ordinal").withConfidence(5).withChangeInfo("ChangeInfo2")
+                .withInitiatives("Initiatives2").withCreatedBy(user).withKeyResult(ordinalKeyResult).build();
+
         this.user = User.Builder.builder().withEmail("Email").withFirstname("Firstname").withLastname("Lastname")
                 .build();
-        this.checkIns.add(checkInWithId);
+        this.jwt = testHelper.mockJwtToken("johnny", "Johnny", "Appleseed", "test@test.ch");
     }
 
     @Test
-    void shouldReturnCheckIn() {
-        Mockito.when(checkInPersistenceService.save(any())).thenReturn(checkIn);
-        Mockito.when(userBusinessService.getUserByAuthorisationToken(any())).thenReturn(user);
+    void shouldGetMetricCheckIn() {
+        when(checkInPersistenceService.findById(this.checkInMetric.getId())).thenReturn(this.checkInMetric);
+        CheckIn checkIn = checkInBusinessService.getCheckInById(this.checkInMetric.getId());
 
-        CheckIn returnCheckIn = this.checkInBusinessService.saveCheckIn(checkIn, any());
-        assertEquals(((CheckInMetric) returnCheckIn).getValue(), 30D);
-        assertEquals(returnCheckIn.getChangeInfo(), "ChangeInfo");
-        assertEquals(returnCheckIn.getInitiatives(), "Initiatives");
-        assertEquals(returnCheckIn.getKeyResult().getId(), 7);
-        assertEquals(returnCheckIn.getCreatedBy().getFirstname(), "Firstname");
+        assertEquals(this.checkInMetric.getCheckInType(), checkIn.getCheckInType());
+        assertEquals(this.checkInMetric.getChangeInfo(), checkIn.getChangeInfo());
+        assertEquals(this.checkInMetric.getInitiatives(), checkIn.getInitiatives());
+        assertEquals(metricKeyResult.getId(), checkIn.getKeyResult().getId());
     }
 
     @Test
-    void shouldReturnCorrectEntity() {
-        Mockito.when(checkInPersistenceService.save(any())).thenReturn(checkIn);
-        Mockito.when(checkInPersistenceService.findById(anyLong())).thenReturn(checkIn);
-        Mockito.when(userBusinessService.getUserByAuthorisationToken(any())).thenReturn(user);
+    void shouldGetOrdinalCheckIn() {
+        when(checkInPersistenceService.findById(this.checkInOrdinal.getId())).thenReturn(this.checkInOrdinal);
+        CheckIn checkIn = checkInBusinessService.getCheckInById(this.checkInOrdinal.getId());
 
-        CheckIn returnedCheckIn = this.checkInBusinessService.updateCheckIn(1L, checkIn, any());
+        assertEquals(this.checkInOrdinal.getCheckInType(), checkIn.getCheckInType());
+        assertEquals(this.checkInOrdinal.getChangeInfo(), checkIn.getChangeInfo());
+        assertEquals(this.checkInOrdinal.getInitiatives(), checkIn.getInitiatives());
+        assertEquals(ordinalKeyResult.getId(), checkIn.getKeyResult().getId());
+    }
 
-        assertEquals(checkIn.getId(), returnedCheckIn.getId());
-        assertEquals(((CheckInMetric) checkIn).getValue(), ((CheckInMetric) returnedCheckIn).getValue());
-        assertEquals(checkIn.getChangeInfo(), returnedCheckIn.getChangeInfo());
-        assertEquals(checkIn.getModifiedOn(), returnedCheckIn.getModifiedOn());
+    @Test
+    void shouldUpdateCheckInMetricWhenNoTypeChange() {
+        when(checkInPersistenceService.findById(this.checkInMetric.getId())).thenReturn(this.checkInMetric);
+        when(checkInPersistenceService.save(any())).thenReturn(this.checkInMetric);
+
+        CheckIn newCheckIn = checkInBusinessService.updateCheckIn(this.checkInMetric.getId(), this.checkInMetric,
+                this.jwt);
+
+        verify(checkInPersistenceService, times(1)).save(this.checkInMetric);
+        assertEquals(this.checkInMetric.getId(), newCheckIn.getId());
+        assertEquals(this.checkInMetric.getChangeInfo(), newCheckIn.getChangeInfo());
+        assertEquals(this.checkInMetric.getCheckInType(), newCheckIn.getCheckInType());
+    }
+
+    @Test
+    void shouldUpdateCheckInOrdinalWhenNoTypeChange() {
+        when(checkInPersistenceService.findById(this.checkInOrdinal.getId())).thenReturn(this.checkInOrdinal);
+        when(checkInPersistenceService.save(any())).thenReturn(this.checkInOrdinal);
+
+        CheckIn newCheckIn = checkInBusinessService.updateCheckIn(this.checkInOrdinal.getId(), this.checkInOrdinal,
+                this.jwt);
+
+        verify(checkInPersistenceService, times(1)).save(this.checkInOrdinal);
+        assertEquals(this.checkInOrdinal.getId(), newCheckIn.getId());
+        assertEquals(this.checkInOrdinal.getChangeInfo(), newCheckIn.getChangeInfo());
+        assertEquals(this.checkInOrdinal.getCheckInType(), newCheckIn.getCheckInType());
+    }
+
+    @Test
+    void shouldNotEditWhenTypeChange() {
+        when(checkInPersistenceService.findById(this.checkInOrdinal.getId())).thenReturn(this.checkInMetric);
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            checkInBusinessService.updateCheckIn(this.checkInOrdinal.getId(), this.checkInOrdinal, this.jwt);
+        });
+        assertTrue(exception.getMessage().contains("It is not possible to change the type of a Check-In"));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    }
+
+    @Test
+    void shouldSaveMetricCheckIn() {
+        when(checkInPersistenceService.save(any())).thenReturn(this.checkInMetric);
+        CheckIn createdCheckIn = checkInBusinessService.saveCheckIn(this.checkInMetric, this.jwt);
+
+        verify(checkInPersistenceService, times(1)).save(this.checkInMetric);
+        assertEquals(this.checkInMetric.getId(), createdCheckIn.getId());
+        assertEquals(this.checkInMetric.getChangeInfo(), createdCheckIn.getChangeInfo());
+        assertEquals(this.checkInMetric.getCheckInType(), createdCheckIn.getCheckInType());
+        assertEquals(((CheckInMetric) this.checkInMetric).getValue(), ((CheckInMetric) createdCheckIn).getValue());
+    }
+
+    @Test
+    void shouldSaveOrdinalCheckIn() {
+        when(checkInPersistenceService.save(any())).thenReturn(this.checkInOrdinal);
+        CheckIn createdCheckIn = checkInBusinessService.saveCheckIn(this.checkInOrdinal, this.jwt);
+
+        verify(checkInPersistenceService, times(1)).save(this.checkInOrdinal);
+        assertEquals(this.checkInOrdinal.getId(), createdCheckIn.getId());
+        assertEquals(this.checkInOrdinal.getChangeInfo(), createdCheckIn.getChangeInfo());
+        assertEquals(this.checkInOrdinal.getCheckInType(), createdCheckIn.getCheckInType());
+        assertEquals(((CheckInOrdinal) this.checkInOrdinal).getValue(), ((CheckInOrdinal) createdCheckIn).getValue());
+    }
+
+    @Test
+    void shouldDeleteCheckIn() {
+        checkInBusinessService.deleteCheckIn(this.checkInOrdinal.getId());
+        verify(checkInPersistenceService, times(1)).deleteById(this.checkInOrdinal.getId());
+    }
+
+    @Test
+    void shouldGetCheckInsByKeyResultId() {
+        checkInBusinessService.getCheckInsByKeyResultId(this.metricKeyResult.getId());
+        verify(checkInPersistenceService, times(1))
+                .getCheckInsByKeyResultIdOrderByCheckInDateDesc(this.metricKeyResult.getId());
+    }
+
+    @Test
+    void shouldGetLastCheckInByKeyResultId() {
+        checkInBusinessService.getLastCheckInByKeyResultId(this.ordinalKeyResult.getId());
+        verify(checkInPersistenceService, times(1)).getLastCheckInOfKeyResult(this.ordinalKeyResult.getId());
     }
 }

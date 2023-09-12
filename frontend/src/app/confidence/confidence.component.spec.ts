9e6f4691bee2223678847b24accc798ed6b4c9db
Yanick Minder <minder@puzzle.ch>
2023-09-12 08:45:21 +0200

    complete tests for confidence component

--- frontend/src/app/confidence/confidence.component.spec.ts
+++ frontend/src/app/confidence/confidence.component.spec.ts
@@ -1,22 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfidenceComponent } from './confidence.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatSliderHarness } from '@angular/material/slider/testing';
+import { keyResultMetric } from '../shared/testData';
+import { MatSliderModule } from '@angular/material/slider';
+import { CheckInMin } from '../shared/types/model/CheckInMin';
+import { FormsModule } from '@angular/forms';
+import { SimpleChange } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ConfidenceComponent', () => {
   let component: ConfidenceComponent;
   let fixture: ComponentFixture<ConfidenceComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ConfidenceComponent],
+      imports: [MatSliderModule, FormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfidenceComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component.keyResult = keyResultMetric;
+    component.edit = true;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it.each([
+    [{ confidence: 8 } as CheckInMin, 8],
+    [null, 5],
+  ])('should create', async (checkIn: CheckInMin | null, expected: number) => {
+    component.keyResult.lastCheckIn = checkIn;
+    component.ngOnChanges({
+      keyResult: new SimpleChange(null, component.keyResult, true),
+    });
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const slider = await loader.getHarness(MatSliderHarness);
+    const textField = fixture.debugElement.query(By.css("[data-testid='confidence']"));
+    const expectedLabel = expected + '/' + component.max;
+
+    expect(await slider.getValue()).toBe(expected);
+    expect(textField.nativeElement.innerHTML).toContain(expectedLabel);
+  });
+
+  it.each([[true], [false]])('should create', async (editable) => {
+    component.edit = editable;
+    fixture.detectChanges();
+    const slider = fixture.debugElement.query(By.css('mat-slider'));
+
+    expect(!!slider).toBe(editable);
   });
 });

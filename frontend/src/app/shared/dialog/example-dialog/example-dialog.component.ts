7f20ff6c37b2b6b2b05d7399f1866e0420331c20
Yanick Minder <minder@puzzle.ch>
2023-09-12 10:35:07 +0200

    run angular material updater

--- frontend/src/app/shared/dialog/example-dialog/example-dialog.component.ts
+++ frontend/src/app/shared/dialog/example-dialog/example-dialog.component.ts
@@ -1,8 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import {
-  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
-  MatLegacyDialogRef as MatDialogRef,
-} from '@angular/material/legacy-dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import errorMessages from '../../../../assets/errors/error-messages.json';
 

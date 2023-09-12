4b29b3d94b9b1b066c6cd1fa6a1812f776639674
megli2 <egli@puzzle.ch>
2023-09-12 10:17:17 +0200

    simplify sql migration script

--- backend/src/main/resources/db/migration/V2_1_1__checkInRefactoringDropAttrsAddNew.sql
+++ backend/src/main/resources/db/migration/V2_1_1__checkInRefactoringDropAttrsAddNew.sql
@@ -1,38 +1,24 @@
-alter table measure rename value to value_metric;
-alter table measure
-    alter column value_metric drop not null,
-    alter column measure_date drop not null,
-    alter column change_info drop not null,
-    alter column change_info TYPE varchar(4096),
-    add column if not exists confidence    integer,
-    add column if not exists check_in_type varchar(255),
-    add column if not exists value_ordinal text;
-alter table measure rename measure_date to modified_on;
+ALTER TABLE measure
+    RENAME value TO value_metric;
+ALTER TABLE measure
+    ALTER COLUMN value_metric DROP NOT NULL,
+    ALTER COLUMN measure_date DROP NOT NULL,
+    ALTER COLUMN change_info DROP NOT NULL,
+    ALTER COLUMN change_info TYPE VARCHAR(4096),
+    ADD COLUMN confidence    INTEGER,
+    ADD COLUMN check_in_type VARCHAR(255),
+    ADD COLUMN value_ordinal TEXT;
+ALTER TABLE measure
+    RENAME measure_date TO modified_on;
 
-DO
-$$
-    DECLARE
-        r record;
-    BEGIN
-        FOR r in SELECT * FROM measure
-            LOOP
-                UPDATE measure m SET confidence = 5 WHERE confidence IS NULL AND m.id = r.id;
-                UPDATE measure m SET check_in_type = 'metric' WHERE m.id = r.id;
-            end loop;
-    end
-$$;
 
-DO
-$$
-    BEGIN
-        IF EXISTS(SELECT *
-                  FROM information_schema.columns WHERE table_name = 'measure')
-        THEN
-            ALTER TABLE "public"."measure" RENAME TO "check_in";
-            ALTER TABLE "public"."sequence_measure" RENAME TO "sequence_check_in";
-        END IF;
-    END
-$$;
+UPDATE measure SET confidence = 5;
+update measure SET check_in_type = 'metric';
+
+ALTER TABLE "public"."measure"
+    RENAME TO "check_in";
+ALTER TABLE "public"."sequence_measure"
+    RENAME TO "sequence_check_in";
 END TRANSACTION;
 
 

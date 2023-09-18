53ad4809d72ccba24a30c8cbaf0f812d9e12d7c5
Manuel <moeri@puzzle.ch>
2023-09-18 14:06:35 +0200

    Fix size of icons

--- frontend/src/app/keyresult-detail/keyresult-detail.component.ts
+++ frontend/src/app/keyresult-detail/keyresult-detail.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import { KeyResult } from '../shared/types/model/KeyResult';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {KeyResult} from '../shared/types/model/KeyResult';
 
 @Component({
   selector: 'app-keyresult-detail',
   templateUrl: './keyresult-detail.component.html',
   styleUrls: ['./keyresult-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class KeyresultDetailComponent implements OnInit {
   @Input() keyResult!: KeyResult;

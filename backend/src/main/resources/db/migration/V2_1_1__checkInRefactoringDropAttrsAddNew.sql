ca70a44b7769b52e4aa8fc99dc4171eef608cf69
megli2 <egli@puzzle.ch>
2023-09-20 08:56:56 +0200

    toggle attributes in migration file back to lowercase to avoid db-error

--- backend/src/main/resources/db/migration/V2_1_1__checkInRefactoringDropAttrsAddNew.sql
+++ backend/src/main/resources/db/migration/V2_1_1__checkInRefactoringDropAttrsAddNew.sql
@@ -19,26 +19,26 @@ ALTER TABLE "public"."sequence_measure" RENAME TO "sequence_check_in";
 
 DROP VIEW IF EXISTS OVERVIEW;
 CREATE VIEW OVERVIEW AS
-SELECT T.ID                AS "TEAM_ID",
-       T.NAME              AS "TEAM_NAME",
-       COALESCE(O.ID, -1)  AS "OBJECTIVE_ID",
-       O.TITLE             AS "OBJECTIVE_TITLE",
-       O.STATE             AS "OBJECTIVE_STATE",
-       Q.ID                AS "QUARTER_ID",
-       Q.LABEL             AS "QUARTER_LABEL",
-       COALESCE(KR.ID, -1) AS "KEY_RESULT_ID",
-       KR.TITLE            AS "KEY_RESULT_TITLE",
-       KR.UNIT,
-       KR.BASELINE,
-       KR.STRETCH_GOAL,
-       KR.COMMIT_ZONE,
-       KR.TARGET_ZONE,
-       KR.STRETCH_ZONE,
-       COALESCE(C.ID, -1)  AS "CHECK_IN_ID",
-       C.VALUE_METRIC      AS "CHECK_IN_VALUE",
-       C.ZONE              AS "CHECK_IN_ZONE",
-       C.CONFIDENCE,
-       C.CREATED_ON
+SELECT t.id                AS "team_id",
+       t.name              AS "team_name",
+       coalesce(o.id, -1)  AS "objective_id",
+       o.title             AS "objective_title",
+       o.state             AS "objective_state",
+       q.id                AS "quarter_id",
+       q.label             AS "quarter_label",
+       coalesce(kr.id, -1) AS "key_result_id",
+       kr.title            AS "key_result_title",
+       kr.unit,
+       kr.baseline,
+       kr.stretch_goal,
+       kr.commit_zone,
+       kr.target_zone,
+       kr.stretch_zone,
+       coalesce(c.id, -1)  AS "check_in_id",
+       c.value_metric      AS "check_in_value",
+       c.zone              AS "check_in_zone",
+       c.confidence,
+       c.created_on
 FROM TEAM T
          LEFT JOIN OBJECTIVE O ON T.ID = O.TEAM_ID
          LEFT JOIN QUARTER Q ON O.QUARTER_ID = Q.ID

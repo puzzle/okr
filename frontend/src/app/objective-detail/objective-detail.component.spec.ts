3a014a5f08764e661d41e3c53ea50fc076836f6a
megli2 <egli@puzzle.ch>
2023-09-20 11:35:33 +0200

    fix test by adding HttpClientTestingModule

--- frontend/src/app/objective-detail/objective-detail.component.spec.ts
+++ frontend/src/app/objective-detail/objective-detail.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ObjectiveDetailComponent } from './objective-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ObjectiveDetailComponent', () => {
   let component: ObjectiveDetailComponent;
@@ -8,6 +9,7 @@ describe('ObjectiveDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ObjectiveDetailComponent],
     }).compileComponents();
 

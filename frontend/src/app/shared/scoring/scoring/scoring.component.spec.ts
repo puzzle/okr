98467bc027e8a84f7697c1c82c3d07553a9d6338
Lias Kleisa <kleisa@puzzle.ch>
2023-09-21 15:58:47 +0200

    Start with testing

--- frontend/src/app/shared/scoring/scoring/scoring.component.spec.ts
+++ frontend/src/app/shared/scoring/scoring/scoring.component.spec.ts
@@ -1,19 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScoringComponent } from './scoring.component';
+import { MatSliderModule } from "@angular/material/slider";
+import { FormsModule } from "@angular/forms";
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
+import { keyResultMetric } from "../../testData";
+import { HarnessLoader } from "@angular/cdk/testing";
 
 describe('ScoringComponent', () => {
   let component: ScoringComponent;
   let fixture: ComponentFixture<ScoringComponent>;
+  let loader: HarnessLoader
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [ScoringComponent],
+      imports: [MatSliderModule, FormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScoringComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component.keyResult = keyResultMetric;
   });
 
   it('should create', () => {

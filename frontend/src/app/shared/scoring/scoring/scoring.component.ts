89acb86c8118a29f959195300e819e09ac05fd9a
Lias Kleisa <kleisa@puzzle.ch>
2023-09-21 14:40:09 +0200

    Implement scoring component

--- frontend/src/app/shared/scoring/scoring/scoring.component.ts
+++ frontend/src/app/shared/scoring/scoring/scoring.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { KeyresultMin } from '../../types/model/KeyresultMin';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-scoring',
@@ -7,7 +8,7 @@ import { KeyresultMin } from '../../types/model/KeyresultMin';
   styleUrls: ['./scoring.component.scss'],
 })
 export class ScoringComponent implements OnInit {
-  // @Input keyResult!: KeyresultMin;
+  @Input keyResult!: KeyresultMin;
   failWidth: string = '50%';
   commitWidth: string = '0';
   targetWidth: string = '0';
@@ -21,29 +22,30 @@ export class ScoringComponent implements OnInit {
   endLineFail: string = '';
   endLineCommit: string = '';
   endLineTarget: string = '';
-  isFailZone: boolean = false;
-  isCommitZone: boolean = false;
-  isTargetZone: boolean = false;
+  zoneWidth: number = 0;
 
-  zonewith: number = '';
-
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // if (this.keyResult.lastCheckIn === undefined) {
-    //   this.failWidth = this.commitWidth = this.targetWidth = '0';
-    // } else {
-    this.isOverview;
-    // if (keyResult.type === 'metric') {
-    this.calculatePercentagesMetric();
+    let url = this.router.url;
+    if (url.includes('keyresult')) {
+      this.isOverview = false;
+      this.zoneWidth = 98;
+    } else {
+      this.isOverview = true;
+      this.zoneWidth = 63;
+    }
 
-    this.setBarColorsMetric();
-
-    // }
-    // else {
-    // this.setColorsOrdinal();
-    // }
-    // }
+    if (this.keyResult.lastCheckIn === undefined) {
+      this.failWidth = this.commitWidth = this.targetWidth = '0';
+    } else {
+      if (this.keyResult.type === 'metric') {
+        this.calculatePercentagesMetric();
+        this.setBarColorsMetric();
+      } else {
+        this.setColorsOrdinal();
+      }
+    }
   }
 
   setBarColorsMetric() {
@@ -61,71 +63,51 @@ export class ScoringComponent implements OnInit {
   }
 
   calculatePercentagesMetric() {
-    // this.metricLabel = this.keyResult.unit + checkInValue;
-    this.metricLabel = 'CHF 230000';
-
-    // let baseline: number = this.keyResult.baseLine;
-    let baseline: number = 0;
-    // let stretchGoal: number = this.keyResult.stretchGoal;
-    let stretchGoal: number = 10;
-    // let checkInValue: number = this.keyResult.lastCheckin.value;
-    let checkInValue: number = 7;
+    let baseline: number = this.keyResult.baseLine;
+    let stretchGoal: number = this.keyResult.stretchGoal;
+    let checkInValue: number = this.keyResult.lastCheckin.value;
+    this.metricLabel = this.keyResult.unit + checkInValue;
 
     let decimal: number = (checkInValue - baseline) / (stretchGoal - baseline);
-
-    console.log(decimal);
-
-    // this.labelWidth = decimal * 100 + "%";
 
     if (decimal < 0.3) {
       let percent = (decimal * 100) / 30;
       this.failWidth = percent * 100 + '%';
+      this.labelWidth = this.zoneWidth * percent + 'px';
+      this.commitWidth = this.targetWidth = '0';
       if (this.failWidth != '100%' && this.failWidth != '0') {
         this.endLineFail = 'endLine';
       }
-      this.labelWidth = this.zonewith * percent + 'px';
-      this.commitWidth = this.targetWidth = '0';
-      this.isFailZone = true;
     } else if (decimal < 0.7) {
+      let percent = (decimal * 100 - 30) / 40;
       this.failWidth = '100%';
-      this.commitWidth = ((decimal * 100 - 30) / 40) * 100 + '%';
+      this.commitWidth = percent * 100 + '%';
+      this.targetWidth = '0';
+      this.labelWidth = this.zoneWidth + this.zoneWidth * percent + 'px';
       if (this.commitWidth != '100%' && this.commitWidth != '0') {
         this.endLineCommit = 'endLine';
       }
-      this.targetWidth = '0';
-      this.isCommitZone = true;
-
-      let percent = (decimal * 100 - 30) / 40;
-      console.log(percent);
-      this.labelWidth = this.zonewith + this.zonewith * percent + 'px';
-      console.log(this.labelWidth);
     } else {
-      this.targetWidth = decimal > 1.0 ? '100%' : ((decimal * 100 - 70) / 30) * 100 + '%';
-
+      let percent = (decimal * 100 - 70) / 30;
+      this.targetWidth = decimal > 1.0 ? '100%' : percent * 100 + '%';
+      this.labelWidth = this.zoneWidth + this.zoneWidth + this.zoneWidth * percent + 'px';
+      this.failWidth = this.commitWidth = '100%';
       if (this.targetWidth != '100%' && this.targetWidth != '0') {
         this.endLineTarget = 'endLine';
       }
-      this.labelWidth = 98 + 98 + (98 * (decimal * 100 - 70)) / 30 + 'px';
-      this.failWidth = this.commitWidth = '100%';
-      this.isTargetZone = true;
     }
   }
 
   setColorsOrdinal() {
-    let value = 'FAIL';
-
-    // if (this.keyResult.lastCheckIn!.value === 'FAIL') {
-    if (value === 'FAIL') {
+    if (this.keyResult.lastCheckIn!.value === 'FAIL') {
       this.failWidth = '100%';
       this.failColor = '#BA3838';
       this.commitColor = this.targetColor = '#ffffff';
-      // } else if (this.keyResult.lastCheckIn!.value === 'COMMIT') {
-    } else if (value === 'COMMIT') {
+    } else if (this.keyResult.lastCheckIn!.value === 'COMMIT') {
       this.failWidth = this.commitWidth = '100%';
       this.failColor = this.commitColor = '#FFD600';
       this.targetColor = '#ffffff';
-      // } else if (this.keyResult.lastCheckIn!.value === 'TARGET') {
-    } else if (value === 'TARGET') {
+    } else if (this.keyResult.lastCheckIn!.value === 'TARGET') {
       this.failWidth = this.commitWidth = this.targetWidth = '100%';
       this.failColor = this.commitColor = this.targetColor = '#1E8A29';
     } else {

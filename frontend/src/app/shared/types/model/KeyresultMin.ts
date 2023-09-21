8fad5a8bc9b5bbb8f34a204a2913d9fac85b44be
Lias Kleisa <kleisa@puzzle.ch>
2023-09-21 16:04:44 +0200

    Adjust KeyResultMin model

--- frontend/src/app/shared/types/model/KeyresultMin.ts
+++ frontend/src/app/shared/types/model/KeyresultMin.ts
@@ -6,10 +6,11 @@ export class KeyresultMin extends SidenavModel {
   keyResultType: string;
   lastCheckIn: CheckInMin | null;
 
-  constructor(id: number, title: string, lastCheckIn: CheckInMin | null) {
+  constructor(id: number, title: string, keyResultType: string, lastCheckIn: CheckInMin | null) {
     super(id, 'keyResult');
     this.id = id;
     this.title = title;
+    this.keyResultType = keyResultType;
     this.lastCheckIn = lastCheckIn;
   }
 }

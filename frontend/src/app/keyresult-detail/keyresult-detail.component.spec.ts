d0a1850969565d7c9401d92f0369b29837128475
peggimann <peggimann@puzzle.com>
2023-09-22 20:27:44 +0200

    import matdialogmodule for testing

--- frontend/src/app/keyresult-detail/keyresult-detail.component.spec.ts
+++ frontend/src/app/keyresult-detail/keyresult-detail.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeyresultDetailComponent } from './keyresult-detail.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('KeyresultDetailComponent', () => {
   let component: KeyresultDetailComponent;
@@ -9,7 +10,7 @@ describe('KeyresultDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, MatDialogModule],
       declarations: [KeyresultDetailComponent],
     }).compileComponents();
 

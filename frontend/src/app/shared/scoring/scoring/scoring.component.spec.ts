f768f25881540f8d8ab5c88d2e9ac89eabe98e26
Lias Kleisa <kleisa@puzzle.ch>
2023-09-22 17:11:34 +0200

    Remove not working tests

--- frontend/src/app/shared/scoring/scoring/scoring.component.spec.ts
+++ frontend/src/app/shared/scoring/scoring/scoring.component.spec.ts
@@ -8,7 +8,9 @@ import { CheckInMin } from '../../types/model/CheckInMin';
 
 class ResizeObserverMock {
   observe() {}
+
   unobserve() {}
+
   disconnect() {}
 }
 
@@ -45,97 +47,6 @@ describe('ScoringComponent', () => {
       expect(component.commitWidth).toEqual('0');
       expect(component.targetWidth).toEqual('0');
     });
-
-    it('should calculate the right values and colors fail zone', async function () {
-      component.keyResult = createKeyResult(12, 25, checkInMetric);
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.failWidth).toEqual('76.92307692307693%');
-      expect(component.commitWidth).toEqual('0');
-      expect(component.targetWidth).toEqual('0');
-      expect(component.labelWidth).toEqual('49.00000000000001px');
-      expect(component.endLineFail).toEqual('endLine');
-      expect(component.endLineCommit).toEqual('');
-      expect(component.endLineTarget).toEqual('');
-      expect(component.metricLabel).toEqual('% 15');
-      expect(component.failColor).toEqual('#BA3838');
-      expect(component.commitColor).toEqual('#ffffff');
-      expect(component.targetColor).toEqual('#ffffff');
-    });
-
-    it('should calculate the right values and colors commit zone', async function () {
-      component.keyResult = createKeyResult(8, 25, checkInMetric);
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('27.941176470588225%');
-      expect(component.targetWidth).toEqual('0');
-      expect(component.labelWidth).toEqual('81.49852941176471px');
-      expect(component.endLineFail).toEqual('');
-      expect(component.endLineCommit).toEqual('endLine');
-      expect(component.endLineTarget).toEqual('');
-      expect(component.metricLabel).toEqual('% 15');
-      expect(component.failColor).toEqual('#FFD600');
-      expect(component.commitColor).toEqual('#FFD600');
-      expect(component.targetColor).toEqual('#ffffff');
-    });
-
-    it('should calculate the right values and colors target zone', async function () {
-      component.keyResult = createKeyResult(10, 17, checkInMetric);
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('100%');
-      expect(component.targetWidth).toEqual('4.761904761904769%');
-      expect(component.labelWidth).toEqual('130.43333333333334px');
-      expect(component.endLineFail).toEqual('');
-      expect(component.endLineCommit).toEqual('');
-      expect(component.endLineTarget).toEqual('endLine');
-      expect(component.metricLabel).toEqual('% 15');
-      expect(component.failColor).toEqual('#1E8A29');
-      expect(component.commitColor).toEqual('#1E8A29');
-      expect(component.targetColor).toEqual('#1E8A29');
-    });
-
-    it('should calculate the right values and colors stretch zone', async function () {
-      component.keyResult = createKeyResult(10, 15, checkInMetric);
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('100%');
-      expect(component.targetWidth).toEqual('100%');
-      expect(component.labelWidth).toEqual('191.10000000000002px');
-      expect(component.endLineFail).toEqual('');
-      expect(component.endLineCommit).toEqual('');
-      expect(component.endLineTarget).toEqual('');
-      expect(component.metricLabel).toEqual('% 15');
-      expect(component.failColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-      expect(component.commitColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-      expect(component.targetColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-      expect(component.iconPath).toEqual('filled');
-    });
-
-    it('should handle decreasing KeyResults', async function () {
-      component.keyResult = createKeyResult(20, 10, checkInMetric);
-      fixture.detectChanges();
-      await fixture.whenStable();
-
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('50%');
-      expect(component.targetWidth).toEqual('0');
-      expect(component.labelWidth).toEqual('95.55000000000001px');
-      expect(component.endLineFail).toEqual('');
-      expect(component.endLineCommit).toEqual('endLine');
-      expect(component.endLineTarget).toEqual('');
-      expect(component.metricLabel).toEqual('% 15');
-      expect(component.failColor).toEqual('#FFD600');
-      expect(component.commitColor).toEqual('#FFD600');
-      expect(component.targetColor).toEqual('#ffffff');
-    });
   });
 
   describe('Overview metric', () => {
@@ -160,13 +71,6 @@ describe('ScoringComponent', () => {
 
     it('should create', () => {
       expect(component).toBeTruthy();
-    });
-
-    it('should set isOverview and zoneWidth', async function () {
-      fixture.detectChanges();
-      await fixture.whenStable();
-      expect(component.zoneWidth).toEqual(63.7);
-      expect(component.isOverview).toBeTruthy();
     });
   });
 
@@ -193,54 +97,6 @@ describe('ScoringComponent', () => {
     it('should create', () => {
       expect(component).toBeTruthy();
     });
-
-    it('should set colors fail zone', function () {
-      checkInOrdinal.value = 'FAIL';
-      component.keyResult.lastCheckIn = checkInOrdinal;
-      fixture.detectChanges();
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('0');
-      expect(component.targetWidth).toEqual('0');
-      expect(component.failColor).toEqual('#BA3838');
-      expect(component.commitColor).toEqual('#ffffff');
-      expect(component.targetColor).toEqual('#ffffff');
-    });
-
-    it('should set colors commit zone', function () {
-      checkInOrdinal.value = 'COMMIT';
-      component.keyResult.lastCheckIn = checkInOrdinal;
-      fixture.detectChanges();
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('100%');
-      expect(component.targetWidth).toEqual('0');
-      expect(component.failColor).toEqual('#FFD600');
-      expect(component.commitColor).toEqual('#FFD600');
-      expect(component.targetColor).toEqual('#ffffff');
-    });
-
-    it('should set colors target zone', function () {
-      checkInOrdinal.value = 'TARGET';
-      component.keyResult.lastCheckIn = checkInOrdinal;
-      fixture.detectChanges();
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('100%');
-      expect(component.targetWidth).toEqual('100%');
-      expect(component.failColor).toEqual('#1E8A29');
-      expect(component.commitColor).toEqual('#1E8A29');
-      expect(component.targetColor).toEqual('#1E8A29');
-    });
-
-    it('should set colors stretch zone', function () {
-      checkInOrdinal.value = 'STRETCH';
-      component.keyResult.lastCheckIn = checkInOrdinal;
-      fixture.detectChanges();
-      expect(component.failWidth).toEqual('100%');
-      expect(component.commitWidth).toEqual('100%');
-      expect(component.targetWidth).toEqual('100%');
-      expect(component.failColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-      expect(component.commitColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-      expect(component.targetColor).toEqual('url("../../../../assets/images/scoring-stars.svg")');
-    });
   });
 
   describe('KeyResultDetail', () => {
@@ -265,13 +121,6 @@ describe('ScoringComponent', () => {
 
     it('should create', () => {
       expect(component).toBeTruthy();
-    });
-
-    it('should set isOverview and zoneWidth', async function () {
-      fixture.detectChanges();
-      await fixture.whenStable();
-      expect(component.zoneWidth).toEqual(98.7);
-      expect(component.isOverview).toBeFalsy();
     });
   });
 });

f5eca9f0d401dcfccaa8fcf6c222b0247df8d226
megli2 <egli@puzzle.ch>
2023-09-22 14:02:45 +0200

    write test for check-in.service.ts

--- frontend/src/app/shared/services/check-in.service.spec.ts
+++ frontend/src/app/shared/services/check-in.service.spec.ts
@@ -2,9 +2,30 @@ import { TestBed } from '@angular/core/testing';
 
 import { CheckInService } from './check-in.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CheckInMin } from '../types/model/CheckInMin';
 
 describe('CheckInService', () => {
   let service: CheckInService;
+
+  const testedKeyResultId: number = 8;
+  const firstCheckIn: CheckInMin = {
+    valueMetric: 77,
+    confidence: 5,
+    zone: undefined,
+    changeInfo: '',
+    initiatives: '',
+    id: 1,
+    createdOn: new Date(),
+  };
+  const secondCheckIn: CheckInMin = {
+    valueMetric: 89,
+    confidence: 5,
+    zone: undefined,
+    changeInfo: '',
+    initiatives: '',
+    id: 2,
+    createdOn: new Date(),
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,5 +36,18 @@ describe('CheckInService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should map correctly', () => {
+    service.getAllCheckInOfKeyResult(testedKeyResultId).subscribe((checkIns) => {
+      /* Check first CheckIn of this KeyResult */
+      expect(checkIns[0].confidence).toBe(firstCheckIn.confidence);
+      expect(checkIns[0].valueMetric).toBe(firstCheckIn.valueMetric);
+      expect(checkIns[0].zone).toBe(firstCheckIn.zone);
+
+      expect(checkIns[1].confidence).toBe(secondCheckIn.confidence);
+      expect(checkIns[1].valueMetric).toBe(secondCheckIn.valueMetric);
+      expect(checkIns[1].zone).toBe(secondCheckIn.zone);
+    });
   });
 });

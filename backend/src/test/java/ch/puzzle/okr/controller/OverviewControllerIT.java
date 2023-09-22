841b9e3d201294db76664b0ad5f1c06d3158ee56
peggimann <peggimann@puzzle.com>
2023-09-22 15:11:39 +0200

    implement constants for overviewcontroller

--- backend/src/test/java/ch/puzzle/okr/controller/OverviewControllerIT.java
+++ backend/src/test/java/ch/puzzle/okr/controller/OverviewControllerIT.java
@@ -35,23 +35,31 @@ public class OverviewControllerIT {
     @MockBean
     private OverviewService overviewService;
 
-    static OverviewDto overviewDtoPuzzle = new OverviewDto(new OverviewTeamDto(1L, "Puzzle"), List.of(
-            new OverviewObjectiveDto(1L, "Objective 1", DRAFT, new OverviewQuarterDto(1L, "GJ 22/23-Q2"),
-                    List.of(new OverviewKeyResultMetricDto(20L, "This is a description", "metric", "CHF", 5.0, 20.0,
+    public static final String PUZZLE = "Puzzle";
+    public static final String DESCRIPTION = "This is a description";
+    public static final String TEAM_KUCHEN = "Kuchen";
+    public static final String QUARTER_LABEL = "GJ 22/23-Q2";
+
+    public static final String JSON_PATH_TEAM_NAME = "$[0].team.name";
+    public static final String JSON_PATH_TEAM_ID = "$[0].team.id";
+
+    static OverviewDto overviewDtoPuzzle = new OverviewDto(new OverviewTeamDto(1L, PUZZLE), List.of(
+            new OverviewObjectiveDto(1L, "Objective 1", DRAFT, new OverviewQuarterDto(1L, QUARTER_LABEL),
+                    List.of(new OverviewKeyResultMetricDto(20L, DESCRIPTION, "CHF", 5.0, 20.0,
                             new OverviewLastCheckInMetricDto(40L, 15.0, 5, LocalDateTime.now())))),
-            new OverviewObjectiveDto(2L, "Objective 2", ONGOING, new OverviewQuarterDto(1L, "GJ 22/23-Q2"),
-                    List.of(new OverviewKeyResultMetricDto(21L, "This is a description", "metric", "CHF", 5.0, 20.0,
+            new OverviewObjectiveDto(2L, "Objective 2", ONGOING, new OverviewQuarterDto(1L, QUARTER_LABEL),
+                    List.of(new OverviewKeyResultMetricDto(21L, DESCRIPTION, "CHF", 5.0, 20.0,
                             new OverviewLastCheckInMetricDto(41L, 15.0, 5, LocalDateTime.now()))))));
     static OverviewDto overviewDtoOKR = new OverviewDto(new OverviewTeamDto(2L, "OKR"), List.of(
-            new OverviewObjectiveDto(5L, "Objective 5", DRAFT, new OverviewQuarterDto(1L, "GJ 22/23-Q2"),
-                    List.of(new OverviewKeyResultMetricDto(20L, "This is a description", "metric", "CHF", 5.0, 20.0,
+            new OverviewObjectiveDto(5L, "Objective 5", DRAFT, new OverviewQuarterDto(1L, QUARTER_LABEL),
+                    List.of(new OverviewKeyResultMetricDto(20L, DESCRIPTION, "CHF", 5.0, 20.0,
                             new OverviewLastCheckInMetricDto(40L, 15.0, 5, LocalDateTime.now())))),
-            new OverviewObjectiveDto(7L, "Objective 7", ONGOING, new OverviewQuarterDto(1L, "GJ 22/23-Q2"),
-                    List.of(new OverviewKeyResultMetricDto(21L, "This is a description", "metric", "CHF", 5.0, 20.0,
+            new OverviewObjectiveDto(7L, "Objective 7", ONGOING, new OverviewQuarterDto(1L, QUARTER_LABEL),
+                    List.of(new OverviewKeyResultMetricDto(21L, DESCRIPTION, "CHF", 5.0, 20.0,
                             new OverviewLastCheckInMetricDto(41L, 15.0, 5, LocalDateTime.now()))))));
-    static OverviewDto overviewDtoKuchen = new OverviewDto(new OverviewTeamDto(3L, "Kuchen"),
-            List.of(new OverviewObjectiveDto(8L, "Objective 8", ONGOING, new OverviewQuarterDto(1L, "GJ 22/23-Q2"),
-                    List.of(new OverviewKeyResultMetricDto(20L, "This is a description", "metric", "CHF", 5.0, 20.0,
+    static OverviewDto overviewDtoKuchen = new OverviewDto(new OverviewTeamDto(3L, TEAM_KUCHEN),
+            List.of(new OverviewObjectiveDto(8L, "Objective 8", ONGOING, new OverviewQuarterDto(1L, QUARTER_LABEL),
+                    List.of(new OverviewKeyResultMetricDto(20L, DESCRIPTION, "CHF", 5.0, 20.0,
                             new OverviewLastCheckInMetricDto(40L, 15.0, 5, LocalDateTime.now()))))));
     static OverviewDto overviewDtoFindus = new OverviewDto(new OverviewTeamDto(4L, "Findus"), Collections.emptyList());
 
@@ -62,13 +70,14 @@ public class OverviewControllerIT {
 
         mvc.perform(get("/api/v2/overview?quarter=2&team=1,2,3,4").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(3)))
-                .andExpect(jsonPath("$[0].team.id", Is.is(1))).andExpect(jsonPath("$[0].team.name", Is.is("Puzzle")))
+                .andExpect(jsonPath(JSON_PATH_TEAM_ID, Is.is(1)))
+                .andExpect(jsonPath(JSON_PATH_TEAM_NAME, Is.is(PUZZLE)))
                 .andExpect(jsonPath("$[0].objectives[0].id", Is.is(1)))
                 .andExpect(jsonPath("$[0].objectives[1].id", Is.is(2))).andExpect(jsonPath("$[1].team.id", Is.is(2)))
                 .andExpect(jsonPath("$[1].team.name", Is.is("OKR")))
                 .andExpect(jsonPath("$[1].objectives[0].id", Is.is(5)))
                 .andExpect(jsonPath("$[1].objectives[1].id", Is.is(7))).andExpect(jsonPath("$[2].team.id", Is.is(3)))
-                .andExpect(jsonPath("$[2].team.name", Is.is("Kuchen")))
+                .andExpect(jsonPath("$[2].team.name", Is.is(TEAM_KUCHEN)))
                 .andExpect(jsonPath("$[2].objectives[0].id", Is.is(8)));
     }
 
@@ -88,10 +97,11 @@ public class OverviewControllerIT {
 
         mvc.perform(get("/api/v2/overview?quarter=2&team=1,3").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(2)))
-                .andExpect(jsonPath("$[0].team.id", Is.is(1))).andExpect(jsonPath("$[0].team.name", Is.is("Puzzle")))
+                .andExpect(jsonPath(JSON_PATH_TEAM_ID, Is.is(1)))
+                .andExpect(jsonPath(JSON_PATH_TEAM_NAME, Is.is(PUZZLE)))
                 .andExpect(jsonPath("$[0].objectives[0].id", Is.is(1)))
                 .andExpect(jsonPath("$[0].objectives[1].id", Is.is(2))).andExpect(jsonPath("$[1].team.id", Is.is(3)))
-                .andExpect(jsonPath("$[1].team.name", Is.is("Kuchen")))
+                .andExpect(jsonPath("$[1].team.name", Is.is(TEAM_KUCHEN)))
                 .andExpect(jsonPath("$[1].objectives[0].id", Is.is(8)));
     }
 
@@ -102,7 +112,8 @@ public class OverviewControllerIT {
 
         mvc.perform(get("/api/v2/overview?quarter=2&team=4").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("$", Matchers.hasSize(1)))
-                .andExpect(jsonPath("$[0].team.id", Is.is(4))).andExpect(jsonPath("$[0].team.name", Is.is("Findus")))
+                .andExpect(jsonPath(JSON_PATH_TEAM_ID, Is.is(4)))
+                .andExpect(jsonPath(JSON_PATH_TEAM_NAME, Is.is("Findus")))
                 .andExpect(jsonPath("$[0].objectives.size()", Is.is(0)));
     }
 }

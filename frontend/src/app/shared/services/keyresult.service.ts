5b047935ef64ac9e46f727363ebdd0e2f9056cff
Yanick Minder <minder@puzzle.ch>
2023-09-22 14:05:35 +0200

    improve styling of keyresult detail component

--- frontend/src/app/shared/services/keyresult.service.ts
+++ frontend/src/app/shared/services/keyresult.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { KeyResult } from '../types/model/KeyResult';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,17 @@ export class KeyresultService {
   constructor(private httpClient: HttpClient) {}
 
   getFullKeyResult(keyresultId: number) {
-    return this.httpClient.get<KeyResult>('/api/v2/keyresults/' + keyresultId);
+    return this.httpClient.get<KeyResult>('/api/v2/keyresults/' + keyresultId).pipe(
+      map((keyresult: any) => {
+        const quarter = keyresult.objective.keyResultQuarterDto;
+        keyresult.objective.quarter = {
+          id: quarter.id,
+          label: quarter.label,
+          startDate: quarter.startDate,
+          endDate: quarter.endDate,
+        };
+        return keyresult;
+      }),
+    );
   }
 }

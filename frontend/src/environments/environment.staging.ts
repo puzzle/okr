4b44ae768994e6621bcf20a32b146fae3b55536e
peggimann <peggimann@puzzle.com>
2023-09-28 16:00:50 +0200

    implement route to mock for staging env

--- frontend/src/environments/environment.staging.ts
+++ frontend/src/environments/environment.staging.ts
@@ -6,10 +6,10 @@ export const environment = {
   oauth: {
     decreaseExpirationBySec: 30,
     clearHashAfterLogin: true,
-    issuer: 'https://sso.puzzle.ch/auth/realms/pitc',
+    issuer: 'https://idp-mock-okr.ocp-internal.cloudscale.puzzle.ch/',
     strictDiscoveryDocumentValidation: false,
     redirectUri: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/auth/keycloakopenid/callback`,
-    scope: 'profile openid',
+    scope: 'profile openid pitc preferred_username given_name family_name email',
     clientId: 'pitc_okr_prod',
     responseType: 'code',
     showDebugInformation: true,

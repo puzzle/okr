aee872b456fd08ef3b9850549b00b748cbec045b
megli2 <egli@puzzle.ch>
2023-09-28 13:11:11 +0200

    fix pipe error of not converting strings by defining value as 0 if strings are written into input

--- frontend/src/app/shared/pipes/unit-transformation.pipe.ts
+++ frontend/src/app/shared/pipes/unit-transformation.pipe.ts
@@ -6,6 +6,9 @@ import { formatCurrency } from '@angular/common';
 })
 export class UnitTransformationPipe implements PipeTransform {
   transform(value: number, unit: String): string {
+    if (Number.isNaN(value)) {
+      value = 0;
+    }
     switch (unit) {
       case 'CHF':
         return value % 1 != 0 ? formatCurrency(value, 'en', '') : value + '.-';

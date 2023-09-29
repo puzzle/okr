77523762f9778a618ec72ba7dd612c5939ba939f
megli2 <egli@puzzle.ch>
2023-09-29 09:31:30 +0200

    format generated pipe

--- frontend/src/app/shared/pipes/unit-label-transformation/unit-label-transformation.pipe.ts
+++ frontend/src/app/shared/pipes/unit-label-transformation/unit-label-transformation.pipe.ts
@@ -1,22 +1,21 @@
-import {Pipe, PipeTransform} from '@angular/core';
-import {Unit} from "../../types/enums/Unit";
-import translation from "../../../../assets/i18n/de.json"
+import { Pipe, PipeTransform } from '@angular/core';
+import { Unit } from '../../types/enums/Unit';
+import translation from '../../../../assets/i18n/de.json';
 
 @Pipe({
-  name: 'unitLabelTransformation'
+  name: 'unitLabelTransformation',
 })
 export class UnitLabelTransformationPipe implements PipeTransform {
-
   transform(unitLabel: String): String {
     switch (unitLabel) {
       case Unit.PERCENT:
-        return "";
+        return '';
       case Unit.FTE:
         return Unit.FTE;
       case Unit.CHF:
         return Unit.CHF;
       case Unit.NUMBER:
-        return translation.UNIT.NUMBER
+        return translation.UNIT.NUMBER;
       default:
         return unitLabel;
     }

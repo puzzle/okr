13e8712a512b3a2a9923c787a5b6dbf810830a98
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-02 10:54:08 +0200

    fix it should be created test of team service

--- frontend/src/app/shared/services/team.service.spec.ts
+++ frontend/src/app/shared/services/team.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeamService } from './team.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TeamService', () => {
   let service: TeamService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(TeamService);
   });
 

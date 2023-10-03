92f806f85379d0daf943f05e34a6613b77574cb6
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-03 07:57:29 +0200

    change change detection strategy of confirm dialog to onpush

--- frontend/src/app/shared/dialog/confirm-dialog/confirm-dialog.component.ts
+++ frontend/src/app/shared/dialog/confirm-dialog/confirm-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConfirmDialogComponent {
   constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>) {}

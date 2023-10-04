cc53b657f5d7c5a78b8ae51b2743600d26c59930
megli2 <egli@puzzle.ch>
2023-10-04 15:13:38 +0200

    add tests to check if parse-pipe works correctly

--- frontend/src/app/shared/pipes/parse-unit-value/parse-unit-value.pipe.spec.ts
+++ frontend/src/app/shared/pipes/parse-unit-value/parse-unit-value.pipe.spec.ts
@@ -5,4 +5,24 @@ describe('ParseUnitValuePipe', () => {
     const pipe = new ParseUnitValuePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should replace characters at end of string', () => {
+    const pipe = new ParseUnitValuePipe();
+    expect(pipe.transform('200HelloWorld')).toBe(200);
+  });
+
+  it('should replace characters at beginning of string', () => {
+    const pipe = new ParseUnitValuePipe();
+    expect(pipe.transform('HelloWorld200')).toBe(200);
+  });
+
+  it('should replace characters between strings', () => {
+    const pipe = new ParseUnitValuePipe();
+    expect(pipe.transform("200'000")).toBe(200000);
+  });
+
+  it('should replace special characters', () => {
+    const pipe = new ParseUnitValuePipe();
+    expect(pipe.transform('1050&%ç*')).toBe(1050);
+  });
 });

e407ebd82fe2fe896f56a376a180fe813c005a7f
megli2 <egli@puzzle.ch>
2023-10-06 17:03:03 +0200

    delete unnecessary imports in quarterService

--- frontend/src/app/shared/services/quarter.service.spec.ts
+++ frontend/src/app/shared/services/quarter.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
 import { QuarterService } from './quarter.service';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('QuarterService', () => {

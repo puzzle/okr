bb17840f90f63e336f8b945e8257d64d6ee6fc13
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-10 09:15:54 +0200

    implement basic dropdown in header to select quarter

--- frontend/src/app/quarter-filter/quarter-filter.component.ts
+++ frontend/src/app/quarter-filter/quarter-filter.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { QuarterService } from '../shared/services/quarter.service';
+import { Quarter } from '../shared/types/model/Quarter';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-quarter-filter',
   templateUrl: './quarter-filter.component.html',
   styleUrls: ['./quarter-filter.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuarterFilterComponent {}
+export class QuarterFilterComponent implements OnInit {
+  quarters$: Observable<Quarter[]> = of([]);
+
+  constructor(private quarterService: QuarterService) {}
+
+  ngOnInit() {
+    this.quarters$ = this.quarterService.getAllQuarters();
+  }
+}

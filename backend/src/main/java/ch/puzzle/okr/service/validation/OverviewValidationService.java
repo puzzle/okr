ac702e4645b51de4e7102d283f62d13d7332db4d
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-12 13:32:22 +0200

    add validation for overview, that validates if the team ids and quarter ids given by the filter are valid

---
+++ backend/src/main/java/ch/puzzle/okr/service/validation/OverviewValidationService.java
@@ -0,0 +1,28 @@
+package ch.puzzle.okr.service.validation;
+
+import java.util.List;
+
+public class OverviewValidationService {
+    private final TeamValidationService teamValidator;
+    private final QuarterValidationService quarterValidator;
+
+    public OverviewValidationService(TeamValidationService teamValidator, QuarterValidationService quarterValidator) {
+        this.teamValidator = teamValidator;
+        this.quarterValidator = quarterValidator;
+    }
+
+    public void validateTeam(Long id) {
+        teamValidator.validateOnGet(id);
+        teamValidator.doesEntityExist(id);
+    }
+
+    public void validateQuarter(Long id) {
+        quarterValidator.validateOnGet(id);
+        quarterValidator.doesEntityExist(id);
+    }
+
+    public void validateOnGet(Long quarterId, List<Long> teamIds) {
+        validateQuarter(quarterId);
+        teamIds.forEach(this::validateTeam);
+    }
+}

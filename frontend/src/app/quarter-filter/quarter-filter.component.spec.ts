9e8f55bb176243fd9ae5afd65518c1d7a020c1b4
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-13 08:59:35 +0200

    add missing import to quarter-filter test

--- frontend/src/app/quarter-filter/quarter-filter.component.spec.ts
+++ frontend/src/app/quarter-filter/quarter-filter.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuarterFilterComponent } from './quarter-filter.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('QuarterFilterComponent', () => {
   let component: QuarterFilterComponent;
@@ -9,6 +10,7 @@ describe('QuarterFilterComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [QuarterFilterComponent],
+      imports: [HttpClientTestingModule],
     });
     fixture = TestBed.createComponent(QuarterFilterComponent);
     component = fixture.componentInstance;

bd25cd6b43a9fbef41a61ae63f8d1361d2964700
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-13 13:35:03 +0200

    write tests for quarter filter

--- frontend/src/app/quarter-filter/quarter-filter.component.spec.ts
+++ frontend/src/app/quarter-filter/quarter-filter.component.spec.ts
@@ -5,28 +5,32 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OverviewService } from '../shared/services/overview.service';
 import { By } from '@angular/platform-browser';
 import { quarter } from '../shared/testData';
+import { Observable, of, Subject } from 'rxjs';
+import { Quarter } from '../shared/types/model/Quarter';
+import { QuarterService } from '../shared/services/quarter.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 const overviewService = {
   getOverview: jest.fn(),
 };
 
 const quarterService = {
-  getAllQuarters() {
-    return [quarter];
+  getAllQuarters(): Observable<Quarter[]> {
+    return of([quarter]);
   },
 };
 
 describe('QuarterFilterComponent', () => {
   let component: QuarterFilterComponent;
   let fixture: ComponentFixture<QuarterFilterComponent>;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [QuarterFilterComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [
         { provide: OverviewService, useValue: overviewService },
-        { provide: quarterService, useValue: quarterService },
+        { provide: QuarterService, useValue: quarterService },
       ],
     });
     fixture = TestBed.createComponent(QuarterFilterComponent);
@@ -43,5 +47,7 @@ describe('QuarterFilterComponent', () => {
     expect(quarterSelect).toBeTruthy();
     component.ngOnInit();
     fixture.detectChanges();
+    expect(component.quarterId).toBe(quarter.id);
+    expect(+quarterSelect.options[quarterSelect.selectedIndex].value).toBe(quarter.id);
   });
 });

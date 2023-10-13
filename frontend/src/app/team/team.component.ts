eb1e9c1df36576eeff277a7262c229d3fbbda83e
megli2 <egli@puzzle.ch>
2023-10-13 08:10:02 +0200

    fix creating keyresults after objective created

--- frontend/src/app/team/team.component.ts
+++ frontend/src/app/team/team.component.ts
@@ -4,6 +4,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { ObjectiveFormComponent } from '../shared/dialog/objective-dialog/objective-form.component';
 import { BehaviorSubject } from 'rxjs';
 import { RefreshDataService } from '../shared/services/refresh-data.service';
+import { KeyResultDialogComponent } from '../key-result-dialog/key-result-dialog.component';
+import { Objective } from '../shared/types/model/Objective';
 
 @Component({
   selector: 'app-team',
@@ -33,8 +35,30 @@ export class TeamComponent {
       data: { teamId: this.overviewEntity.value.team.id },
       width: '850px',
     });
-    matDialogRef.afterClosed().subscribe(() => {
+    matDialogRef.afterClosed().subscribe((result) => {
+      if (result?.addKeyResult) {
+        this.openAddKeyResultDialog(result.objective);
+      }
       this.refreshDataService.markDataRefresh();
     });
+  }
+
+  openAddKeyResultDialog(objective: Objective) {
+    this.dialog
+      .open(KeyResultDialogComponent, {
+        width: '45em',
+        height: 'auto',
+        data: {
+          objective: objective,
+          keyResult: null,
+        },
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result?.openNew) {
+          this.openAddKeyResultDialog(objective);
+        }
+        this.refreshDataService.markDataRefresh();
+      });
   }
 }

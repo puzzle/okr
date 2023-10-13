5446284d51f2a467c9314f1dc54ec08d5d0e2871
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-13 13:53:00 +0200

    test if default overview is loaded in overview.component.spec.ts

--- frontend/src/app/overview/overview.component.spec.ts
+++ frontend/src/app/overview/overview.component.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OverviewComponent } from './overview.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { overViewEntity1 } from '../shared/testData';
+import { of } from 'rxjs';
+import { OverviewService } from '../shared/services/overview.service';
+
+const overviewService = {
+  getOverview() {
+    return of(overViewEntity1);
+  },
+};
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
@@ -11,6 +20,7 @@ describe('OverviewComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [OverviewComponent],
+      providers: [{ provide: OverviewService, useValue: overviewService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OverviewComponent);
@@ -20,5 +30,11 @@ describe('OverviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load default overview', () => {
+    jest.spyOn(overviewService, 'getOverview');
+    component.ngOnInit();
+    expect(overviewService.getOverview).toHaveBeenCalled();
   });
 });

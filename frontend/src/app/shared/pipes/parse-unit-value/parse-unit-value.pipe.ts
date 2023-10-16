b6f927ea20b5fa6196423964c8b1a6137eea21f7
megli2 <egli@puzzle.ch>
2023-10-16 08:26:35 +0200

    allow negative value in check-in form

--- frontend/src/app/shared/pipes/parse-unit-value/parse-unit-value.pipe.ts
+++ frontend/src/app/shared/pipes/parse-unit-value/parse-unit-value.pipe.ts
@@ -6,6 +6,9 @@ import { CHAR_REGEX } from '../../regexLibrary';
 })
 export class ParseUnitValuePipe implements PipeTransform {
   transform(value: string): number {
+    if (value.toString().at(0) == '-') {
+      return +('-' + value.toString().replace(CHAR_REGEX, ''));
+    }
     return +value.toString().replace(CHAR_REGEX, '');
   }
 }

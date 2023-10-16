9db06558223a9de90a107af88ad06f958a021642
Yanick Minder <minder@puzzle.ch>
2023-10-16 11:06:05 +0200

    implement filter logic

--- frontend/src/app/team-filter/team-filter.component.ts
+++ frontend/src/app/team-filter/team-filter.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of, Subject } from 'rxjs';
+import { Team } from '../shared/types/model/Team';
+import { TeamService } from '../shared/services/team.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotifierService } from '../shared/services/notifier.service';
 
 @Component({
   selector: 'app-team-filter',
   templateUrl: './team-filter.component.html',
   styleUrls: ['./team-filter.component.scss'],
 })
-export class TeamFilterComponent {}
+export class TeamFilterComponent implements OnInit {
+  teams$: BehaviorSubject<Team[]> = new BehaviorSubject<Team[]>([]);
+
+  constructor(
+    private teamService: TeamService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private notifierService: NotifierService,
+  ) {}
+  ngOnInit(): void {
+    this.teamService.getAllTeams().subscribe((teams) => {
+      this.route.queryParams.subscribe((params) => {
+        const teamIds = params['teams'];
+        const selectedTeams = teams.filter((team) => teamIds?.includes(team.id));
+        this.teams$.next(selectedTeams);
+      });
+    });
+  }
+
+  changeTeamFilter() {
+    const teamIds = this.teams$.getValue().map((team) => team.id);
+    this.router.navigate([], { queryParams: { teams: teamIds }, queryParamsHandling: 'merge' }).then(() => {
+      this.notifierService.reloadOverview.next(null);
+    });
+  }
+}

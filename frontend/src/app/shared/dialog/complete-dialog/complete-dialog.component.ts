623e49df4d83d4361776b832a23ec41ad34e3007
Lias Kleisa <kleisa@puzzle.ch>
2023-10-19 11:25:08 +0200

    Style complete dialog

--- frontend/src/app/shared/dialog/complete-dialog/complete-dialog.component.ts
+++ frontend/src/app/shared/dialog/complete-dialog/complete-dialog.component.ts
@@ -8,23 +8,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./complete-dialog.component.scss'],
 })
 export class CompleteDialogComponent {
-  isSuccessful: boolean = true;
   completeForm = new FormGroup({
+    isSuccessful: new FormControl<boolean | null>(null, [Validators.required]),
     comment: new FormControl<string>('', [Validators.maxLength(4096)]),
   });
 
   constructor(public dialogRef: MatDialogRef<CompleteDialogComponent>) {}
 
   switchSuccessState(input: string) {
-    if ((input == 'successful' && !this.isSuccessful) || (input == 'notSuccessful' && this.isSuccessful)) {
-      this.isSuccessful = !this.isSuccessful;
+    this.removeStandardHover();
+    let successfulValue = this.completeForm.value.isSuccessful;
+    if (
+      successfulValue == null ||
+      (input == 'successful' && !successfulValue) ||
+      (input == 'notSuccessful' && successfulValue)
+    ) {
+      successfulValue = input == 'successful';
+      this.completeForm.patchValue({ isSuccessful: successfulValue });
     }
   }
 
   closeDialog() {
     this.dialogRef.close({
-      endState: this.isSuccessful ? 'SUCCESSFUL' : 'NOTSUCCESSFUL',
+      endState: this.completeForm.value.isSuccessful ? 'SUCCESSFUL' : 'NOTSUCCESSFUL',
       comment: this.completeForm.value.comment,
+    });
+  }
+
+  removeStandardHover() {
+    let elements = document.querySelectorAll('.card-hover');
+    elements.forEach((el) => {
+      el.classList.remove('card-hover');
     });
   }
 }

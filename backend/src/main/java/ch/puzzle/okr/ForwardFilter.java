bdc7fee2e5f028f9a3266459c39e9c904b366dd6
Yanick Minder <minder@puzzle.ch>
2023-10-20 13:28:58 +0200

    fix redirect whitelabel error

--- backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
+++ backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
@@ -7,19 +7,27 @@ import org.springframework.stereotype.Component;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
 
 @Component
 public class ForwardFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(ForwardFilter.class);
+    private final String[] allowedRoutes = { "/keyresult", "/objective" };
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String path = new URL(request.getRequestURL().toString()).getPath();
         if (request.getParameter("state") != null) {
             logger.info(String.format("Keycloak state parameter detected ====> make a forward from '%s' to '%s'",
                     request.getRequestURI(), "/"));
+            servletRequest.getRequestDispatcher("/").forward(servletRequest, servletResponse);
+            return;
+        }
+        if (Arrays.stream(this.allowedRoutes).anyMatch(path::startsWith)) {
             servletRequest.getRequestDispatcher("/").forward(servletRequest, servletResponse);
             return;
         }

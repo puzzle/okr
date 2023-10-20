507b16106a97717b33e129700a8baa1ea6d80800
peggimann <peggimann@puzzle.com>
2023-10-20 15:13:53 +0200

    implement cron job for ldap query

--- backend/src/main/resources/db/migration/V2_0_7__createAuthorizationOrganisation.sql
+++ backend/src/main/resources/db/migration/V2_0_7__createAuthorizationOrganisation.sql
@@ -1,13 +1,13 @@
 create table if not exists organisation
 (
-    id bigint not null,
+    id       bigint       not null,
     org_name varchar(255) not null,
     primary key (id)
-    );
+);
 
 create table if not exists team_organisation
 (
-    team_id bigint not null,
+    team_id         bigint not null,
     organisation_id bigint not null,
     constraint fkkya7xh83whc05etnjbndf9rkf
         foreign key (organisation_id) references organisation,

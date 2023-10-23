5965c37df06dcbff13808c920e9d4836c9e4b35e
Yanick Minder <minder@puzzle.ch>
2023-10-23 16:42:11 +0200

    finish styling

--- frontend/src/app/objective-filter/objective-filter.component.ts
+++ frontend/src/app/objective-filter/objective-filter.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RefreshDataService } from '../shared/services/refresh-data.service';
+import { optionalReplaceWithNulls } from '../shared/common';
 
 @Component({
   selector: 'app-objective-filter',
@@ -20,18 +21,18 @@ export class ObjectiveFilterComponent implements OnInit {
   ) {}
 
   updateURL() {
-    this.router
-      .navigate([], { queryParams: { objectiveQuery: this.form.value.objectiveControl } })
-      .then(() => this.refreshService.markDataRefresh());
+    const params = { objectiveQuery: this.form.value.objectiveControl };
+    const optionalParams = optionalReplaceWithNulls(params);
+    this.router.navigate([], { queryParams: optionalParams }).then(() => this.refreshService.markDataRefresh());
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       const objectiveQuery = (params['objectiveQuery'] as string) || '';
-      // if (this.form.value.objectiveControl?.toLowerCase().trim() !== objectiveQuery.toLowerCase().trim()) {
-      this.form.controls.objectiveControl.setValue(objectiveQuery);
-      // this.updateURL();
-      // }
+      if (this.form.value.objectiveControl?.toLowerCase().trim() !== objectiveQuery.toLowerCase().trim()) {
+        this.form.controls.objectiveControl.setValue(objectiveQuery);
+        this.updateURL();
+      }
     });
   }
 }

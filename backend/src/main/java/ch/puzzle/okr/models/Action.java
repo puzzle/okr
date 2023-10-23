1fa6f4bc4f05257ee8e20352af2bf1e9339028b7
Lias Kleisa <kleisa@puzzle.ch>
2023-10-23 11:52:02 +0200

    Add new model action

---
+++ backend/src/main/java/ch/puzzle/okr/models/Action.java
@@ -0,0 +1,142 @@
+package ch.puzzle.okr.models;
+
+import ch.puzzle.okr.models.keyresult.KeyResult;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+public class Action {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "sequence_action")
+    private Long id;
+
+    @NotNull(message = "Action must not be null")
+    @Size(max = 4096, message = "Attribute Action has a max length of 4096 characters when creating new Action")
+    private String action;
+
+    @NotNull(message = "Priority must not be null")
+    private int priority;
+
+    @NotNull(message = "IsChecked must not be null")
+    private boolean isChecked;
+
+    @NotNull(message = "KeyResult must not be null")
+    @ManyToOne
+    private KeyResult keyResult;
+
+    public Action() {
+    }
+
+    private Action(Builder builder) {
+        id = builder.id;
+        action = builder.action;
+        priority = builder.priority;
+        isChecked = builder.isChecked;
+        keyResult = builder.keyResult;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    public KeyResult getKeyResult() {
+        return keyResult;
+    }
+
+    public void setKeyResult(KeyResult keyResult) {
+        this.keyResult = keyResult;
+    }
+
+    @Override
+    public String toString() {
+        return "Action{" + "id=" + id + ", action='" + action + '\'' + ", priority=" + priority + ", isChecked="
+                + isChecked + ", keyResult=" + keyResult + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Action action1 = (Action) o;
+        return priority == action1.priority && isChecked == action1.isChecked && Objects.equals(id, action1.id)
+                && Objects.equals(action, action1.action) && Objects.equals(keyResult, action1.keyResult);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, action, priority, isChecked, keyResult);
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String action;
+        private int priority;
+        private boolean isChecked;
+        private KeyResult keyResult;
+
+        private Builder() {
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        public Builder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withAction(String action) {
+            this.action = action;
+            return this;
+        }
+
+        public Builder withPriority(int priority) {
+            this.priority = priority;
+            return this;
+        }
+
+        public Builder withIsChecked(boolean isChecked) {
+            this.isChecked = isChecked;
+            return this;
+        }
+
+        public Builder withKeyResult(KeyResult keyResult) {
+            this.keyResult = keyResult;
+            return this;
+        }
+
+        public Action build() {
+            return new Action(this);
+        }
+    }
+}

6a34435e94847240c114f91f49b29cab3e0e137f
Yanick Minder <minder@puzzle.ch>
2023-10-23 11:25:23 +0200

    add objective filter component and integrate into banner

---
+++ frontend/src/app/objective-filter/objective-filter.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ObjectiveFilterComponent } from './objective-filter.component';
+
+describe('ObjectiveFilterComponent', () => {
+  let component: ObjectiveFilterComponent;
+  let fixture: ComponentFixture<ObjectiveFilterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ObjectiveFilterComponent]
+    });
+    fixture = TestBed.createComponent(ObjectiveFilterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/objective-filter/objective-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-objective-filter',
+  templateUrl: './objective-filter.component.html',
+  styleUrls: ['./objective-filter.component.scss']
+})
+export class ObjectiveFilterComponent {
+
+}

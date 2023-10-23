a6d148125a6651a85d05358fa9b2a9096276c086
Yanick Minder <minder@puzzle.ch>
2023-10-23 13:52:35 +0200

    add filter logic

--- frontend/src/app/objective-filter/objective-filter.component.ts
+++ frontend/src/app/objective-filter/objective-filter.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RefreshDataService } from '../shared/services/refresh-data.service';
 
 @Component({
   selector: 'app-objective-filter',
   templateUrl: './objective-filter.component.html',
   styleUrls: ['./objective-filter.component.scss'],
 })
-export class ObjectiveFilterComponent {
-  objectiveControl = new FormControl('', [Validators.required, Validators.email]);
+export class ObjectiveFilterComponent implements OnInit {
+  objectiveControl = new FormControl<string>('');
 
-  submit() {
-    console.log('test');
+  constructor(
+    private router: Router,
+    private refreshService: RefreshDataService,
+    private route: ActivatedRoute,
+  ) {}
+
+  updateURL() {
+    this.router
+      .navigate([], { queryParams: { objectiveQuery: this.objectiveControl.value } })
+      .then(() => this.refreshService.markDataRefresh());
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const objectiveQuery = (params['objectiveQuery'] as string) || '';
+      if (this.objectiveControl.value?.toLowerCase().trim() !== objectiveQuery.toLowerCase().trim()) {
+        console.log(objectiveQuery);
+        this.objectiveControl.patchValue(objectiveQuery);
+        console.log(this.objectiveControl.value);
+        this.updateURL();
+      }
+    });
   }
 }

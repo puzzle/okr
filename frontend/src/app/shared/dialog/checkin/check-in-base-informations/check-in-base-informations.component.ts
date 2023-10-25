5000a415762e81313591305911b266498c1831dc
Lias Kleisa <kleisa@puzzle.ch>
2023-10-25 16:40:42 +0200

    Add checkboxes to check in dialog

--- frontend/src/app/shared/dialog/checkin/check-in-base-informations/check-in-base-informations.component.ts
+++ frontend/src/app/shared/dialog/checkin/check-in-base-informations/check-in-base-informations.component.ts
@@ -1,17 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import errorMessages from '../../../../../assets/errors/error-messages.json';
+import { KeyResult } from '../../../types/model/KeyResult';
+import { ActionService } from '../../../services/action.service';
+import { Action } from '../../../types/model/Action';
+import { Observable } from 'rxjs';
 import { formInputCheck } from '../../../common';
 
 @Component({
   selector: 'app-check-in-base-informations',
   templateUrl: './check-in-base-informations.component.html',
 })
-export class CheckInBaseInformationsComponent {
+export class CheckInBaseInformationsComponent implements OnInit {
   @Input()
   dialogForm!: FormGroup;
+  @Input() keyresult!: KeyResult;
+  actionPoints!: Observable<Action[]>;
   protected readonly errorMessages: any = errorMessages;
   protected readonly formInputCheck = formInputCheck;
+
+  constructor(private actionService: ActionService) {}
+
+  ngOnInit() {
+    this.actionPoints = this.actionService.getActionsFromKeyResult(this.keyresult.id);
+  }
 
   isTouchedOrDirty(name: string) {
     return this.dialogForm.get(name)?.dirty || this.dialogForm.get(name)?.touched;

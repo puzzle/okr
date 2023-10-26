61cee124596b1bedc7f246a561a854c66eb19cf9
Yanick Minder <minder@puzzle.ch>
2023-10-26 13:20:13 +0200

    update tests

--- backend/src/test/java/ch/puzzle/okr/service/persistence/OverviewPersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/persistence/OverviewPersistenceServiceIT.java
@@ -31,7 +31,7 @@ class OverviewPersistenceServiceIT {
         List<Overview> overviews = overviewPersistenceService.getOverviewByQuarterAndTeamsAndObjectiveQuery(2L,
                 List.of(5L, 6L, 8L), "kundenzufriedenheit");
 
-        assertEquals(4, overviews.size());
+        assertEquals(3, overviews.size());
         assertIterableEquals(overviewIds_getExpectedOverviewIds_getOverviewByQuarterIdAndTeamIdsShouldReturnOverviews(),
                 getOverviewIds(overviews));
     }
@@ -50,25 +50,14 @@ class OverviewPersistenceServiceIT {
     }
 
     private static List<OverviewId> overviewIds_getOverviewByQuarterIdAndTeamIdsAndObjectiveQueryShouldReturnOverviews() {
-        return List.of(OverviewId.of(5L, 3L, 3L, 9L), //
-                OverviewId.of(5L, 3L, 4L, 8L), //
-                OverviewId.of(5L, 3L, 5L, 7L), //
-                OverviewId.of(5L, 4L, 6L, 5L), //
-                OverviewId.of(5L, 4L, 7L, 4L), //
-                OverviewId.of(5L, 4L, 8L, 2L), //
-                OverviewId.of(6L, 8L, 18L, 20L), //
-                OverviewId.of(6L, 8L, 19L, 19L), //
-                OverviewId.of(6L, 9L, 15L, 18L), //
-                OverviewId.of(6L, 9L, 16L, 17L), //
-                OverviewId.of(6L, 9L, 17L, 16L), //
-                OverviewId.of(6L, 10L, -1L, -1L), //
+        return List.of(OverviewId.of(5L, 3L, 3L, 9L), OverviewId.of(5L, 3L, 4L, 8L), OverviewId.of(5L, 3L, 5L, 7L),
+                OverviewId.of(5L, 4L, 6L, 5L), OverviewId.of(5L, 4L, 7L, 4L), OverviewId.of(5L, 4L, 8L, 2L),
+                OverviewId.of(6L, 8L, 18L, 20L), OverviewId.of(6L, 8L, 19L, 19L), OverviewId.of(6L, 9L, 15L, 18L),
+                OverviewId.of(6L, 9L, 16L, 17L), OverviewId.of(6L, 9L, 17L, 16L), OverviewId.of(6L, 10L, -1L, -1L),
                 OverviewId.of(8L, -1L, -1L, -1L));
     }
 
     private static List<OverviewId> overviewIds_getExpectedOverviewIds_getOverviewByQuarterIdAndTeamIdsShouldReturnOverviews() {
-        return List.of(OverviewId.of(5L, 3L, 3L, 9L), //
-                OverviewId.of(5L, 3L, 4L, 8L), //
-                OverviewId.of(5L, 3L, 5L, 7L), //
-                OverviewId.of(8L, -1L, -1L, -1L));
+        return List.of(OverviewId.of(5L, 3L, 3L, 9L), OverviewId.of(5L, 3L, 4L, 8L), OverviewId.of(5L, 3L, 5L, 7L));
     }
 }

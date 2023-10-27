42265d05f1e596cfa07940047a92981e9e2cb6c7
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-27 07:58:05 +0200

    delete empty scss files

--- frontend/src/app/shared/custom/dialog-header/dialog-header.component.ts
+++ frontend/src/app/shared/custom/dialog-header/dialog-header.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-dialog-header',
   templateUrl: './dialog-header.component.html',
-  styleUrls: ['./dialog-header.component.scss'],
 })
 export class DialogHeaderComponent {
   @Input()

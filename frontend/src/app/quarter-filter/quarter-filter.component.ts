9aa25c92b46c43cbd1dd3d11f7d1eab20ed550d5
Jannik Pulfer <pulfer@puzzle.ch>
2023-10-31 17:03:46 +0100

    fix one test and start writing test to test filtering out of state param

--- frontend/src/app/quarter-filter/quarter-filter.component.ts
+++ frontend/src/app/quarter-filter/quarter-filter.component.ts
@@ -27,6 +27,7 @@ export class QuarterFilterComponent implements OnInit {
       const quarterId: number = getValueFromQuery(quarterQuery)[0];
       if (quarters.map((quarter) => quarter.id).includes(quarterId)) {
         this.quarterId = quarterId;
+        this.changeDisplayedQuarter();
       } else {
         this.quarterId = quarters[0].id;
         if (quarterId !== undefined) {

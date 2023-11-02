90d3cf8212665e87cc379d62fcfe70d763ba1842
Yanick Minder <minder@puzzle.ch>
2023-11-02 09:04:02 +0100

    add overview test

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -5,7 +5,7 @@ describe('OKR Login', () => {
     cy.loginAsUser(users.gl);
   });
 
-  it.skip('Login and check correct name is displayed', () => {
+  it('Login and check correct name is displayed', () => {
     cy.title().should('equal', 'Puzzle OKR');
     cy.get("pzsh-menu-dropdown > div[slot='toggle']").contains(users.gl.name);
   });
@@ -13,9 +13,7 @@ describe('OKR Login', () => {
   it('Login  and logout', () => {
     cy.title().should('equal', 'Puzzle OKR');
     cy.get("pzsh-menu-dropdown > div[slot='toggle']").click();
-    cy.wait(500);
     cy.getByTestId('logout').click();
-    cy.wait(2000);
     cy.origin('https://idp-mock-okr.ocp-internal.cloudscale.puzzle.ch', () => {
       cy.url().should('include', 'https://idp-mock-okr.ocp-internal.cloudscale.puzzle.ch');
       cy.get('#kc-page-title').contains('Sign in to your account');

36cc7970bda42c3da551981a71b4117940db56df
Lias Kleisa <kleisa@puzzle.ch>
2023-11-02 09:24:48 +0100

    Fix ActionControllerIT

--- backend/src/test/java/ch/puzzle/okr/controller/ActionControllerIT.java
+++ backend/src/test/java/ch/puzzle/okr/controller/ActionControllerIT.java
@@ -1,9 +1,13 @@
 package ch.puzzle.okr.controller;
 
 import ch.puzzle.okr.mapper.ActionMapper;
+import ch.puzzle.okr.models.Action;
+import ch.puzzle.okr.models.keyresult.KeyResultMetric;
 import ch.puzzle.okr.service.authorization.ActionAuthorizationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,49 +20,79 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @WithMockUser(value = "spring")
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(ActionController.class)
 class ActionControllerIT {
     public static final String SUCCESSFUL_UPDATE_BODY = """
-            {
-                [
-                    {
-                        "id":1,
-                        "action":"Neuer Drucker",
-                        "priority":0,
-                        "isChecked":true,
-                        "keyResultId":8
-                    },
-                    {
-                        "id":2,
-                        "action":"Neues Papier",
-                        "priority":1,
-                        "isChecked":false,
-                        "keyResultId":8
-                    }
-                ]
-
-            }
+            [
+                {
+                    "id":1,
+                    "action":"Neuer Drucker",
+                    "priority":0,
+                    "isChecked":true,
+                    "keyResultId":8
+                },
+                {
+                    "id":2,
+                    "action":"Neues Papier",
+                    "priority":1,
+                    "isChecked":false,
+                    "keyResultId":8
+                }
+            ]
+            """;
+    public static final String SUCCESSFUL_UPDATE_BODY_SINGLE_ACTION = """
+            [
+                {
+                    "id":1,
+                    "action":"Neuer Drucker",
+                    "priority":0,
+                    "isChecked":true,
+                    "keyResultId":8
+                }
+            ]
             """;
     @MockBean
     ActionAuthorizationService actionAuthorizationService;
     @MockBean
     ActionMapper actionMapper;
-    String baseUrl = "/api/v2/action";
     @Autowired
     private MockMvc mvc;
 
+    @BeforeEach
+    void setUp() {
+        Action action = Action.Builder.builder().withId(3L).withAction("Neues Haus").withPriority(1).withIsChecked(true)
+                .withKeyResult(KeyResultMetric.Builder.builder().withId(10L).withTitle("KR Title").build()).build();
+        BDDMockito.given(actionMapper.toActions(any())).willReturn(List.of(action, action));
+    }
+
     @Test
-    void updateSuccessfulAction() throws Exception {
-        mvc.perform(post(baseUrl).content(SUCCESSFUL_UPDATE_BODY).contentType(MediaType.APPLICATION_JSON)
+    void updateSuccessfulActions() throws Exception {
+        mvc.perform(put("/api/v2/action").content(SUCCESSFUL_UPDATE_BODY).contentType(MediaType.APPLICATION_JSON)
                 .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        verify(actionMapper, times(1)).toActions(any());
+        verify(actionAuthorizationService, times(1)).updateEntities(any());
+    }
+
+    @Test
+    void updateSuccessfulOnlyOneAction() throws Exception {
+        mvc.perform(put("/api/v2/action").content(SUCCESSFUL_UPDATE_BODY_SINGLE_ACTION)
+                .contentType(MediaType.APPLICATION_JSON).with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+
+        verify(actionMapper, times(1)).toActions(any());
+        verify(actionAuthorizationService, times(1)).updateEntities(any());
     }
 
     @Test

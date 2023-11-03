3f7c8d1668eedf69a90f818c4063e84a0cba61aa
megli2 <egli@puzzle.ch>
2023-11-03 15:27:33 +0100

    remove unused imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { map } from 'rxjs';
-import { OAuthService } from 'angular-oauth2-oidc';
 import { ConfigService } from './config.service';
-import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',

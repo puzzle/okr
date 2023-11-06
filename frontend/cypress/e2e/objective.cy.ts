92a55bce887eb61c68c9bc50e267f5a964900ca3
Yanick Minder <minder@puzzle.ch>
2023-11-06 13:20:39 +0100

    combine in parametrized test

--- frontend/cypress/e2e/objective.cy.ts
+++ frontend/cypress/e2e/objective.cy.ts
@@ -6,29 +6,20 @@ describe('OKR Objective e2e tests', () => {
       cy.loginAsUser(users.gl);
     });
 
-    it('Create ongoing objective, no keyresults', () => {
-      const objectiveTitle = 'This is a new ongoing objective by cypress';
-      cy.getByTestId('create-objective').first().click();
-      cy.getByTestId('title').first().type(objectiveTitle);
-      cy.getByTestId('description').first().type('This is the description of the new Objective');
-      cy.get('select#quarter').select('GJ 22/23-Q3');
-      cy.getByTestId('safe').click();
-      cy.visit('/?quarter=3');
-      cy.contains(objectiveTitle);
-      const objective = cy.contains(objectiveTitle).first().parentsUntil('#objective-column').last();
-      objective.getByTestId('objective-state').should('have.attr', 'src', 'assets/icons/ongoing-icon.svg');
-    });
-
-    it('Create draft objective, no keyresults', () => {
-      const objectiveTitle = 'This is a new draft objective by cypress';
-      cy.getByTestId('create-objective').first().click();
-      cy.getByTestId('title').first().type(objectiveTitle);
-      cy.getByTestId('description').first().type('This is the description of the new Objective');
-      cy.get('select#quarter').select('GJ 22/23-Q3');
-      cy.getByTestId('safe-draft').click();
-      cy.visit('/?quarter=3');
-      const objective = cy.contains(objectiveTitle).first().parentsUntil('#objective-column').last();
-      objective.getByTestId('objective-state').should('have.attr', 'src', 'assets/icons/draft-icon.svg');
+    [
+      ['ongoing objective title', 'safe', 'ongoing-icon.svg'],
+      ['draft objective title', 'safe-draft', 'draft-icon.svg'],
+    ].forEach(([objectiveTitle, buttonTestId, icon]) => {
+      it(`Create ${buttonTestId} objective, no keyresults`, () => {
+        cy.getByTestId('create-objective').first().click();
+        cy.getByTestId('title').first().type(objectiveTitle);
+        cy.getByTestId('description').first().type('This is the description of the new Objective');
+        cy.get('select#quarter').select('GJ 22/23-Q3');
+        cy.getByTestId(buttonTestId).click();
+        cy.visit('/?quarter=3');
+        const objective = cy.contains(objectiveTitle).first().parentsUntil('#objective-column').last();
+        objective.getByTestId('objective-state').should('have.attr', 'src', `assets/icons/${icon}`);
+      });
     });
   });
 

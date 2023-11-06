df27373189536ff087c328c66c6921fd8e676457
Jannik Pulfer <pulfer@puzzle.ch>
2023-11-06 14:49:49 +0100

    increase cypress test timeout

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
+    defaultCommandTimeout: 10000,
   },
   env: {
     login_url: 'https://idp-mock-okr.ocp.cloudscale.puzzle.ch',

5f35a8e79d2c6cba823ad4eee423c6d29bb9e141
Jannik Pulfer <pulfer@puzzle.ch>
2023-11-07 13:20:37 +0100

    remove defaultCommandTimeout from cypress config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    defaultCommandTimeout: 10000,
   },
   env: {
     login_url: 'https://idp-mock-okr.ocp.cloudscale.puzzle.ch',

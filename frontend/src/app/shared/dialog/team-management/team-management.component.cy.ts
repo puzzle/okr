63fbac3b5d77f6f37e8e9ddd98931791b92b9280
megli2 <egli@puzzle.ch>
2023-11-07 16:50:05 +0100

    implement dialog which opens on click to header section

---
+++ frontend/src/app/shared/dialog/team-management/team-management.component.cy.ts
@@ -0,0 +1,7 @@
+import { TeamManagementComponent } from './team-management.component'
+
+describe('TeamManagementComponent', () => {
+  it('should mount', () => {
+    cy.mount(TeamManagementComponent)
+  })
+})

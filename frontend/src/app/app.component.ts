b1d0ee909dcb7dba2efd46d6dd2949bfd2cf2fbd
Jannik Pulfer <pulfer@puzzle.ch>
2023-11-07 12:47:51 +0100

    remove code thats unwanted after rebase in app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,39 +11,4 @@ import { username } from './shared/common';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
-  constructor(
-    public router: Router,
-    private oauthService: OAuthService,
-    private configService: ConfigService,
-  ) {
-    // Try to login via url state
-    oauthService
-      .loadDiscoveryDocumentAndTryLogin()
-      .then(() => {
-        // if the login failed initialize code flow
-        if (!oauthService.hasValidAccessToken()) {
-          this.oauthService.initCodeFlow();
-        }
-        oauthService.setupAutomaticSilentRefresh();
-        location.hash = '';
-      })
-      .then(() => {
-        if (this.oauthService.hasValidIdToken()) {
-          username.next(this.oauthService.getIdentityClaims()['name']);
-        }
-      });
-  }
-
-  ngOnInit(): void {
-    this.configService.config$
-      .pipe(
-        map((config) => {
-          if (config.activeProfile === 'staging') {
-            document.getElementById('pzsh-topbar')!.style.backgroundColor = '#ab31ad';
-          }
-        }),
-      )
-      .subscribe();
-  }
-}
+export class AppComponent {}

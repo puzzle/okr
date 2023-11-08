bfcd581f10bedd67f23a3e23346041a48b5b2cad
megli2 <egli@puzzle.ch>
2023-11-08 10:45:55 +0100

    fix frontend service test by inserting httpclienttesting module

--- frontend/src/app/shared/services/organisation.service.spec.ts
+++ frontend/src/app/shared/services/organisation.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OrganisationService } from './organisation.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('OrganisationService', () => {
   let service: OrganisationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(OrganisationService);
   });
 

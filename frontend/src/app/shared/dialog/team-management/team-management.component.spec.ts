b0f6daed3cb7e7b26bf8ec94f4c65b747db58c94
megli2 <egli@puzzle.ch>
2023-11-08 15:53:01 +0100

    fix test by rewriting it to unit test

--- frontend/src/app/shared/dialog/team-management/team-management.component.spec.ts
+++ frontend/src/app/shared/dialog/team-management/team-management.component.spec.ts
@@ -1,7 +1,52 @@
 import { TeamManagementComponent } from './team-management.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DialogHeaderComponent } from '../../custom/dialog-header/dialog-header.component';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+
+const dialogMock = {
+  close: jest.fn(),
+};
 
 describe('TeamManagementComponent', () => {
-  it('should mount', () => {
-    cy.mount(TeamManagementComponent);
+  let component: TeamManagementComponent;
+  let fixture: ComponentFixture<TeamManagementComponent>;
+  let loader: HarnessLoader;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatDialogModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        NoopAnimationsModule,
+        MatCheckboxModule,
+        RouterTestingModule,
+      ],
+      declarations: [TeamManagementComponent, DialogHeaderComponent],
+      providers: [{ provide: MatDialogRef, useValue: dialogMock }],
+    });
+    fixture = TestBed.createComponent(TeamManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 });

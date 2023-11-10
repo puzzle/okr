b671ad68259fc32b1bfcef72698b6fb1c74d6922
Jannik Pulfer <pulfer@puzzle.ch>
2023-11-10 15:52:19 +0100

    make test in scoring component tests create ordinal keyresult

--- frontend/cypress/e2e/scoring.cy.ts
+++ frontend/cypress/e2e/scoring.cy.ts
@@ -5,12 +5,26 @@ describe('Scoring component e2e tests', () => {
     cy.loginAsUser(users.gl);
   });
 
-  it('Creates metric checkin', () => {
-    cy.fillMetricKeyResult('Metric scoring keyresult');
-    cy.submitKeyresultForm();
+  it('Create metric checkin and validate value of scoring component', () => {
+    cy.createMetricKeyresult('Metric scoring keyresult');
     cy.getByTestId('keyresult').get(':contains("Metric scoring keyresult")').last().click();
     cy.getByTestId('add-check-in').click();
     cy.getByTestId('key-result-metric-value').clear().type('30');
+    cy.tabForward();
+    cy.realPress('{rightarrow}').realPress('{rightarrow}').realPress('{rightarrow}');
+    cy.getByTestId('continue-checkin').click();
+    cy.getByTestId('changeInfo').click().type('Testveränderungen');
+    cy.getByTestId('initiatives').click().type('Testmassnahmen');
+    cy.getByTestId('create-checkin').click();
+  });
+
+  it('Create ordinal checkin and validate value of scoring component', () => {
+    cy.createOrdinalKeyresult('Ordinal scoring keyresult', null);
+    cy.getByTestId('keyresult').get(':contains("Ordinal scoring keyresult")').last().click();
+    cy.getByTestId('add-check-in').click();
+    cy.tabForward();
+    cy.realPress('{downarrow}').realPress('{downarrow}');
+    cy.getByTestId('target-zone').click();
     cy.tabForward();
     cy.realPress('{rightarrow}').realPress('{rightarrow}').realPress('{rightarrow}');
     cy.getByTestId('continue-checkin').click();

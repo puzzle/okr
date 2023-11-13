378bf76c43c7a623081cb6f362b9bcc89c0e93ee
megli2 <egli@puzzle.ch>
2023-11-13 16:16:31 +0100

    refactor structure

--- frontend/src/app/shared/dialog/team-management/team-management.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { TeamManagementComponent } from './team-management.component'
-
-describe('TeamManagementComponent', () => {
-  it('should mount', () => {
-    cy.mount(TeamManagementComponent)
-  })
-})

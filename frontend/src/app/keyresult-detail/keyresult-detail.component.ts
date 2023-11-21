093d30b8bddba45ac108b0a655383e1b76fe3973
megli2 <egli@puzzle.ch>
2023-11-21 11:51:41 +0100

    format dialog component

--- frontend/src/app/keyresult-detail/keyresult-detail.component.ts
+++ frontend/src/app/keyresult-detail/keyresult-detail.component.ts
@@ -130,15 +130,15 @@ export class KeyresultDetailComponent implements OnInit {
   openCheckInForm() {
     const dialogConfig = isMobileDevice()
       ? {
-        maxWidth: '100vw',
-        maxHeight: '100vh',
-        height: '100vh',
-        width: '100vw',
-      }
+          maxWidth: '100vw',
+          maxHeight: '100vh',
+          height: '100vh',
+          width: '100vw',
+        }
       : {
-        width: '45em',
-        height: 'auto',
-      };
+          width: '45em',
+          height: 'auto',
+        };
     const dialogRef = this.dialog.open(CheckInFormComponent, {
       data: {
         keyResult: this.keyResult$.getValue(),

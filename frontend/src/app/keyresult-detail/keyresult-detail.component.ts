fdb69fae5dc018f0fb97c7043aaf5e596e5e9957
Lias Kleisa <kleisa@puzzle.ch>
2023-11-24 12:03:34 +0100

    Fix check in action buttons

--- frontend/src/app/keyresult-detail/keyresult-detail.component.ts
+++ frontend/src/app/keyresult-detail/keyresult-detail.component.ts
@@ -140,13 +140,13 @@ export class KeyresultDetailComponent implements OnInit {
           height: 'auto',
         };
     const dialogRef = this.dialog.open(CheckInFormComponent, {
-      data: {
-        keyResult: this.keyResult$.getValue(),
-      },
       height: dialogConfig.height,
       width: dialogConfig.width,
       maxHeight: dialogConfig.maxHeight,
       maxWidth: dialogConfig.maxWidth,
+      data: {
+        keyResult: this.keyResult$.getValue(),
+      },
     });
     dialogRef.afterClosed().subscribe(() => {
       this.loadKeyResult();

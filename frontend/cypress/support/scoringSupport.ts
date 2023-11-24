3dbe5b2d0364308c57e350cd7a9505adb7936b51
Jannik Pulfer <pulfer@puzzle.ch>
2023-11-24 16:34:17 +0100

    fix scoring component tests to test if scoring image is displayed

--- frontend/cypress/support/scoringSupport.ts
+++ frontend/cypress/support/scoringSupport.ts
@@ -53,7 +53,23 @@ function validateScoringWidth(zone: string, percent: number, isOverview: boolean
 function validateScoringColor(zone: string, rgbCode: string, isOverview: boolean) {
   cy.getZone(zone, isOverview).invoke('css', 'background-color').should('equal', rgbCode);
   if (rgbCode == 'rgba(0, 0, 0, 0)') {
-    cy.getZone(zone, isOverview).invoke('css', 'background-image').should('contain', 'scoring-stars.svg');
+    (isOverview ? cy.focused() : cy.getByTestId('side-panel'))
+      .getByTestId('star-scoring')
+      .invoke('css', 'display')
+      .should('equal', 'block');
+    (isOverview ? cy.focused() : cy.getByTestId('side-panel'))
+      .getByTestId('normal-scoring')
+      .invoke('css', 'display')
+      .should('equal', 'none');
+  } else {
+    (isOverview ? cy.focused() : cy.getByTestId('side-panel'))
+      .getByTestId('star-scoring')
+      .invoke('css', 'display')
+      .should('equal', 'none');
+    (isOverview ? cy.focused() : cy.getByTestId('side-panel'))
+      .getByTestId('normal-scoring')
+      .invoke('css', 'display')
+      .should('equal', 'block');
   }
 }
 

e121ac43714e9c5186cf19c1ea59bc698aeab82c
Janik Endtner <endtner@puzzle.ch>
2023-12-29 14:36:28 +0100

    #705 format

--- frontend/src/app/team-management/search-team-management/search-team-management.component.ts
+++ frontend/src/app/team-management/search-team-management/search-team-management.component.ts
@@ -1,10 +1,8 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-team-management',
   templateUrl: './search-team-management.component.html',
-  styleUrl: './search-team-management.component.scss'
+  styleUrl: './search-team-management.component.scss',
 })
-export class SearchTeamManagementComponent {
-
-}
+export class SearchTeamManagementComponent {}

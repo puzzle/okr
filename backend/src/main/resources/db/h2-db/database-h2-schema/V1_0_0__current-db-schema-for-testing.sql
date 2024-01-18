d6d3e44144859dcba8e402e45c32607f3df9e5e3
Lias Kleisa <kleisa@puzzle.ch>
2024-01-18 14:13:05 +0100

    Make overview view all uppercase for fixing tests

--- backend/src/main/resources/db/h2-db/database-h2-schema/V1_0_0__current-db-schema-for-testing.sql
+++ backend/src/main/resources/db/h2-db/database-h2-schema/V1_0_0__current-db-schema-for-testing.sql
@@ -160,35 +160,35 @@ create index if not exists idx_completed_objective
 
 DROP VIEW IF EXISTS OVERVIEW;
 CREATE VIEW OVERVIEW AS
-SELECT tq.team_id          AS "team_id",
-       tq.team_version     AS "team_version",
-       tq.name             AS "team_name",
-       tq.quater_id        AS "quarter_id",
-       tq.label            AS "quarter_label",
-       tq.start_date        AS "quarter_start_date",
-       tq.end_date          AS "quarter_end_date",
-       coalesce(o.id, -1)  AS "objective_id",
-       o.title             AS "objective_title",
-       o.state             AS "objective_state",
-       o.created_on        AS "objective_created_on",
-       coalesce(kr.id, -1) AS "key_result_id",
-       kr.title            AS "key_result_title",
-       kr.key_result_type  AS "key_result_type",
-       kr.unit,
-       kr.baseline,
-       kr.stretch_goal,
-       kr.commit_zone,
-       kr.target_zone,
-       kr.stretch_zone,
-       coalesce(c.id, -1)  AS "check_in_id",
-       c.value_metric      AS "check_in_value",
-       c.zone              AS "check_in_zone",
-       c.confidence,
-       c.created_on        AS "check_in_created_on"
-FROM (select t.id as team_id, t.version as team_version, t.name, q.id as quater_id, q.label, q.start_date, q.end_date
-      from team t,
-           quarter q) tq
-         LEFT JOIN OBJECTIVE O ON TQ.TEAM_ID = O.TEAM_ID AND TQ.QUATER_ID = O.QUARTER_ID
+SELECT TQ.TEAM_ID          AS "TEAM_ID",
+       TQ.TEAM_VERSION     AS "TEAM_VERSION",
+       TQ.NAME             AS "TEAM_NAME",
+       TQ.QUARTER_ID       AS "QUARTER_ID",
+       TQ.LABEL            AS "QUARTER_LABEL",
+       TQ.START_DATE       AS "QUARTER_START_DATE",
+       TQ.END_DATE         AS "QUARTER_END_DATE",
+       COALESCE(O.ID, -1)  AS "OBJECTIVE_ID",
+       O.TITLE             AS "OBJECTIVE_TITLE",
+       O.STATE             AS "OBJECTIVE_STATE",
+       O.CREATED_ON        AS "OBJECTIVE_CREATED_ON",
+       COALESCE(KR.ID, -1) AS "KEY_RESULT_ID",
+       KR.TITLE            AS "KEY_RESULT_TITLE",
+       KR.KEY_RESULT_TYPE  AS "KEY_RESULT_TYPE",
+       KR.UNIT,
+       KR.BASELINE,
+       KR.STRETCH_GOAL,
+       KR.COMMIT_ZONE,
+       KR.TARGET_ZONE,
+       KR.STRETCH_ZONE,
+       COALESCE(C.ID, -1)  AS "CHECK_IN_ID",
+       C.VALUE_METRIC      AS "CHECK_IN_VALUE",
+       C.ZONE              AS "CHECK_IN_ZONE",
+       C.CONFIDENCE,
+       C.CREATED_ON        AS "CHECK_IN_CREATED_ON"
+FROM (SELECT T.ID AS TEAM_ID, T.VERSION AS TEAM_VERSION, T.NAME, Q.ID AS QUARTER_ID, Q.LABEL, Q.START_DATE, Q.END_DATE
+      FROM TEAM T,
+           QUARTER Q) TQ
+         LEFT JOIN OBJECTIVE O ON TQ.TEAM_ID = O.TEAM_ID AND TQ.QUARTER_ID = O.QUARTER_ID
          LEFT JOIN KEY_RESULT KR ON O.ID = KR.OBJECTIVE_ID
          LEFT JOIN CHECK_IN C ON KR.ID = C.KEY_RESULT_ID AND C.MODIFIED_ON = (SELECT MAX(CC.MODIFIED_ON)
                                                                               FROM CHECK_IN CC

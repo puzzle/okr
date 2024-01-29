f424245bf7f8811ed02f2f2bd40038dd70cbcebb
Janik Endtner <endtner@puzzle.ch>
2024-01-29 10:47:15 +0100

    #704 set okr champion from application properties in registrationService instead of applicationListener

--- backend/src/main/java/ch/puzzle/okr/service/authorization/AuthorizationRegistrationService.java
+++ backend/src/main/java/ch/puzzle/okr/service/authorization/AuthorizationRegistrationService.java
@@ -3,6 +3,7 @@ package ch.puzzle.okr.service.authorization;
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.models.authorization.AuthorizationUser;
 import ch.puzzle.okr.service.business.UserBusinessService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,12 @@ import static ch.puzzle.okr.SpringCachingConfig.AUTHORIZATION_USER_CACHE;
 
 @Service
 public class AuthorizationRegistrationService {
+
+
+    private static final String DELIMITER = ",";
+
+    @Value("${okr.user.champion.emails}")
+    private String okrChampionEmails;
 
     private final UserBusinessService userBusinessService;
 
@@ -19,6 +26,19 @@ public class AuthorizationRegistrationService {
 
     @Cacheable(value = AUTHORIZATION_USER_CACHE, key = "#user.email")
     public AuthorizationUser updateOrAddAuthorizationUser(User user) {
-        return new AuthorizationUser(userBusinessService.getOrCreateUser(user));
+        var userCreated = userBusinessService.getOrCreateUser(user);
+        setOkrChampionFromProperties(userCreated);
+        return new AuthorizationUser(userCreated);
+    }
+
+    public void setOkrChampionFromProperties(User user) {
+        String[] championMails = okrChampionEmails.split(DELIMITER);
+        for (var mail : championMails) {
+            if (mail.trim().equals(user.getEmail())) {
+                user.setOkrChampion(true);
+                userBusinessService.saveUser(user);
+                return;
+            }
+        }
     }
 }
--- backend/src/test/java/ch/puzzle/okr/service/authorization/AuthorizationRegistrationServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/authorization/AuthorizationRegistrationServiceIT.java
@@ -2,6 +2,7 @@ package ch.puzzle.okr.service.authorization;
 
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.models.authorization.AuthorizationUser;
+import ch.puzzle.okr.service.persistence.UserPersistenceService;
 import ch.puzzle.okr.test.SpringIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,8 @@ import org.springframework.cache.CacheManager;
 
 import static ch.puzzle.okr.SpringCachingConfig.AUTHORIZATION_USER_CACHE;
 import static ch.puzzle.okr.TestHelper.defaultUser;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
 @SpringIntegrationTest
 class AuthorizationRegistrationServiceIT {
@@ -19,6 +21,9 @@ class AuthorizationRegistrationServiceIT {
     private CacheManager cacheManager;
     @Autowired
     private AuthorizationRegistrationService authorizationRegistrationService;
+    @Autowired
+    private UserPersistenceService userPersistenceService;
+
     private final User user = defaultUser(null);
 
     @AfterEach
@@ -35,5 +40,38 @@ class AuthorizationRegistrationServiceIT {
         Cache cache = cacheManager.getCache(AUTHORIZATION_USER_CACHE);
         assertNotNull(cache);
         assertNotNull(cache.get(user.getEmail(), AuthorizationUser.class));
+    }
+
+    @Test
+    void registerAuthorizationUser_shouldSetOkrChampionsCorrectly() {
+        var userRichard = User.Builder.builder().withFirstname("Richard").withLastname("Eberhard")
+                .withEmail("richard.eberhard@puzzle.ch").build();
+        var userMaria = User.Builder.builder().withFirstname("Maria").withLastname("Gerber")
+                .withEmail("maria.gerber@puzzle.ch").build();
+        var userAndrea = User.Builder.builder().withFirstname("Andrea").withLastname("Nydegger")
+                .withEmail("andrea.nydegger@puzzle.ch").build();
+
+        userPersistenceService.getOrCreateUser(userRichard);
+        userPersistenceService.getOrCreateUser(userMaria);
+        userPersistenceService.getOrCreateUser(userAndrea);
+
+        setField(authorizationRegistrationService, "okrChampionEmails",
+                "maria.gerber@puzzle.ch, richard.eberhard@puzzle.ch");
+
+        authorizationRegistrationService.updateOrAddAuthorizationUser(userRichard);
+        authorizationRegistrationService.updateOrAddAuthorizationUser(userMaria);
+        authorizationRegistrationService.updateOrAddAuthorizationUser(userAndrea);
+
+        var userRichardSaved = userPersistenceService.findByEmail(userRichard.getEmail());
+        var userMariaSaved = userPersistenceService.findByEmail(userMaria.getEmail());
+        var userAndreaSaved = userPersistenceService.findByEmail(userAndrea.getEmail());
+
+        assertTrue(userRichardSaved.get().isOkrChampion());
+        assertTrue(userMariaSaved.get().isOkrChampion());
+        assertFalse(userAndreaSaved.get().isOkrChampion());
+
+        userPersistenceService.deleteById(userRichardSaved.get().getId());
+        userPersistenceService.deleteById(userMariaSaved.get().getId());
+        userPersistenceService.deleteById(userAndreaSaved.get().getId());
     }
 }

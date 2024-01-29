29e80bb15801fc039a317e0a92bf596335f4bb0b
Martin Käser <kaeser@puzzle.ch>
2024-01-29 16:54:36 +0100

    #704 - Fix issue with cypress interception of @keyresults

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -244,13 +244,13 @@ function setConfidence(confidence: number) {
 }
 
 function loginWithCredentials(username: string, password: string) {
-  cy.intercept('GET', '/api/v1/users/current').as('getCurrentUser');
   cy.visit('/');
+  cy.intercept('GET', '**/users/current').as('getCurrentUser');
   cy.origin(Cypress.env('login_url'), { args: { username, password } }, ({ username, password }) => {
     cy.get('input[name="username"]').type(username);
     cy.get('input[name="password"]').type(password);
     cy.get('input[type="submit"]').click();
-    cy.wait('@getCurrentUser');
+    cy.wait('@getCurrentUser', { responseTimeout: 10000 });
   });
   cy.url().then((url) => {
     const currentUrl = new URL(url);

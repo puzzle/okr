6d04917b485a0a40a20fd057583af5cb29a52640
Lias Kleisa <kleisa@puzzle.ch>
2024-04-04 14:46:28 +0200

    Add new DTOs to return compact data

---
+++ backend/src/main/java/ch/puzzle/okr/dto/alignment/AlignmentObjectDto.java
@@ -0,0 +1,5 @@
+package ch.puzzle.okr.dto.alignment;
+
+public record AlignmentObjectDto(Long objectId, String objectTitle, String objectTeamName, String objectState,
+        String objectType) {
+}

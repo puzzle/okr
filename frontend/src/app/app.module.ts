b8eca029ad76b34667e9b047c2772a7d9e1bc75f
Lias Kleisa <kleisa@puzzle.ch>
2024-04-05 16:05:28 +0200

    Add new DiagramComponent and draw draft diagram with real data

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -69,6 +69,7 @@ import { ApplicationTopBarComponent } from './components/application-top-bar/app
 import { A11yModule } from '@angular/cdk/a11y';
 import { CustomizationService } from './services/customization.service';
 import { MetricCheckInDirective } from './components/checkin/check-in-form-metric/metric-check-in-directive';
+import { DiagramComponent } from './diagram/diagram.component';
 
 function initOauthFactory(configService: ConfigService, oauthService: OAuthService) {
   return async () => {
@@ -125,6 +126,7 @@ export const MY_FORMATS = {
     CheckInFormOrdinalComponent,
     CheckInFormComponent,
     MetricCheckInDirective,
+    DiagramComponent,
   ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],

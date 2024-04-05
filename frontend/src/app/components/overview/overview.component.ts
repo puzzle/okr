04bf2090c0fc0cd9d1e0c989a6e697181d23edb1
Lias Kleisa <kleisa@puzzle.ch>
2024-04-05 14:30:13 +0200

    Load alignment data on filter change

--- frontend/src/app/components/overview/overview.component.ts
+++ frontend/src/app/components/overview/overview.component.ts
@@ -13,9 +13,11 @@ import {
 } from 'rxjs';
 import { OverviewService } from '../../services/overview.service';
 import { ActivatedRoute } from '@angular/router';
-import { RefreshDataService } from '../../services/refresh-data.service';
 import { getQueryString, getValueFromQuery, isMobileDevice, trackByFn } from '../../shared/common';
 import { ConfigService } from '../../services/config.service';
+import { RefreshDataService } from '../shared/services/refresh-data.service';
+import { AlignmentService } from '../shared/services/alignment.service';
+import { AlignmentLists } from '../shared/types/model/AlignmentLists';
 
 @Component({
   selector: 'app-overview',
@@ -25,15 +27,19 @@ import { ConfigService } from '../../services/config.service';
 })
 export class OverviewComponent implements OnInit, OnDestroy {
   overviewEntities$: Subject<OverviewEntity[]> = new Subject<OverviewEntity[]>();
+  alignmentData$: Subject<AlignmentLists> = new Subject<AlignmentLists>();
+  emptyAlignmentList: AlignmentLists = { alignmentObjectDtoList: [], alignmentConnectionDtoList: [] } as AlignmentLists;
   protected readonly trackByFn = trackByFn;
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
   overviewPadding: Subject<number> = new Subject();
   isOverview: boolean = true;
+  service!: OverviewService | AlignmentService;
 
   backgroundLogoSrc$ = new BehaviorSubject<String>('assets/images/empty.svg');
 
   constructor(
     private overviewService: OverviewService,
+    private alignmentService: AlignmentService,
     private refreshDataService: RefreshDataService,
     private activatedRoute: ActivatedRoute,
     private changeDetector: ChangeDetectorRef,
@@ -84,17 +90,31 @@ export class OverviewComponent implements OnInit, OnDestroy {
   }
 
   loadOverview(quarterId?: number, teamIds?: number[], objectiveQuery?: string) {
-    this.overviewService
-      .getOverview(quarterId, teamIds, objectiveQuery)
-      .pipe(
-        catchError(() => {
-          this.loadOverview();
-          return EMPTY;
-        }),
-      )
-      .subscribe((overviews) => {
-        this.overviewEntities$.next(overviews);
-      });
+    if (this.isOverview) {
+      this.overviewService
+        .getOverview(quarterId, teamIds, objectiveQuery)
+        .pipe(
+          catchError(() => {
+            this.loadOverview();
+            return EMPTY;
+          }),
+        )
+        .subscribe((overviews) => {
+          this.overviewEntities$.next(overviews);
+        });
+    } else {
+      this.alignmentService
+        .getAlignmentByFilter(quarterId, teamIds, objectiveQuery)
+        .pipe(
+          catchError(() => {
+            this.loadOverview();
+            return EMPTY;
+          }),
+        )
+        .subscribe((alignmentLists) => {
+          this.alignmentData$.next(alignmentLists);
+        });
+    }
   }
 
   ngOnDestroy(): void {
@@ -105,6 +125,7 @@ export class OverviewComponent implements OnInit, OnDestroy {
   switchPage(input: string) {
     if (input == 'diagram') {
       this.isOverview = false;
+      this.loadOverviewWithParams();
     } else {
       this.isOverview = true;
       this.loadOverviewWithParams();

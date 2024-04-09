630597e9d2cc660a130c0d5eaa52aa24f1c2172f
Lias Kleisa <kleisa@puzzle.ch>
2024-04-09 17:01:37 +0200

    Fix frontend e2e tests

--- frontend/src/app/diagram/diagram.component.spec.ts
+++ frontend/src/app/diagram/diagram.component.spec.ts
@@ -1,11 +1,16 @@
 import { DiagramComponent } from './diagram.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DiagramComponent', () => {
   let component: DiagramComponent;
   let fixture: ComponentFixture<DiagramComponent>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DiagramComponent],
+      imports: [HttpClientTestingModule],
+    });
     fixture = TestBed.createComponent(DiagramComponent);
     component = fixture.componentInstance;
   });

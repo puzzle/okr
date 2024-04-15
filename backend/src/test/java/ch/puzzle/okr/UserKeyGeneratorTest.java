7d1f7e97ff19f56d044a00f03a4337722d934079
clean-coder <jean-claude.brantschen@gmx.ch>
2024-04-15 08:42:07 +0200

    fix flaky test

--- backend/src/test/java/ch/puzzle/okr/UserKeyGeneratorTest.java
+++ backend/src/test/java/ch/puzzle/okr/UserKeyGeneratorTest.java
@@ -1,6 +1,8 @@
 package ch.puzzle.okr;
 
+import ch.puzzle.okr.multitenancy.TenantContext;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -8,6 +10,12 @@ import java.lang.reflect.Method;
 import static ch.puzzle.okr.KeyResultTestHelpers.user;
 
 class UserKeyGeneratorTest {
+
+    @BeforeEach
+    void setUp() {
+        TenantContext.setCurrentTenant(null);
+    }
+
     @Test
     void generate_shouldReturnTenantAndUserInfo() {
         Object notUsedObject = new Object();

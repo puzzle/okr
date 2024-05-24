692b16eab57839a884bb9ff3a07901f330069017
Lias Kleisa <kleisa@puzzle.ch>
2024-05-24 18:10:25 +0200

    Remove unused files

--- backend/src/main/java/ch/puzzle/okr/dto/ObjectiveAlignmentsDto.java
+++
@@ -1,9 +0,0 @@
-package ch.puzzle.okr.dto;
-
-import ch.puzzle.okr.dto.keyresult.KeyResultAlignmentsDto;
-
-import java.util.List;
-
-public record ObjectiveAlignmentsDto(Long objectiveId, String objectiveTitle,
-        List<KeyResultAlignmentsDto> keyResultAlignmentsDtos) {
-}

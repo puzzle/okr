c5d967f809fe6cac6b544f280c7fda3386bffb26
Lias Kleisa <kleisa@puzzle.ch>
2024-06-03 13:50:33 +0200

    Add check for alignment in same team

--- backend/src/main/java/ch/puzzle/okr/service/validation/AlignmentValidationService.java
+++ backend/src/main/java/ch/puzzle/okr/service/validation/AlignmentValidationService.java
@@ -2,11 +2,13 @@ package ch.puzzle.okr.service.validation;
 
 import ch.puzzle.okr.ErrorKey;
 import ch.puzzle.okr.exception.OkrResponseStatusException;
+import ch.puzzle.okr.models.Team;
 import ch.puzzle.okr.models.alignment.Alignment;
 import ch.puzzle.okr.models.alignment.KeyResultAlignment;
 import ch.puzzle.okr.models.alignment.ObjectiveAlignment;
 import ch.puzzle.okr.repository.AlignmentRepository;
 import ch.puzzle.okr.service.persistence.AlignmentPersistenceService;
+import ch.puzzle.okr.service.persistence.TeamPersistenceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +20,13 @@ public class AlignmentValidationService
         extends ValidationBase<Alignment, Long, AlignmentRepository, AlignmentPersistenceService> {
 
     private final AlignmentPersistenceService alignmentPersistenceService;
+    private final TeamPersistenceService teamPersistenceService;
 
-    public AlignmentValidationService(AlignmentPersistenceService alignmentPersistenceService) {
+    public AlignmentValidationService(AlignmentPersistenceService alignmentPersistenceService,
+            TeamPersistenceService teamPersistenceService) {
         super(alignmentPersistenceService);
         this.alignmentPersistenceService = alignmentPersistenceService;
+        this.teamPersistenceService = teamPersistenceService;
     }
 
     @Override
@@ -30,6 +35,7 @@ public class AlignmentValidationService
         throwExceptionWhenIdIsNotNull(model.getId());
         throwExceptionWhenAlignedObjectIsNull(model);
         throwExceptionWhenAlignedIdIsSameAsTargetId(model);
+        throwExceptionWhenAlignmentIsInSameTeam(model);
         throwExceptionWhenAlignedObjectiveAlreadyExists(model);
         validate(model);
     }
@@ -40,6 +46,7 @@ public class AlignmentValidationService
         throwExceptionWhenIdIsNull(model.getId());
         throwExceptionWhenAlignedObjectIsNull(model);
         throwExceptionWhenAlignedIdIsSameAsTargetId(model);
+        throwExceptionWhenAlignmentIsInSameTeam(model);
         validate(model);
     }
 
@@ -57,6 +64,23 @@ public class AlignmentValidationService
                 throw new OkrResponseStatusException(HttpStatus.BAD_REQUEST, ErrorKey.ATTRIBUTE_NULL,
                         List.of("targetKeyResultId", keyResultAlignment.getAlignedObjective().getId()));
             }
+        }
+    }
+
+    private void throwExceptionWhenAlignmentIsInSameTeam(Alignment model) {
+        Team alignedTeam = teamPersistenceService.findById(model.getAlignedObjective().getTeam().getId());
+        Team targetTeam = null;
+
+        if (model instanceof ObjectiveAlignment objectiveAlignment) {
+            targetTeam = teamPersistenceService.findById(objectiveAlignment.getAlignmentTarget().getTeam().getId());
+        } else if (model instanceof KeyResultAlignment keyResultAlignment) {
+            targetTeam = teamPersistenceService
+                    .findById(keyResultAlignment.getAlignmentTarget().getObjective().getTeam().getId());
+        }
+
+        if (alignedTeam.equals(targetTeam)) {
+            throw new OkrResponseStatusException(HttpStatus.BAD_REQUEST, ErrorKey.NOT_LINK_IN_SAME_TEAM,
+                    List.of("teamId", targetTeam.getId()));
         }
     }
 

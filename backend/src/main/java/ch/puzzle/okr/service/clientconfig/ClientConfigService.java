b750e4d3e992e6c20467740f8b2d660b86be8191
clean-coder <jean-claude.brantschen@gmx.ch>
2024-07-04 10:56:07 +0200

    #948: support or fallbacks to find tenant in url

--- backend/src/main/java/ch/puzzle/okr/service/clientconfig/ClientConfigService.java
+++ backend/src/main/java/ch/puzzle/okr/service/clientconfig/ClientConfigService.java
@@ -3,6 +3,8 @@ package ch.puzzle.okr.service.clientconfig;
 import ch.puzzle.okr.dto.ClientConfigDto;
 import ch.puzzle.okr.multitenancy.TenantConfigProvider;
 import jakarta.persistence.EntityNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import java.util.Optional;
 @Service
 public class ClientConfigService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ClientConfigService.class);
     private final ClientCustomizationProperties clientCustomizationProperties;
     private final TenantConfigProvider tenantConfigProvider;
 
@@ -26,8 +29,11 @@ public class ClientConfigService {
 
     public ClientConfigDto getConfigBasedOnActiveEnv(String hostName) {
         String subdomain = hostName.split("\\.")[0];
+        String domainPrefixByHyphen = hostName.split("-")[0];
 
-        Optional<TenantConfigProvider.TenantConfig> tenantConfig = tenantConfigProvider.getTenantConfigById(subdomain);
+        Optional<TenantConfigProvider.TenantConfig> tenantConfig = getTenantConfig(hostName, subdomain,
+                domainPrefixByHyphen);
+
         if (tenantConfig.isEmpty()) {
             throw new EntityNotFoundException(
                     MessageFormat.format("Could not find tenant for subdomain:{0}", subdomain));
@@ -36,5 +42,18 @@ public class ClientConfigService {
         return new ClientConfigDto(activeProfile, tenantConfig.get().issuerUrl(), tenantConfig.get().clientId(),
                 clientCustomizationProperties.getFavicon(), clientCustomizationProperties.getLogo(),
                 clientCustomizationProperties.getTitle(), clientCustomizationProperties.getCustomStyles());
+    }
+
+    private Optional<TenantConfigProvider.TenantConfig> getTenantConfig(String hostname, String... tenantsFromUrl) {
+        for (String tenant : tenantsFromUrl) {
+            Optional<TenantConfigProvider.TenantConfig> tenantConfig = tenantConfigProvider.getTenantConfigById(tenant);
+            if (tenantConfig.isPresent()) {
+                logger.info("get config for " + tenant + ": OK");
+                return tenantConfig;
+            }
+            logger.info("get config found for " + tenant + ": failed");
+        }
+        logger.info("no config found for " + hostname + ": failed");
+        return Optional.empty();
     }
 }

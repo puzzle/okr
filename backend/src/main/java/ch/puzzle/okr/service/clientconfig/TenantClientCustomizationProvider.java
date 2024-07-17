c27808cb4df833308d5b9add0b6b020b4d86cd67
clean-coder <jean-claude.brantschen@gmx.ch>
2024-07-17 16:37:23 +0200

    #972: backend support for custom CSS styles

--- backend/src/main/java/ch/puzzle/okr/service/clientconfig/TenantClientCustomizationProvider.java
+++ backend/src/main/java/ch/puzzle/okr/service/clientconfig/TenantClientCustomizationProvider.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,10 @@ import java.util.Optional;
 public class TenantClientCustomizationProvider {
     private final Map<String, TenantClientCustomization> tenantCustomizations = new HashMap<>();
     private final Environment env;
+    private static String CUSTOMSTYLES_PREFIX = "okr.tenants.{0}.clientcustomization.customstyles";
+
+    record CssConfigItem(String cssName, String cssValue) implements Serializable {
+    }
 
     public TenantClientCustomizationProvider(final @Value("${okr.tenant-ids}") String[] tenantIds, Environment env) {
         this.env = env;
@@ -29,11 +34,48 @@ public class TenantClientCustomizationProvider {
                 env.getProperty(MessageFormat.format("okr.tenants.{0}.clientcustomization.triangles", tenantId)),
                 env.getProperty(MessageFormat.format("okr.tenants.{0}.clientcustomization.background-logo", tenantId)),
                 env.getProperty(MessageFormat.format("okr.tenants.{0}.clientcustomization.title", tenantId)),
-                new HashMap<>());
+                getCustomStyles(tenantId) //
+        );
+    }
+
+    private Map<String, String> getCustomStyles(String tenantId) {
+        Map<String, String> customStyles = new HashMap<>();
+
+        CssConfigItem topbarBackgroundColor = getTopbarBackgroundColor(tenantId);
+        customStyles.put(topbarBackgroundColor.cssName(), topbarBackgroundColor.cssValue());
+
+        return customStyles;
+    }
+
+    private CssConfigItem getTopbarBackgroundColor(String tenantId) {
+        String topbarColorPropertyName = formatWithTenant( //
+                CUSTOMSTYLES_PREFIX + ".okr-topbar-background-color", //
+                tenantId);
+        return getCssPropertyNameAndValue(topbarColorPropertyName, tenantId);
+    }
+
+    private CssConfigItem getCssPropertyNameAndValue(String propertyName, String tenantId) {
+        String cssName = extractCssNameFromPropertyName(propertyName, tenantId);
+        String cssValue = env.getProperty(formatWithTenant(propertyName, tenantId));
+        return new CssConfigItem(cssName, cssValue);
     }
 
     public Optional<TenantClientCustomization> getTenantClientCustomizationsById(String tenantId) {
         return Optional.ofNullable(this.tenantCustomizations.get(tenantId));
     }
 
+    String extractCssNameFromPropertyName(String propertyName, String tenantId) {
+        String prefix = formatWithTenant("okr.tenants.{0}.clientcustomization.customstyles", tenantId);
+        int startPrefix = propertyName.indexOf(prefix);
+        if (startPrefix == -1) {
+            throw new IllegalArgumentException(propertyName + " is not a valid property name client customization");
+        }
+
+        int startCssPropertyName = startPrefix + prefix.length() + 1;
+        return propertyName.substring(startCssPropertyName);
+    }
+
+    private String formatWithTenant(String stringWithTenantPlaceholder, String tenantId) {
+        return MessageFormat.format(stringWithTenantPlaceholder, tenantId);
+    }
 }
---
+++ backend/src/test/java/ch/puzzle/okr/service/clientconfig/TenantClientCustomizationProviderTest.java
@@ -0,0 +1,40 @@
+package ch.puzzle.okr.service.clientconfig;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TenantClientCustomizationProviderTest {
+
+    @DisplayName("extractCssNameFromPropertyName() should return css name for valid property name")
+    @ParameterizedTest
+    @CsvSource({ "pitc", "acme" })
+    void extractCssNameFromPropertyNameShouldReturnCssNameForValidPropertyName(String tenantId) {
+        // arrange
+        String propertyNameWithTenant = "okr.tenants." + tenantId
+                + ".clientcustomization.customstyles.my-css-property-name";
+        TenantClientCustomizationProvider provider = new TenantClientCustomizationProvider(new String[] {}, null);
+
+        // act
+        String cssName = provider.extractCssNameFromPropertyName(propertyNameWithTenant, tenantId);
+
+        // assert
+        assertEquals("my-css-property-name", cssName);
+    }
+
+    @DisplayName("extractCssNameFromPropertyName() should throw IllegalArgumentException for invalid property name")
+    @Test
+    void extractCssNameFromPropertyNameShouldThrowIllegalArgumentExceptionForInvalidPropertyName() {
+        // arrange
+        String propertyNameWithoutTenant = "okr.tenants.clientcustomization.customstyles.my-css-property-name";
+        TenantClientCustomizationProvider provider = new TenantClientCustomizationProvider(new String[] {}, null);
+
+        // act + assert
+        assertThrows(IllegalArgumentException.class,
+                () -> provider.extractCssNameFromPropertyName(propertyNameWithoutTenant, "pitc"));
+    }
+}

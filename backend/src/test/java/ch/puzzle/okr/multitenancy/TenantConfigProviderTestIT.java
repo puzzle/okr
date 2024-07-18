9355774a6b1f478e95f0c8a32f47cb5ce3d8d8c3
clean-coder <jean-claude.brantschen@gmx.ch>
2024-07-18 12:05:44 +0200

    #972: cleanup TenantConfigProviderTestIT

--- backend/src/test/java/ch/puzzle/okr/multitenancy/TenantConfigProviderTestIT.java
+++ backend/src/test/java/ch/puzzle/okr/multitenancy/TenantConfigProviderTestIT.java
@@ -1,67 +1,78 @@
 package ch.puzzle.okr.multitenancy;
 
 import ch.puzzle.okr.test.SpringIntegrationTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.core.env.Environment;
-import org.springframework.test.context.TestPropertySource;
 
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringIntegrationTest
 @SpringBootConfiguration
-@TestPropertySource(properties = { "okr.tenant-ids=pitc,acme", })
 public class TenantConfigProviderTestIT {
-
-    private static final String JWK_SET_URI = "_jwkSetUri";
-    private static final String FRONTEND_CLIENT_ISSUER_URL = "_frontendClientIssuerUrl";
-    private static final String FRONTEND_CLIENT_ID = "_frontendClientId";
-    private static final String DATASOURCE_URL = "_datasourceUrl";
-    private static final String DATASOURCE_NAME = "_datasourceName";
-    private static final String DATASOURCE_PASSWORD = "_datasourcePassword";
-    private static final String DATASOURCE_SCHEMA = "_datasourceSchema";
+    private static final String JWK_SET_URI = "jwkSetUri";
+    private static final String FRONTEND_CLIENT_ISSUER_URL = "frontendClientIssuerUrl";
+    private static final String FRONTEND_CLIENT_ID = "frontendClientId";
+    private static final String DATASOURCE_URL = "datasourceUrl";
+    private static final String DATASOURCE_NAME = "datasourceName";
+    private static final String DATASOURCE_PASSWORD = "datasourcePassword";
+    private static final String DATASOURCE_SCHEMA = "datasourceSchema";
     private static final String DRIVER_CLASS_NAME = "driverClassName";
     private static final String CHAMPION_EMAILS_1 = "a@pitc.ch";
     private static final String CHAMPION_EMAILS_2 = "b@pitc.ch";
+    private static final String CHAMPION_EMAILS = CHAMPION_EMAILS_1 + "," + CHAMPION_EMAILS_2;
 
-    @Value("${okr.tenant-ids}")
-    private String[] tenantIds;
+    private final String[] tenantIds = { "pitc", "acme" };
 
     @Mock
     private Environment env;
 
-    private static final String PITC = "pitc";
-    private static final String ACME = "acme";
-
     @BeforeEach
     void setUp() {
-        setupPropertiesForTenantWithId(PITC);
-        setupPropertiesForTenantWithId(ACME);
+        for (String tenantId : tenantIds) {
+            setupPropertiesForTenantWithId(tenantId);
+        }
     }
 
     private void setupPropertiesForTenantWithId(String id) {
-        when(env.getProperty(fmt("okr.tenants.{0}.security.oauth2.resourceserver.jwt.jwk-set-uri", id))).thenReturn(id + JWK_SET_URI);
-        when(env.getProperty(fmt("okr.tenants.{0}.security.oauth2.frontend.issuer-url", id))).thenReturn(id + FRONTEND_CLIENT_ISSUER_URL);
-        when(env.getProperty(fmt("okr.tenants.{0}.security.oauth2.frontend.client-id", id))).thenReturn(id + FRONTEND_CLIENT_ID);
-        when(env.getProperty(fmt("okr.tenants.{0}.user.champion.emails", id), "")).thenReturn(CHAMPION_EMAILS_1 + "," + CHAMPION_EMAILS_2); // with default value
-        when(env.getProperty(fmt("okr.tenants.{0}.datasource.url", id))).thenReturn(id + DATASOURCE_URL);
-        when(env.getProperty(fmt("okr.tenants.{0}.datasource.username", id))).thenReturn(id + DATASOURCE_NAME);
-        when(env.getProperty(fmt("okr.tenants.{0}.datasource.password", id))).thenReturn(id + DATASOURCE_PASSWORD);
-        when(env.getProperty(fmt("okr.tenants.{0}.datasource.schema", id))).thenReturn(id + DATASOURCE_SCHEMA);
-        when(env.getProperty("okr.datasource.driver-class-name")).thenReturn(DRIVER_CLASS_NAME);
+        mockProperty("okr.tenants.{0}.security.oauth2.resourceserver.jwt.jwk-set-uri", JWK_SET_URI, id);
+        mockProperty("okr.tenants.{0}.security.oauth2.frontend.issuer-url", FRONTEND_CLIENT_ISSUER_URL, id);
+        mockProperty("okr.tenants.{0}.security.oauth2.frontend.client-id", FRONTEND_CLIENT_ID, id);
+        mockProperty("okr.tenants.{0}.datasource.url", DATASOURCE_URL, id);
+        mockProperty("okr.tenants.{0}.datasource.username", DATASOURCE_NAME, id);
+        mockProperty("okr.tenants.{0}.datasource.password", DATASOURCE_PASSWORD, id);
+        mockProperty("okr.tenants.{0}.datasource.schema", DATASOURCE_SCHEMA, id);
+
+        mockProperty("okr.datasource.driver-class-name", DRIVER_CLASS_NAME);
+        mockPropertyWithDefaultValue("okr.tenants.{0}.user.champion.emails", CHAMPION_EMAILS, id);
     }
 
-    private String fmt(String template, String tenantId) {
-        return MessageFormat.format(template, tenantId);
+    private void mockProperty(String propertyName, String propertyValue, String tenantId) {
+        when(env.getProperty(MessageFormat.format(propertyName, tenantId))) //
+                .thenReturn(prefix(tenantId) + propertyValue);
+    }
+
+    private void mockProperty(String propertyName, String propertyValue) {
+        when(env.getProperty(propertyName)).thenReturn(propertyValue);
+    }
+
+    private void mockPropertyWithDefaultValue(String propertyName, String propertyValue, String id) {
+        when(env.getProperty(MessageFormat.format(propertyName, id), "")) //
+                .thenReturn(propertyValue);
+    }
+
+    private String prefix(String tenantId) {
+        return tenantId + "_";
     }
 
     @DisplayName("getTenantConfigs returns all TenantConfigs as List")
@@ -75,52 +86,61 @@ public class TenantConfigProviderTestIT {
     }
 
     @DisplayName("getTenantConfigById returns for an existing TenantId the TenantConfig as Optional")
-    @Test
-    void testGetTenantConfigByIdForExistingTenantId() {
+    @ParameterizedTest
+    @CsvSource({ "pitc, acme" })
+    void testGetTenantConfigByIdForExistingTenantId(String tenantId) {
         TenantConfigProvider configProvider = new TenantConfigProvider(tenantIds, env);
-        Optional<TenantConfigProvider.TenantConfig> config = configProvider.getTenantConfigById(PITC);
-        Assertions.assertTrue(config.isPresent());
+        Optional<TenantConfigProvider.TenantConfig> config = configProvider.getTenantConfigById(tenantId);
+        assertTrue(config.isPresent());
         assertTenantConfigProvider(config.get());
     }
 
     @DisplayName("getTenantConfigById returns for a non existing TenantId an empty Optional")
-    @Test
-    void testGetTenantConfigByIdForNonExistingTenantId() {
+    @ParameterizedTest
+    @CsvSource({ "PITC-London" })
+    void testGetTenantConfigByIdForNonExistingTenantId(String nonExistingTenantId) {
         TenantConfigProvider configProvider = new TenantConfigProvider(tenantIds, env);
-        Optional<TenantConfigProvider.TenantConfig> config = configProvider.getTenantConfigById("PITC-London");
-        Assertions.assertTrue(config.isEmpty());
+        Optional<TenantConfigProvider.TenantConfig> config = configProvider.getTenantConfigById(nonExistingTenantId);
+        assertTrue(config.isEmpty());
     }
 
     @DisplayName("getJwkSetUri returns for an existing TenantId the JwkSetUri as Optional")
-    @Test
-    void testGetJwkSetUriForExistingTenantId() {
+    @ParameterizedTest
+    @CsvSource({ "pitc", "acme" })
+    void testGetJwkSetUriForExistingTenantId(String tenantId) {
+        // arrange
         TenantConfigProvider configProvider = new TenantConfigProvider(tenantIds, env);
-        Optional<String> jwkSetUri = configProvider.getJwkSetUri(PITC);
-        Assertions.assertTrue(jwkSetUri.isPresent());
-        Assertions.assertEquals(PITC + JWK_SET_URI, jwkSetUri.get());
+
+        // act
+        Optional<String> jwkSetUri = configProvider.getJwkSetUri(tenantId);
+
+        // assert
+        assertTrue(jwkSetUri.isPresent());
+        assertEquals(prefix(tenantId) + JWK_SET_URI, jwkSetUri.get());
     }
 
     @DisplayName("getJwkSetUri returns for a non existing TenantId an empty Optional")
-    @Test
-    void testGetJwkSetUriForNonExistingTenantId() {
+    @ParameterizedTest
+    @CsvSource({ "PITC-London" })
+    void testGetJwkSetUriForNonExistingTenantId(String nonExistingTenantId) {
         TenantConfigProvider configProvider = new TenantConfigProvider(tenantIds, env);
-        Optional<String> jwkSetUri = configProvider.getJwkSetUri("PITC-London");
-        Assertions.assertTrue(jwkSetUri.isEmpty());
+        Optional<String> jwkSetUri = configProvider.getJwkSetUri(nonExistingTenantId);
+        assertTrue(jwkSetUri.isEmpty());
     }
 
-    private static void assertTenantConfigProvider(TenantConfigProvider.TenantConfig tenantConfig) {
+    private void assertTenantConfigProvider(TenantConfigProvider.TenantConfig tenantConfig) {
         String tenantId = tenantConfig.tenantId();
-        Assertions.assertEquals(tenantId, tenantConfig.tenantId());
-        Assertions.assertEquals(tenantId + JWK_SET_URI, tenantConfig.jwkSetUri());
-        Assertions.assertEquals(tenantId + FRONTEND_CLIENT_ISSUER_URL, tenantConfig.issuerUrl());
-        Assertions.assertEquals(tenantId + FRONTEND_CLIENT_ID, tenantConfig.clientId());
-        Assertions.assertArrayEquals(new String[] { CHAMPION_EMAILS_1, CHAMPION_EMAILS_2 },
-                tenantConfig.okrChampionEmails());
-        Assertions.assertEquals(tenantId + DATASOURCE_URL, tenantConfig.dataSourceConfig().url());
-        Assertions.assertEquals(tenantId + DATASOURCE_NAME, tenantConfig.dataSourceConfig().name());
-        Assertions.assertEquals(tenantId + DATASOURCE_PASSWORD, tenantConfig.dataSourceConfig().password());
-        Assertions.assertEquals(tenantId + DATASOURCE_SCHEMA, tenantConfig.dataSourceConfig().schema());
-        Assertions.assertEquals(DRIVER_CLASS_NAME, tenantConfig.dataSourceConfig().driverClassName());
+        assertEquals(tenantId, tenantConfig.tenantId());
+        assertEquals(prefix(tenantId) + JWK_SET_URI, tenantConfig.jwkSetUri());
+        assertEquals(prefix(tenantId) + FRONTEND_CLIENT_ISSUER_URL, tenantConfig.issuerUrl());
+        assertEquals(prefix(tenantId) + FRONTEND_CLIENT_ID, tenantConfig.clientId());
+        assertEquals(prefix(tenantId) + DATASOURCE_URL, tenantConfig.dataSourceConfig().url());
+        assertEquals(prefix(tenantId) + DATASOURCE_NAME, tenantConfig.dataSourceConfig().name());
+        assertEquals(prefix(tenantId) + DATASOURCE_PASSWORD, tenantConfig.dataSourceConfig().password());
+        assertEquals(prefix(tenantId) + DATASOURCE_SCHEMA, tenantConfig.dataSourceConfig().schema());
+
+        assertArrayEquals(new String[] { CHAMPION_EMAILS_1, CHAMPION_EMAILS_2 }, tenantConfig.okrChampionEmails());
+        assertEquals(DRIVER_CLASS_NAME, tenantConfig.dataSourceConfig().driverClassName());
     }
 
 }

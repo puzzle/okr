65ac1309f81629b5fd33c5c4833b47fbdbf401df
clean-coder <jean-claude.brantschen@gmx.ch>
2024-07-22 14:20:26 +0200

    #958: refactoring - GJ uses Quarters functionality

--- backend/src/main/java/ch/puzzle/okr/util/GJ.java
+++ backend/src/main/java/ch/puzzle/okr/util/GJ.java
@@ -1,58 +1,21 @@
 package ch.puzzle.okr.util;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GJ {
-    public static final String Q1_START = "07-01";
-    public static final String Q1_END = "09-30";
-    public static final String Q2_START = "10-01";
-    public static final String Q2_END = "12-31";
-    public static final String Q3_START = "01-01";
-    public static final String Q3_END = "03-31";
-    public static final String Q4_START = "04-01";
-    public static final String Q4_END = "06-30";
-
     private final LocalDate now;
-    private final List<QuarterData> quarters = new ArrayList<>();
+    private final Quarters quarters;
 
     public GJ(LocalDate now) {
         this.now = now;
-        calculateQuartersOfGJ();
-    }
-
-    private void calculateQuartersOfGJ() {
-        quarters.add(new QuarterData(1, date(Q1_START), date(Q1_END)));
-        quarters.add(new QuarterData(2, date(Q2_START), date(Q2_END)));
-        quarters.add(new QuarterData(3, date(Q3_START), date(Q3_END)));
-        quarters.add(new QuarterData(4, date(Q4_START), date(Q4_END)));
-    }
-
-    private LocalDate date(String monthDay) {
-        return LocalDate.parse(now.getYear() + "-" + monthDay);
-    }
-
-    public QuarterData getCurrentQuarter() {
-        for (QuarterData quarter : quarters) {
-            if (isInQuarter(quarter.start(), quarter.end())) {
-                return quarter;
-            }
-        }
-        return null;
-    }
-
-    private boolean isInQuarter(LocalDate start, LocalDate end) {
-        boolean isAfterStart = now.equals(start) || now.isAfter(start);
-        boolean isBeforeEnd = now.isBefore(end) || now.equals(end);
-        return isAfterStart && isBeforeEnd;
+        this.quarters = new Quarters(now);
     }
 
     public String getLabel() {
         return "GJ " + //
                 formatYearAs2Digits(firstYearOfGJ()) + "/" + //
                 formatYearAs2Digits(secondYearOfGJ()) + "-Q" + //
-                getCurrentQuarter().quarterDigit();
+                quarters.currentQuarter().quarterDigit();
     }
 
     private int firstYearOfGJ() {

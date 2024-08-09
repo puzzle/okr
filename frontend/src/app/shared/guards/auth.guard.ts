b0a5dd9fcf95a5965da7d0f2c7fe6e28e5076362
Yanick Minder <minder@puzzle.ch>
2024-08-09 15:48:57 +0200

    remove useless .run/prod files

--- frontend/src/app/shared/guards/auth.guard.ts
+++ frontend/src/app/shared/guards/auth.guard.ts
@@ -1,16 +1,9 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { log } from '@angular-devkit/build-angular/src/builders/ssr-dev-server';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const oauthService = inject(OAuthService);
-  const document = oauthService.loadDiscoveryDocument();
-
-  if (oauthService.hasValidIdToken()) {
-    return true;
-  }
-
   return oauthService.loadDiscoveryDocumentAndTryLogin().then(async () => {
     // if the login failed initialize code flow
     let validToken = oauthService.hasValidIdToken();

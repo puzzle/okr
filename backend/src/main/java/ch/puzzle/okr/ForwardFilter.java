9ceb89878056682b10e16b0bf5597300568f8bd9
Yanick Minder <minder@puzzle.ch>
2024-08-20 17:42:32 +0200

    add team-management path to list of forwarded paths

--- backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
+++ backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 public class ForwardFilter extends GenericFilterBean {
 
     private static final Logger logger = LoggerFactory.getLogger(ForwardFilter.class);
-    private final String[] allowedRoutes = { "/keyresult", "/objective", "/?state" };
+    private final String[] allowedRoutes = { "/keyresult", "/objective", "/?state", "/team-management" };
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)

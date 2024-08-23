e818c3fbc1c169f8e40c1019bf3a6634ed55b73d
Yanick Minder <minder@puzzle.ch>
2024-08-23 07:53:21 +0200

    use build watch to auto build angular

--- frontend/src/app/shared/guards/auth.guard.ts
+++ frontend/src/app/shared/guards/auth.guard.ts
@@ -6,6 +6,7 @@ export const authGuard: CanActivateFn = (route, state) => {
   const oauthService = inject(OAuthService);
   const router = inject(Router);
 
+  console.log('test123');
   return oauthService.loadDiscoveryDocumentAndTryLogin().then(async () => {
     // if the login failed initialize code flow
     let validToken = oauthService.hasValidIdToken();

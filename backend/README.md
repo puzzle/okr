3ce67247e10aeec46237ced8825c8d5f4ba3a26b
Yanick Minder <minder@puzzle.ch>
2024-09-02 15:20:28 +0200

    fix error in docker setup and provide the correct asset

--- backend/README.md
+++ backend/README.md
@@ -19,12 +19,12 @@ NATIVE
 USING DOCKER
 - cd into the root directory of the project
 - cd `docker/local-prod`
-- Run `docker-compose up`
+- Run `docker compose up`
 - You have to restart after every code change `docker compose restart spring`
 - Get the logs with `docker compose logs -f spring`
 !IMPORTANT!
 - If after the first start the backend is not reachable, restart the backend container with `docker compose restart spring`
-- if any permission issues occur, following folders
+- if any permission issues occur, delete all of the following folders
   - .angular
   - frontend/dist
   - frontend/node_modules
--- backend/src/main/java/ch/puzzle/okr/controller/ClientConfigController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/ClientConfigController.java
@@ -1,17 +1,21 @@
 package ch.puzzle.okr.controller;
 
+import ch.puzzle.okr.ForwardFilter;
 import ch.puzzle.okr.dto.ClientConfigDto;
 import ch.puzzle.okr.service.clientconfig.ClientConfigService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ClientConfigController {
 
     private final ClientConfigService configService;
+    private static final Logger logger = LoggerFactory.getLogger(ClientConfigController.class);
 
     public ClientConfigController(ClientConfigService configService) {
         this.configService = configService;
@@ -22,8 +26,21 @@ public class ClientConfigController {
         return ResponseEntity.status(HttpStatus.OK).body(configService.getConfigBasedOnActiveEnv());
     }
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
-    public String redirect() {
-        return "forward:/index.html";
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect(HttpServletRequest request) {
+        String path = request.getRequestURI();
+
+        logger.debug("----------------------");
+        logger.debug("Forwarding request to: " + path);
+        logger.debug("----------------------");
+
+        // Serve static resources or paths containing a dot directly
+        if (path.startsWith("/assets/") || path.contains(".")) {
+            return "forward:" + path;
+        }
+
+        // Forward all other requests to index.html
+        return "forward:/";
     }
+
 }

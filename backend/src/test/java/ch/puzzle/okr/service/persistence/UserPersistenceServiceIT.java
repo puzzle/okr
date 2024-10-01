a7908c46aa05cb93e281834b8e8eaa97cd086b51
clean-coder <jean-claude.brantschen@gmx.ch>
2024-10-01 14:43:46 +0200

    #1012: remove findX test (already covered by PersistenceBase tests)

--- backend/src/test/java/ch/puzzle/okr/service/persistence/UserPersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/persistence/UserPersistenceServiceIT.java
@@ -3,17 +3,13 @@ package ch.puzzle.okr.service.persistence;
 import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.multitenancy.TenantContext;
 import ch.puzzle.okr.test.SpringIntegrationTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringIntegrationTest
 class UserPersistenceServiceIT {
@@ -40,41 +36,6 @@ class UserPersistenceServiceIT {
     }
 
     @Test
-    void shouldReturnAllUsersCorrect() throws ResponseStatusException {
-        List<User> userList = userPersistenceService.findAll();
-
-        Assertions.assertThat(userList.size()).isGreaterThanOrEqualTo(7);
-    }
-
-    @Test
-    void shouldReturnSingleUserWhenFindingOwnerByValidId() {
-        User returnedUser = userPersistenceService.findById(1L);
-
-        assertEquals(1L, returnedUser.getId());
-        assertEquals("Paco", returnedUser.getFirstname());
-        assertEquals("Eggimann", returnedUser.getLastname());
-        assertEquals("peggimann@puzzle.ch", returnedUser.getEmail());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenFindingOwnerNotFound() {
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> userPersistenceService.findById(321L));
-
-        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
-        assertEquals("MODEL_WITH_ID_NOT_FOUND", exception.getReason());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenFindingOwnerWithNullId() {
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
-                () -> userPersistenceService.findById(null));
-
-        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
-        assertEquals("ATTRIBUTE_NULL", exception.getReason());
-    }
-
-    @Test
     void getOrCreateUserShouldReturnSingleUserWhenUserFound() {
         User existingUser = User.Builder.builder().withEmail(EMAIL_ALICE).build();
 
@@ -88,8 +49,12 @@ class UserPersistenceServiceIT {
 
     @Test
     void getOrCreateUserShouldReturnSavedUserWhenUserNotFound() {
-        User newUser = User.Builder.builder().withId(null).withFirstname("firstname").withLastname("lastname")
-                .withEmail("lastname@puzzle.ch").build();
+        User newUser = User.Builder.builder() //
+                .withId(null) //
+                .withFirstname("firstname") //
+                .withLastname("lastname") //
+                .withEmail("lastname@puzzle.ch") //
+                .build();
 
         createdUser = userPersistenceService.getOrCreateUser(newUser);
 

13dd0b46f61ac91b3067c2e713b71f18f07cdfa0
clean-coder <jean-claude.brantschen@gmx.ch>
2024-10-08 09:06:19 +0200

    #1012: remove unused code (Organisation does no longer exists in the DB)

--- backend/src/main/java/ch/puzzle/okr/repository/TeamRepository.java
+++ backend/src/main/java/ch/puzzle/okr/repository/TeamRepository.java
@@ -1,9 +1,7 @@
 package ch.puzzle.okr.repository;
 
 import ch.puzzle.okr.models.Team;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 

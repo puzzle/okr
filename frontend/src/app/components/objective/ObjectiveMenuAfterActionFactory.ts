77ba2d6acd65af697c551eced5a11438930cec84
Yanick Minder <minder@puzzle.ch>
2024-10-23 15:57:16 +0200

    add ObjectiveAfterActionFactory

---
+++ frontend/src/app/components/objective/ObjectiveMenuAfterActionFactory.ts
@@ -0,0 +1,52 @@
+import { DialogService } from '../../services/dialog.service';
+import { Objective } from '../../shared/types/model/Objective';
+import { State } from '../../shared/types/enums/State';
+import { Completed } from '../../shared/types/model/Completed';
+import { ObjectiveService } from '../../services/objective.service';
+import { RefreshDataService } from '../../services/refresh-data.service';
+
+export class ObjectiveMenuAfterActionFactory {
+  constructor(
+    private readonly dialogService: DialogService,
+    private readonly objectiveService: ObjectiveService,
+    private readonly refreshDataService: RefreshDataService,
+  ) {}
+
+  completeObjective(objective: Objective, result: { endState: string; comment: string | null; objective: any }) {
+    objective.state = result.endState as State;
+    const completed: Completed = {
+      id: null,
+      version: objective.version,
+      objective: objective,
+      comment: result.comment,
+    };
+    this.objectiveService.updateObjective(objective).subscribe(() => {
+      this.objectiveService.createCompleted(completed).subscribe(() => {
+        this.refreshDataService.markDataRefresh();
+      });
+    });
+  }
+
+  releaseFromQuarter(objective: Objective) {
+    objective.state = 'ONGOING' as State;
+    this.objectiveService.updateObjective(objective).subscribe(() => {
+      this.refreshDataService.markDataRefresh();
+    });
+  }
+
+  objectiveBackToDraft(objective: Objective) {
+    objective.state = 'DRAFT' as State;
+    this.objectiveService.updateObjective(objective).subscribe(() => {
+      this.refreshDataService.markDataRefresh();
+    });
+  }
+
+  objectiveReopen(objective: Objective) {
+    objective.state = 'ONGOING' as State;
+    this.objectiveService.updateObjective(objective).subscribe(() => {
+      this.objectiveService.deleteCompleted(objective.id).subscribe(() => {
+        this.refreshDataService.markDataRefresh();
+      });
+    });
+  }
+}

d7049d92429b27f0cbe7159dae4dd93037783985
Yanick Minder <minder@puzzle.ch>
2024-10-28 16:29:28 +0100

    create base template for dialogs

--- frontend/src/app/components/dialog-template-core/dialog-template-core.component.ts
+++ frontend/src/app/components/dialog-template-core/dialog-template-core.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dialog-template-core',
   templateUrl: './dialog-template-core.component.html',
-  styleUrl: './dialog-template-core.component.css',
+  styleUrl: './dialog-template-core.component.scss',
 })
 export class DialogTemplateCoreComponent {}

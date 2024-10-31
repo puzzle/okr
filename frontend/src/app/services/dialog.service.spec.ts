86f4c843a56dc22e73e15badaaf5a81ea89d11b9
Nevio Di Gennaro <digennaro@puzzle.ch>
2024-10-31 08:58:16 +0100

    create tests for dialog-service

--- frontend/src/app/services/dialog.service.spec.ts
+++ frontend/src/app/services/dialog.service.spec.ts
@@ -1,9 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TranslateCompiler, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateTestingModule } from 'ngx-translate-testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../shared/dialog/confirm-dialog/confirm-dialog.component';
+import { TeamComponent } from '../components/team/team.component';
 
 describe('DialogService', () => {
   let service: DialogService;
@@ -15,5 +16,23 @@ describe('DialogService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should open dialog', () => {
+    const dialog = service.open(TeamComponent);
+    expect(dialog).toBeInstanceOf(MatDialogRef);
+    expect(dialog._containerInstance._config.panelClass).toEqual(service.DIALOG_CONFIG.panelClass);
+    expect(dialog._containerInstance._config.maxWidth).toEqual(service.DIALOG_CONFIG.maxWidth);
+    expect(dialog.componentInstance).toBeInstanceOf(TeamComponent);
+  });
+
+  it('should open confirm dialog', () => {
+    jest.spyOn(service, 'open');
+    const dialog = service.openConfirmDialog('DELETE.ACTION');
+    expect(service.open).toHaveBeenCalledTimes(1);
+    expect(dialog).toBeInstanceOf(MatDialogRef);
+    expect(dialog.componentInstance).toBeInstanceOf(ConfirmDialogComponent);
+    expect(dialog.componentInstance.data.title).toBe('DELETE.ACTION.TITLE');
+    expect(dialog.componentInstance.data.text).toBe('DELETE.ACTION.TEXT');
   });
 });

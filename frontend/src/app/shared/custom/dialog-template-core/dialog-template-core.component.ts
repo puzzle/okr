e3d37d063e78b792ddb827394f4391d30ed27fd7
Yanick Minder <minder@puzzle.ch>
2024-11-01 09:33:15 +0100

    add spinner to dialog core

--- frontend/src/app/shared/custom/dialog-template-core/dialog-template-core.component.ts
+++ frontend/src/app/shared/custom/dialog-template-core/dialog-template-core.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dialog-template-core',
   templateUrl: './dialog-template-core.component.html',
   styleUrl: './dialog-template-core.component.scss',
 })
-export class DialogTemplateCoreComponent {}
+export class DialogTemplateCoreComponent {
+  @Input() observable: Observable<any> = new Observable();
+  @Input() title: string = '';
+
+  isValueReady(obj: any): boolean {
+    if (obj == null) {
+      return false;
+    }
+    if (Array.isArray(obj)) {
+      return obj.length > 0;
+    }
+    return true;
+  }
+}

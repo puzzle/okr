fdb13181d4ee4e163a94059720ff1ec5e296d592
Yanick Minder <minder@puzzle.ch>
2024-11-05 09:38:33 +0100

    fix action service tests

--- frontend/src/app/services/objective-menu-actions.service.spec.ts
+++ frontend/src/app/services/objective-menu-actions.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ObjectiveMenuActionsService } from './objective-menu-actions.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ObjectiveMenuActionsService', () => {
   let service: ObjectiveMenuActionsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot()],
+      providers: [TranslateService, provideRouter([]), provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ObjectiveMenuActionsService);
   });
 

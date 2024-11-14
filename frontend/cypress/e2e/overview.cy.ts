e93fcfc58db7ca9da720341d38c1c713d7a614d9
Nevio Di Gennaro <digennaro@puzzle.ch>
2024-11-14 11:55:31 +0100

    add test to test if aktuell label is shown

--- frontend/cypress/e2e/overview.cy.ts
+++ frontend/cypress/e2e/overview.cy.ts
@@ -5,6 +5,10 @@ describe('OKR Overview', () => {
     cy.loginAsUser(users.gl);
   });
 
+  it('should have the current quarter with label Aktuell', () => {
+    cy.getByTestId('quarterFilter').contains('Aktuell');
+  });
+
   it('Check order of teams', () => {
     cy.intercept('GET', '**/overview*').as('overview');
 

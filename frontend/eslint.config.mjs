681b83823abd9c96e1d4355aaf9419fb8baf39cc
Miguel Lehmann <lehmann@puzzle.ch>
2024-11-19 10:28:52 +0100

    edit config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,8 +1,14 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import {rules} from "@typescript-eslint/eslint-plugin";
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
+  {
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+    }
+  }
 );

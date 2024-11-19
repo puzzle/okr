bf840a05cd2d6ee2c572df816a62fbc0424f610b
Nevio Di Gennaro <digennaro@puzzle.ch>
2024-11-19 09:04:54 +0100

    change config and Tenant enitity to have new attribute supportSiteURL

--- backend/src/main/java/ch/puzzle/okr/multitenancy/customization/TenantClientCustomization.java
+++ backend/src/main/java/ch/puzzle/okr/multitenancy/customization/TenantClientCustomization.java
@@ -3,5 +3,5 @@ package ch.puzzle.okr.multitenancy.customization;
 import java.util.Map;
 
 public record TenantClientCustomization(String favicon, String logo, String triangles, String backgroundLogo,
-        String title, Map<String, String> customStyles) {
+        String title, String supportSiteURL, Map<String, String> customStyles) {
 }
--- frontend/src/app/shared/types/model/ClientConfig.ts
+++ frontend/src/app/shared/types/model/ClientConfig.ts
@@ -14,6 +14,7 @@ export interface CustomizationConfig {
   logo: string;
   triangles: string;
   backgroundLogo: string;
+  supportSiteURL: string;
   customStyles: CustomStyles;
 }
 export interface ClientConfig extends AuthConfig, CustomizationConfig {}

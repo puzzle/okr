563436728c28769fc53439c641ef86cd2392fcc1
Miguel Lehmann <lehmann@puzzle.ch>
2024-11-19 10:43:46 +0100

    put unused-imports plugin into config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,12 +1,15 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import {rules} from "@typescript-eslint/eslint-plugin";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
   {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
     rules: {
       "unused-imports/no-unused-imports": "error",
     }

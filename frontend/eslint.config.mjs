85c9c66709ba6d98364dbbfd94467d960c44d139
Yanick Minder <minder@puzzle.ch>
2024-11-22 08:40:02 +0100

    add stylistic plugin to eslnt

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,8 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import unusedImports from "eslint-plugin-unused-imports";
+import stylistic from '@stylistic/eslint-plugin'
+
 
 
 export default tseslint.config(
@@ -16,9 +18,13 @@ export default tseslint.config(
   {
     plugins: {
       'unused-imports': unusedImports,
+        '@stylistic': stylistic
     },
     rules: {
       'unused-imports/no-unused-imports': 'error',
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unused-expressions": "off",
+        "no-undef": "off",
     },
   }
 );

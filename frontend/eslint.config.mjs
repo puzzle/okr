0ad5337f3ef6a96c167831e5d7f158f04433939f
Yanick Minder <minder@puzzle.ch>
2024-11-22 11:41:54 +0100

    refactor config to extract rules to apply only on ts files

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,44 +5,42 @@ import stylistic from "@stylistic/eslint-plugin";
 import html from "@html-eslint/eslint-plugin";
 
 export default tsEslint.config(
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-    {
-        ...stylistic.configs['all-flat'],
-        files: ['**/*.ts'],
-    },
-    {
-      files: ['**/*.spec.ts'],
+  eslint.configs.recommended, ...tsEslint.configs.recommended, {
+    ...stylistic.configs["all-flat"],
+    files: ["**/*.ts"],
       rules: {
-        "@typescript-eslint/no-explicit-any": "off"
-      },
-    },
-    {
-        ...html.configs["flat/recommended"],
-        files: ["**/*.html"],
-        rules: {
-            ...html.configs["flat/recommended"].rules,
-            "@html-eslint/indent": ["error",2],
-
-        }
-    },
-  {
-    plugins: {
-      'unused-imports': unusedImports,
-      '@stylistic': stylistic
-    },
-    rules: {
-      'unused-imports/no-unused-imports': 'error',
+        "unused-imports/no-unused-imports": "error",
+        //disable rules so eslint passes, probably fix later
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-unused-expressions": "off",
         "no-undef": "off",
-      "@stylistic/function-call-argument-newline": ["error", "never"],
-      "@stylistic/padded-blocks": ["error", "never"],
-      "@stylistic/dot-location": ["error", "property"],
-      "@stylistic/newline-per-chained-call": ["error", { "ignoreChainWithDepth": 1 }],
-      "@stylistic/indent": ["error", 2],
-      "@stylistic/quotes":["error","double"],
+        "@typescript-eslint/no-explicit-any": "off",
+        //stylistic rules
+        "@stylistic/function-call-argument-newline": ["error", "never"],
+        "@stylistic/padded-blocks": ["error", "never"],
+        "@stylistic/dot-location": ["error", "property"],
+        "@stylistic/newline-per-chained-call": ["error", { "ignoreChainWithDepth": 1 }],
+        "@stylistic/indent": ["error", 2],
+        "@stylistic/quotes":["error","double"],
         "@stylistic/multiline-ternary": ["off"],
+      },
+  }, {
+    files: ["**/*.spec.ts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    },
+  }, {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    rules: {
+      ...html.configs["flat/recommended"].rules,
+      "@html-eslint/indent": ["error",2],
+
+    }
+  }, {
+    plugins: {
+      "unused-imports": unusedImports,
+      "@stylistic": stylistic
     },
   }
 );

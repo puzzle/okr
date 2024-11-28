3c6f8f20952f60181872b43874357cd6533619f2
Yanick Minder <minder@puzzle.ch>
2024-11-28 10:07:43 +0100

    get id from path as backup

--- backend/src/main/java/ch/puzzle/okr/deserializer/DeserializerHelper.java
+++ backend/src/main/java/ch/puzzle/okr/deserializer/DeserializerHelper.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
@@ -26,12 +28,14 @@ public class DeserializerHelper {
             throws IOException {
         ObjectMapper mapper = (ObjectMapper) jsonParser.getCodec();
         ObjectNode root = mapper.readTree(jsonParser);
-        JsonNode keyResultId = root.get(identifier);
-        if (keyResultId == null) {
+        JsonNode keyResultIdNode = root.get(identifier);
+        Long idFromPath = getIdFromPath();
+        if (keyResultIdNode == null && idFromPath == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "missing keyResult ID to deserialize keyResult DTO");
         }
-        KeyResult keyResult = keyResultBusinessService.getEntityById(keyResultId.asLong());
+        long id = keyResultIdNode == null ? idFromPath : keyResultIdNode.asLong();
+        KeyResult keyResult = keyResultBusinessService.getEntityById(id);
 
         if (keyResult.getKeyResultType() == null || !map.containsKey(keyResult.getKeyResultType())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "unsupported entity DTO to deserialize");
@@ -39,5 +43,16 @@ public class DeserializerHelper {
 
         Class<? extends T> aClass = map.get(keyResult.getKeyResultType());
         return mapper.readValue(root.toString(), aClass);
+    }
+
+    private Long getIdFromPath() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        Map attributes = (Map) request.getAttribute("org.springframework.web.servlet.View.pathVariables");
+        ;
+        return (Long) attributes.getOrDefault("id", null);
     }
 }

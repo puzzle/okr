1913f368a20244d6a8f3e4cebbea8a8ec299ce88
Yanick Minder <minder@puzzle.ch>
2024-12-09 10:27:54 +0100

    update backend formatter

--- backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
+++ backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
@@ -1,11 +1,9 @@
 package ch.puzzle.okr;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.GenericFilterBean;

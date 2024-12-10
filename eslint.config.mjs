8f73dd26b7d6ad79ffbc62c02d5776f0535bfd83
Miguel Lehmann <lehmann@puzzle.ch>
2024-12-10 10:03:16 +0100

    remove defult config so only naming gets checked

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,136 +1,103 @@
-import eslint from "@eslint/js";
 import tsEslint from "typescript-eslint";
-import unusedImports from "eslint-plugin-unused-imports";
-import stylistic from "@stylistic/eslint-plugin";
-import html from "@html-eslint/eslint-plugin";
-import css from "eslint-plugin-css-modules";
 
 export default tsEslint.config(
-    eslint.configs.recommended, ...tsEslint.configs.recommended, {
-        ...stylistic.configs["all-flat"],
-        files: ["**/*.ts"],
-        rules: {
-            "@typescript-eslint/naming-convention": [
-                "error",
-                {
-                    "selector": "variable",
-                    "format": ["camelCase"]
-                },
-                {
-                    "selector": "enum",
-                    "format": ["PascalCase"]
-                },
-                {
-                    "selector": "enumMember",
-                    "format": ["UPPER_CASE"]
-                },
-                {
-                    "selector": ["class", "interface"],
-                    "format": ["PascalCase"]
-                },
-            ]
-        },
-        overrides: [
-            {
-                files: ["e2e/*.ts"],
-                rules: {
-                    "@typescript-eslint/naming-convention": [
-                        "error",
-                        {
-                            "selector": "class",
-                            "format": ["kebab-case"]
-                        }
-                    ]
-                },
-            }
-        ]
-    },
-    {
-        ...
-            html.configs["flat/recommended"],
-        files:
-            ["**/*.html"],
-        rules:
-            {
-                "@typescript-eslint/naming-convention": [
-                    "error",
-                    {
-                        "selector": "default",
-                        "format": ["camelCase"],
-                        "leadingUnderscore": "allow"
-                    },
-                    {
-                        "selector": "variable",
-                        "format": ["camelCase", "UPPER_CASE"]
-                    },
-                    {
-                        "selector": "function",
-                        "format": ["camelCase"]
-                    },
-                    {
-                        "selector": ["class", "interface", "typeAlias", "enum"],
-                        "format": ["PascalCase"]
-                    },
-                    {
-                        "selector": "enumMember",
-                        "format": ["UPPER_CASE"]
-                    },
-                    {
-                        "selector": "property",
-                        "modifiers": ["private"],
-                        "format": ["camelCase"],
-                        "leadingUnderscore": "require"
-                    },
-                    {
-                        "selector": "method",
-                        "format": ["camelCase"]
-                    },
-                    {
-                        "selector": "typeParameter",
-                        "format": ["PascalCase"],
-                        "prefix": ["T"]
-                    }
-                ]
-            }
-    }
-    ,
-    {
-        ...
-            css.configs["flat/recommended"],
-        files:
-            ["**/*.scss"],
-        rules:
-            {
-                "@typescript-eslint/naming-convention": [
-                    "error",
-                    {
-                        "selector": "default",
-                        "format": ["kebab-case"]
-                    },
-                    {
-                        "selector": "variable",
-                        "format": ["kebab-case"]
-                    },
-                    {
-                        "selector": "class",
-                        "format": ["kebab-case"]
-                    },
-                    {
-                        "selector": "id",
-                        "format": ["kebab-case"]
-                    }
-                ]
-            }
-    }
-    ,
-    {
-        plugins: {
-            "unused-imports":
-            unusedImports,
-            "@stylistic":
-            stylistic
-        }
-        ,
-    }
-)
-;
+  {
+      extends: [tsEslint.configs.base],
+  },
+  {
+      files: ["**/*.ts"],
+      rules: {
+          "@typescript-eslint/naming-convention": [
+              "error",
+              {
+                  selector: "variable",
+                  format: ["camelCase"]
+              },
+              {
+                  selector: "enum",
+                  format: ["PascalCase"]
+              },
+              {
+                  selector: "enumMember",
+                  format: ["UPPER_CASE"]
+              },
+              {
+                  selector: ["class", "interface"],
+                  format: ["PascalCase"]
+              },
+              // {
+              //     selector: "class",
+              //     format: ["PascalCase"],
+              //     filter: {
+              //         regex: "^e2e/.*\\.ts$",
+              //         match: true
+              //     }
+              // }
+          ]
+      }
+  },
+  {
+      files: ["**/*.html"],
+      rules: {
+          "@typescript-eslint/naming-convention": [
+              "error",
+              {
+                  selector: "default",
+                  format: ["camelCase"],
+                  leadingUnderscore: "allow"
+              },
+              {
+                  selector: "variable",
+                  format: ["camelCase", "UPPER_CASE"]
+              },
+              {
+                  selector: "function",
+                  format: ["camelCase"]
+              },
+              {
+                  selector: ["class", "interface", "typeAlias", "enum"],
+                  format: ["PascalCase"]
+              },
+              {
+                  selector: "enumMember",
+                  format: ["UPPER_CASE"]
+              },
+              {
+                  selector: "property",
+                  modifiers: ["private"],
+                  format: ["camelCase"],
+                  leadingUnderscore: "require"
+              },
+              {
+                  selector: "method",
+                  format: ["camelCase"]
+              },
+              {
+                  selector: "typeParameter",
+                  format: ["PascalCase"],
+                  prefix: ["T"]
+              }
+          ]
+      }
+  },
+  {
+      // files: ["**/*.scss"],
+      // rules: {
+      //     "@typescript-eslint/naming-convention": [
+      //         "error",
+      //         {
+      //             selector: "default",
+      //             format: ["PascalCase"]
+      //         },
+      //         {
+      //             selector: "variable",
+      //             format: ["PascalCase"]
+      //         },
+      //         {
+      //             selector: "class",
+      //             format: ["PascalCase"]
+      //         },
+      //     ]
+      // }
+  },
+);

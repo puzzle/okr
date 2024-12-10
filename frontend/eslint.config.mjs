05a73ddc673c2d3b04a7d48004af557fd2b21223
Yanick Minder <minder@puzzle.ch>
2024-12-10 13:07:21 +0100

    add angular eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,51 +3,81 @@ import tsEslint from "typescript-eslint";
 import unusedImports from "eslint-plugin-unused-imports";
 import stylistic from "@stylistic/eslint-plugin";
 import html from "@html-eslint/eslint-plugin";
+import angular from "angular-eslint";
 
 export default tsEslint.config(
-  eslint.configs.recommended, ...tsEslint.configs.recommended, {
-    ...stylistic.configs["all-flat"],
-    files: ["**/*.ts"],
-      rules: {
-        "unused-imports/no-unused-imports": "error",
-        // ToDo: Disable rules so eslint passes, fix in followup ticket
-        "@typescript-eslint/no-unused-vars": "off",
-        "@typescript-eslint/no-unused-expressions": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
-        "no-undef": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-namespace": "off",
-        "prefer-rest-params": "off",
-        //stylistic rules
-        "@stylistic/function-call-argument-newline": ["error", "never"],
-        "@stylistic/padded-blocks": ["error", "never"],
-        "@stylistic/dot-location": ["error", "property"],
-        "@stylistic/newline-per-chained-call": ["error", { "ignoreChainWithDepth": 1 }],
-        "@stylistic/indent": ["error", 2],
-        "@stylistic/quotes":["error","double"],
-        "@stylistic/multiline-ternary": ["off"],
-      },
-  }, {
-    files: ["**/*.spec.ts"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "prefer-rest-params": "off",
+    {
+        extends: [
+            eslint.configs.recommended,
+            ...tsEslint.configs.recommended,
+            ...tsEslint.configs.stylistic,
+            ...angular.configs.tsRecommended,
+            ...stylistic.configs["all-flat"]
+        ],
+        // processor: angular.processInlineTemplates,
+        files: ["**/*.ts"],
+        rules: {
+            "unused-imports/no-unused-imports": "error",
+            // ToDo: Disable rules so eslint passes, fix in followup ticket
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-unused-expressions": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "no-undef": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "prefer-rest-params": "off",
+            //stylistic rules
+            "@stylistic/function-call-argument-newline": ["error", "never"],
+            "@stylistic/padded-blocks": ["error", "never"],
+            "@stylistic/dot-location": ["error", "property"],
+            "@stylistic/newline-per-chained-call": ["error", {"ignoreChainWithDepth": 1}],
+            "@stylistic/indent": ["error", 2],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/multiline-ternary": ["off"],
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: "app",
+                    style: "camelCase",
+                },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: "app",
+                    style: "kebab-case",
+                }
+            ]
+        },
     },
-  }, {
-    ...html.configs["flat/recommended"],
-    files: ["**/*.html"],
-    rules: {
-      ...html.configs["flat/recommended"].rules,
-      // ToDo: Disable rules so eslint passes, fix in followup ticket
-      "@html-eslint/require-img-alt": "off",
-      "@html-eslint/element-newline": "off",
-      //stylistic rules
-      "@html-eslint/indent": ["error",2],
+    {
+        files: ["**/*.spec.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "prefer-rest-params": "off",
+        },
+    },
+    {
+        files: ["**/*.html"],
+        extends: [
+            ...html.configs["flat/recommended"],
+            ...angular.configs.templateRecommended,
+            ...angular.configs.templateAccessibility,
+        ],
+        rules: {
+            // ToDo: Disable rules so eslint passes, fix in followup ticket
+            "@html-eslint/require-img-alt": "off",
+            "@html-eslint/element-newline": "off",
+            //stylistic rules
+            "@html-eslint/indent": ["error", 2],
+        }
+    },
+    {
+        plugins: {
+            "unused-imports": unusedImports,
+            "@stylistic": stylistic
+        },
     }
-  }, {
-    plugins: {
-      "unused-imports": unusedImports,
-      "@stylistic": stylistic
-    },
-  }
 );

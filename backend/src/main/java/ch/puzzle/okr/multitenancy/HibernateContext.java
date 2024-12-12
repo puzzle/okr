e24eb1896d655202e45dcefe30aab0440b06327a
Giannin <puzzle@giannin.ch>
2024-12-12 07:27:47 +0100

    Make `public static` variables also `final`

--- backend/src/main/java/ch/puzzle/okr/multitenancy/HibernateContext.java
+++ backend/src/main/java/ch/puzzle/okr/multitenancy/HibernateContext.java
@@ -10,9 +10,9 @@ public class HibernateContext {
     public static final String HIBERNATE_CONNECTION_PASSWORD = "hibernate.connection.password";
     public static final String HIBERNATE_MULTITENANCY = "hibernate.multiTenancy";
 
-    public static String SPRING_DATASOURCE_URL = "spring.datasource.url";
-    public static String SPRING_DATASOURCE_USERNAME = "spring.datasource.username";
-    public static String SPRING_DATASOURCE_PASSWORD = "spring.datasource.password";
+    public static final String SPRING_DATASOURCE_URL = "spring.datasource.url";
+    public static final String SPRING_DATASOURCE_USERNAME = "spring.datasource.username";
+    public static final String SPRING_DATASOURCE_PASSWORD = "spring.datasource.password";
 
     public record DbConfig(String url, String username, String password, String multiTenancy) {
 

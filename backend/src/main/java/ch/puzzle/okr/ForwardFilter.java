457519b8256f21acec14afe349e1245f3c7c75de
Giannin <puzzle@giannin.ch>
2024-12-12 13:52:59 +0100

    Rename logger to no overwrite field of parent class

--- backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
+++ backend/src/main/java/ch/puzzle/okr/ForwardFilter.java
@@ -11,13 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.GenericFilterBean;
 
 public class ForwardFilter extends GenericFilterBean {
-    private static final Logger logger = LoggerFactory.getLogger(ForwardFilter.class);
+    private static final Logger beanFilterLogger = LoggerFactory.getLogger(ForwardFilter.class);
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        logger.debug("====> pass through the filter '{}'", request.getRequestURI());
+        beanFilterLogger.debug("====> pass through the filter '{}'", request.getRequestURI());
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }

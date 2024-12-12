6c5de0dbd6b7fbf8f41017cdca80835e40a1bc42
Yanick Minder <minder@puzzle.ch>
2024-12-12 10:44:08 +0100

    add prettierignore file

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'http://pitc.okr.localhost:4200',
-    experimentalMemoryManagement: true,
-    testIsolation: true,
-    viewportWidth: 1920,
-    viewportHeight: 1080,
-  },
-  env: {
-    login_url: 'http://localhost:8544',
-  },
-});
+    e2e: {
+        baseUrl: 'http://pitc.okr.localhost:4200',
+        experimentalMemoryManagement: true,
+        testIsolation: true,
+        viewportWidth: 1920,
+        viewportHeight: 1080,
+    },
+    env: {
+        login_url: 'http://localhost:8544',
+    },
+})

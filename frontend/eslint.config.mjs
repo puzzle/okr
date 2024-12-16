eff0bbac39bfc2647e1481933fbb98370b92b8d2
Miguel Lehmann <lehmann@puzzle.ch>
2024-12-16 15:51:33 +0100

    add working regexes and work on the right naming rules to get checked

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -16,29 +16,35 @@ export default tsEslint.config(
 
           selector: ["class", "interface"], format: ["PascalCase"]
         }, {
-          selector: "typeProperty", types: ["string"], format: ["UPPER_CASE"]
-        }, {
-          selector: "classProperty", modifiers: ["readonly"], format: ["UPPER_CASE"]
-        }, {
-          selector: "variable", modifiers: ["const"], types: ["string"], format: ["UPPER_CASE"]
-        }, {
-          selector: "variable", modifiers: [], format: ["camelCase"]
-        }, {
+        //   It has to many exceptions for me to check this usefully
+        //
+        //   selector: "typeProperty", types: ["string"], modifiers: ["readonly"], format: ["UPPER_CASE"]
+        // },{
+        //   selector: "classProperty", modifiers: ["readonly"], format: ["UPPER_CASE"]
+        // }, {
+        //   selector: "variable", modifiers: ["global"], types: ["string"], format: ["UPPER_CASE"]
+        // }, {
+        //     selector: "variable", modifiers: [], format: ["camelCase"]
+        // },  {
           selector: "enum", format: ["PascalCase"]
         }, {
           selector: "enumMember", format: ["UPPER_CASE"]
         }, {
           selector: ["method", "function"], format: ["camelCase"]
         }, {
-          selector: "property", format: ["camelCase"]
-        }, {
-          selector: "typeParameter", format: ["PascalCase"], prefix: ["T"]
+        //   selector: "property", format: ["camelCase"]
+        // }, {
+          selector: "typeParameter", format: ["PascalCase"]
         }
           ],
-//        "id-match": [
-//          "error",
-//          "(?i:key.*result)?^(?:(?!KeyResult).)*"
-//        ],
+       "id-match": [
+         "error",
+         "((?=.*[Kk]?[Ee]?[Yy].?[Rr]?[Ee]?[Ss]?[Uu]?[Ll]?[Tt]?.*)(.*[Kk]eyResult.*|[A-Z_]*KEY_RESULT[A-Z_]*)|(?=.*[Cc]?[Hh]?[Ee]?[Cc]?[Kk].?[Ii]?[Nn]?.*)(.*[Cc]heckIn.*|[A-Z_]*CHECK_IN[A-Z_]*))|(?=(^(?!.*[Cc][Hh][Ee][Cc][Kk].?[Ii][Nn]).*)(?=^(?!.*[Kk][Ee][Yy].?[Rr][Ee][Ss][Uu][Ll][Tt]).*))"
+         // "((?=.*[Ss]?[Tt]?[Rr]?[Ee]?[Tt]?[Cc]?[Hh].?[Gg]?[Oo]?[Aa]?[Ll]?.*)(.*[Ss]tretchGoal.*|[A-Z_]*STRETCH_GOAL[A-Z_]*)|^(?!.*[Ss][Tt][Rr][Ee][Tt][Cc][Hh].?[Gg][Oo][Aa][Ll]).*)"
+         // "((?=.*[Kk]?[Ee]?[Yy].?[Rr]?[Ee]?[Ss]?[Uu]?[Ll]?[Tt]?.*)(.*[Kk]eyResult.*|[A-Z_]*KEY_RESULT[A-Z_]*)|^(?!.*[Kk][Ee][Yy].?[Rr][Ee][Ss][Uu][Ll][Tt]).*)"
+         // "((?=.*[Cc]?[Hh]?[Ee]?[Cc]?[Kk].?[Ii]?[Nn]?.*)(.*[Cc]heckIn.*|[A-Z_]*CHECK_IN[A-Z_]*)|^(?!.*[Cc][Hh][Ee][Cc][Kk].?[Ii][Nn]).*)"
+         // "((?=.*[Tt]?[Ee]?[Aa]?[Mm].?[Mm]?[Aa]?[Nn]?[Aa]?[Gg]?[Ee]?[Mm]?[Ee]?[Nn]?[Tt]?.*)(.*[Tt]eamManagement.*|[A-Z_]*TEAM_MANAGEMENT[A-Z_]*)|^(?!.*[Tt][Ee][Aa][Mm].?[Mm][Aa][Nn][Aa][Gg][Ee][Mm][Ee][Nn][Tt]).*)"
+       ]
       }
   }, {
 
@@ -53,3 +59,4 @@ export default tsEslint.config(
   //     files: ["**/*.scss"], rules: {}
   // },
 );
+

116abcbb9cb83c6cdf9f3a912314c723d952e4c5
Nevio Di Gennaro <digennaro@puzzle.ch>
2024-12-17 11:38:41 +0100

    turn prefer-rest-params on

--- frontend/src/global.ts
+++ frontend/src/global.ts
@@ -5,9 +5,8 @@ declare global {
   }
 }
 
-String.prototype.format = function () {
-  const args = Array.from(arguments)
-    .flat();
+String.prototype.format = function (...args: any[]): string {
+  args.flat();
   return this.replace(/{(\d+)}/g,
     function (match, index) {
       return typeof args[index] == "undefined" ? match : args[index];

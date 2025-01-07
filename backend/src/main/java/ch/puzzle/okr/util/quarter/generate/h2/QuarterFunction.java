8d16356d8f363f8c100f8949d3e196c76a5b6140
clean-coder <jean-claude.brantschen@gmx.ch>
2025-01-07 09:29:12 +0100

    code cleanup (2 variablen statt einer Map)

--- backend/src/main/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunction.java
+++ backend/src/main/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunction.java
@@ -3,8 +3,6 @@ package ch.puzzle.okr.util.quarter.generate.h2;
 import ch.puzzle.okr.util.quarter.generate.QuarterData;
 import ch.puzzle.okr.util.quarter.generate.Quarters;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -19,11 +17,11 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("integration-test")
 public class QuarterFunction {
-    private static final int CURRENT_QUARTER_MAP_KEY = 0;
-    private static final int NEXT_QUARTER_MAP_KEY = 1;
 
-    private static final Map<Integer, QuarterData> QUARTERS = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(QuarterFunction.class);
+
+    private static QuarterData currentQuarter;
+    private static QuarterData nextQuarter;
 
     // used in V100_0_0__TestData.sql
     public static void initQuarterData() {
@@ -31,50 +29,49 @@ public class QuarterFunction {
     }
 
     public static void initQuarterData(LocalDate now) {
-        registerCurrentQuarterForDate(now, CURRENT_QUARTER_MAP_KEY);
+        currentQuarter = createQuarterForDate(now);
 
         LocalDate in3Months = now.plusMonths(3);
-        registerCurrentQuarterForDate(in3Months, NEXT_QUARTER_MAP_KEY);
+        nextQuarter = createQuarterForDate(in3Months);
     }
 
-    private static void registerCurrentQuarterForDate(LocalDate date, int mapKey) {
+    private static QuarterData createQuarterForDate(LocalDate date) {
         Quarters quarters = new Quarters(date.getYear());
-        QuarterData currentQuarter = quarters.currentQuarter(date);
-        QUARTERS.put(mapKey, currentQuarter);
+        return quarters.currentQuarter(date);
     }
 
     public static String currentQuarterLabel() {
-        var label = QUARTERS.get(CURRENT_QUARTER_MAP_KEY).label();
+        var label = currentQuarter.label();
         logger.info("currentQuarterLabel    : {}", label);
         return label;
     }
 
     public static String currentQuarterStartDate() {
-        var start = QUARTERS.get(CURRENT_QUARTER_MAP_KEY).startDateAsIsoString();
+        var start = currentQuarter.startDateAsIsoString();
         logger.info("currentQuarterStartDate: {}", start);
         return start;
     }
 
     public static String currentQuarterEndDate() {
-        var end = QUARTERS.get(CURRENT_QUARTER_MAP_KEY).endDateAsIsoString();
+        var end = currentQuarter.endDateAsIsoString();
         logger.info("currentQuarterEndDate  : {}", end);
         return end;
     }
 
     public static String nextQuarterLabel() {
-        var label = QUARTERS.get(NEXT_QUARTER_MAP_KEY).label();
+        var label = nextQuarter.label();
         logger.info("nextQuarterLabel       : {}", label);
         return label;
     }
 
     public static String nextQuarterStartDate() {
-        var start = QUARTERS.get(NEXT_QUARTER_MAP_KEY).startDateAsIsoString();
+        var start = nextQuarter.startDateAsIsoString();
         logger.info("nextQuarterStartDate   : {}", start);
         return start;
     }
 
     public static String nextQuarterEndDate() {
-        var end = QUARTERS.get(NEXT_QUARTER_MAP_KEY).endDateAsIsoString();
+        var end = nextQuarter.endDateAsIsoString();
         logger.info("nextQuarterEndDate     : {}", end);
         return end;
     }

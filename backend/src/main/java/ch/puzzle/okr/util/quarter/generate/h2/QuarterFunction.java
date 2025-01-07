43d5918c7a493c6b50cc3fe03858eccfab99ccea
Jean-Claude Brantschen <jean-claude.brantschen@gmx.ch>
2025-01-07 10:11:54 +0100

    Bug/1270 fix quarter function test (#1274)
    * fix QuarterFunctionTest
    
    * formatting code
    
    * code cleanup (2 variablen statt einer Map)

--- backend/src/main/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunction.java
+++ backend/src/main/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunction.java
@@ -3,8 +3,6 @@ package ch.puzzle.okr.util.quarter.generate.h2;
 import ch.puzzle.okr.util.quarter.generate.QuarterData;
 import ch.puzzle.okr.util.quarter.generate.Quarters;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
@@ -19,59 +17,61 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile("integration-test")
 public class QuarterFunction {
-    public static final int CURRENT_QUARTER_DB_ID = 2;
-    public static final int NEXT_QUARTER_DB_ID = 3;
 
-    private static Map<Integer, QuarterData> quarters = new HashMap<>();
     private static final Logger logger = LoggerFactory.getLogger(QuarterFunction.class);
 
-    public static void initQuarterData() {
-        LocalDate now = LocalDate.now();
-        registerCurrentQuarterForDate(now, CURRENT_QUARTER_DB_ID);
+    private static QuarterData currentQuarter;
+    private static QuarterData nextQuarter;
 
-        LocalDate inThreeMonths = now.plusMonths(3);
-        registerCurrentQuarterForDate(inThreeMonths, NEXT_QUARTER_DB_ID);
+    // used in V100_0_0__TestData.sql
+    public static void initQuarterData() {
+        initQuarterData(LocalDate.now());
     }
 
-    private static void registerCurrentQuarterForDate(LocalDate date, int dbId) {
+    public static void initQuarterData(LocalDate now) {
+        currentQuarter = createQuarterForDate(now);
+
+        LocalDate in3Months = now.plusMonths(3);
+        nextQuarter = createQuarterForDate(in3Months);
+    }
+
+    private static QuarterData createQuarterForDate(LocalDate date) {
         Quarters quarters = new Quarters(date.getYear());
-        QuarterData currentQuarter = quarters.currentQuarter(date);
-        QuarterFunction.quarters.put(dbId, currentQuarter);
+        return quarters.currentQuarter(date);
     }
 
     public static String currentQuarterLabel() {
-        String label = quarters.get(CURRENT_QUARTER_DB_ID).label();
+        var label = currentQuarter.label();
         logger.info("currentQuarterLabel    : {}", label);
         return label;
-
     }
 
     public static String currentQuarterStartDate() {
-        String start = quarters.get(CURRENT_QUARTER_DB_ID).startDateAsIsoString();
+        var start = currentQuarter.startDateAsIsoString();
         logger.info("currentQuarterStartDate: {}", start);
         return start;
     }
 
     public static String currentQuarterEndDate() {
-        String end = quarters.get(CURRENT_QUARTER_DB_ID).endDateAsIsoString();
+        var end = currentQuarter.endDateAsIsoString();
         logger.info("currentQuarterEndDate  : {}", end);
         return end;
     }
 
     public static String nextQuarterLabel() {
-        String label = quarters.get(NEXT_QUARTER_DB_ID).label();
+        var label = nextQuarter.label();
         logger.info("nextQuarterLabel       : {}", label);
         return label;
     }
 
     public static String nextQuarterStartDate() {
-        String start = quarters.get(NEXT_QUARTER_DB_ID).startDateAsIsoString();
+        var start = nextQuarter.startDateAsIsoString();
         logger.info("nextQuarterStartDate   : {}", start);
         return start;
     }
 
     public static String nextQuarterEndDate() {
-        String end = quarters.get(NEXT_QUARTER_DB_ID).endDateAsIsoString();
+        var end = nextQuarter.endDateAsIsoString();
         logger.info("nextQuarterEndDate     : {}", end);
         return end;
     }
--- backend/src/test/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunctionTest.java
+++ backend/src/test/java/ch/puzzle/okr/util/quarter/generate/h2/QuarterFunctionTest.java
@@ -3,42 +3,43 @@ package ch.puzzle.okr.util.quarter.generate.h2;
 import static ch.puzzle.okr.util.quarter.generate.h2.QuarterFunction.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class QuarterFunctionTest {
 
-    @DisplayName("Should return correct current quarter data")
+    @DisplayName("QuarterFunction should return correct current quarter data")
     @Test
-    void shouldReturnCorrectCurrentQuarterData() {
+    void quarterFunctionShouldReturnCorrectCurrentQuarterData() {
         // arrange
-        initQuarterData();
+        initQuarterData(LocalDate.of(2024, 12, 24));
 
         // act
-        String currentQuarter = "(2, " //
+        String currentQuarter = "(" //
                                 + currentQuarterLabel() + ", " //
                                 + currentQuarterStartDate() + ", " //
                                 + currentQuarterEndDate() + ")";
 
         // assert
-        String expectedCurrent = "(2, GJ 24/25-Q2, 2024-10-01, 2024-12-31)";
+        String expectedCurrent = "(GJ 24/25-Q2, 2024-10-01, 2024-12-31)";
         assertEquals(expectedCurrent, currentQuarter);
     }
 
-    @DisplayName("Should return correct next quarter data")
+    @DisplayName("QuarterFunction should return correct next quarter data")
     @Test
-    void shouldReturnCorrectNextQuarterData() {
+    void quarterFunctionShouldReturnCorrectNextQuarterData() {
         // arrange
-        initQuarterData();
+        initQuarterData(LocalDate.of(2024, 12, 24));
 
         // act
-        String nextQuarter = "(3, " //
+        String nextQuarter = "(" //
                              + nextQuarterLabel() + ", " //
                              + nextQuarterStartDate() + ", " //
                              + nextQuarterEndDate() + ")";
 
         // assert
-        String expectedNext = "(3, GJ 24/25-Q3, 2025-01-01, 2025-03-31)";
+        String expectedNext = "(GJ 24/25-Q3, 2025-01-01, 2025-03-31)";
         assertEquals(expectedNext, nextQuarter);
     }
 }

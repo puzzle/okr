9ee69aa135ac717a2714b9669921be9ba98b42bf
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-01-09 13:18:39 +0100

    fix unit tests failing becuase of new attribute inside checkInDto

--- backend/src/test/java/ch/puzzle/okr/mapper/checkin/helper/TestDataHelper.java
+++ backend/src/test/java/ch/puzzle/okr/mapper/checkin/helper/TestDataHelper.java
@@ -1,14 +1,54 @@
 package ch.puzzle.okr.mapper.checkin.helper;
 
-import static ch.puzzle.okr.mapper.checkin.helper.TestDataConstants.*;
-
+import ch.puzzle.okr.models.User;
 import ch.puzzle.okr.models.checkin.CheckIn;
 import ch.puzzle.okr.models.checkin.CheckInMetric;
 import ch.puzzle.okr.models.checkin.CheckInOrdinal;
 import ch.puzzle.okr.models.keyresult.KeyResult;
 import ch.puzzle.okr.models.keyresult.KeyResultMetric;
 
+import static ch.puzzle.okr.mapper.checkin.helper.TestDataConstants.*;
+
 public class TestDataHelper {
+
+    public static final User user = User.Builder
+            .builder()
+            .withId(1L)
+            .withFirstName("Bob")
+            .withLastName("Kaufmann")
+            .withEmail("kaufmann@puzzle.ch")
+            .build();
+
+    private static final KeyResult keyResult = KeyResultMetric.Builder
+            .builder() //
+            .withId(KEY_RESULT_ID) //
+            .build();
+    private static final CheckIn checkInMetric = CheckInMetric.Builder
+            .builder() //
+            .withId(CHECK_IN_ID) //
+            .withVersion(CHECK_IN_VERSION) //
+            .withChangeInfo(CHECK_IN_CHANGE_INFO) //
+            .withInitiatives(CHECK_IN_INITIATIVES) //
+            .withConfidence(CHECK_IN_CONFIDENCE) //
+            .withKeyResult(keyResult) //
+            .withCreatedBy(user)
+            .withCreatedOn(CHECK_IN_CREATE_DATE_TIME) //
+            .withModifiedOn(CHECK_IN_MODIFIED_DATE_TIME) //
+            .withValue(CHECK_IN_METRIC_VALUE) //
+            .build();
+    private static final CheckIn checkInOrdinal = CheckInOrdinal.Builder
+            .builder() //
+            .withId(CHECK_IN_ID) //
+            .withVersion(CHECK_IN_VERSION) //
+            .withChangeInfo(CHECK_IN_CHANGE_INFO) //
+            .withInitiatives(CHECK_IN_INITIATIVES) //
+            .withConfidence(CHECK_IN_CONFIDENCE) //
+            .withKeyResult(keyResult) //
+            .withCreatedBy(user)
+            .withCreatedOn(CHECK_IN_CREATE_DATE_TIME) //
+            .withModifiedOn(CHECK_IN_MODIFIED_DATE_TIME) //
+            .withZone(CHECK_IN_ORDINAL_ZONE)
+            .build();
 
     public static KeyResult keyResult() {
         return keyResult;
@@ -23,35 +63,4 @@ public class TestDataHelper {
         checkInOrdinal.setWriteable(CHECK_IN_IS_WRITEABLE);
         return (CheckInOrdinal) TestDataHelper.checkInOrdinal;
     }
-
-    private static final KeyResult keyResult = KeyResultMetric.Builder
-            .builder() //
-            .withId(KEY_RESULT_ID) //
-            .build();
-
-    private static final CheckIn checkInMetric = CheckInMetric.Builder
-            .builder() //
-            .withId(CHECK_IN_ID) //
-            .withVersion(CHECK_IN_VERSION) //
-            .withChangeInfo(CHECK_IN_CHANGE_INFO) //
-            .withInitiatives(CHECK_IN_INITIATIVES) //
-            .withConfidence(CHECK_IN_CONFIDENCE) //
-            .withKeyResult(keyResult) //
-            .withCreatedOn(CHECK_IN_CREATE_DATE_TIME) //
-            .withModifiedOn(CHECK_IN_MODIFIED_DATE_TIME) //
-            .withValue(CHECK_IN_METRIC_VALUE) //
-            .build();
-
-    private static final CheckIn checkInOrdinal = CheckInOrdinal.Builder
-            .builder() //
-            .withId(CHECK_IN_ID) //
-            .withVersion(CHECK_IN_VERSION) //
-            .withChangeInfo(CHECK_IN_CHANGE_INFO) //
-            .withInitiatives(CHECK_IN_INITIATIVES) //
-            .withConfidence(CHECK_IN_CONFIDENCE) //
-            .withKeyResult(keyResult) //
-            .withCreatedOn(CHECK_IN_CREATE_DATE_TIME) //
-            .withModifiedOn(CHECK_IN_MODIFIED_DATE_TIME) //
-            .withZone(CHECK_IN_ORDINAL_ZONE)
-            .build();
 }

de7820f9b76da34ffe24cf981c89d10c678bb251
Nevio Di Gennaro <digennaro@puzzle.ch>
2025-01-14 08:01:16 +0100

    fix action being asigned to the wrong action

--- backend/src/main/java/ch/puzzle/okr/service/business/ObjectiveBusinessService.java
+++ backend/src/main/java/ch/puzzle/okr/service/business/ObjectiveBusinessService.java
@@ -1,8 +1,5 @@
 package ch.puzzle.okr.service.business;
 
-import static ch.puzzle.okr.Constants.KEY_RESULT_TYPE_METRIC;
-import static ch.puzzle.okr.Constants.KEY_RESULT_TYPE_ORDINAL;
-
 import ch.puzzle.okr.models.Action;
 import ch.puzzle.okr.models.Objective;
 import ch.puzzle.okr.models.authorization.AuthorizationUser;
@@ -12,13 +9,17 @@ import ch.puzzle.okr.models.keyresult.KeyResultOrdinal;
 import ch.puzzle.okr.service.persistence.ObjectivePersistenceService;
 import ch.puzzle.okr.service.validation.ObjectiveValidationService;
 import jakarta.transaction.Transactional;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+import static ch.puzzle.okr.Constants.KEY_RESULT_TYPE_METRIC;
+import static ch.puzzle.okr.Constants.KEY_RESULT_TYPE_ORDINAL;
 
 @Service
 public class ObjectiveBusinessService implements BusinessServiceInterface<Long, Objective> {
@@ -111,15 +112,10 @@ public class ObjectiveBusinessService implements BusinessServiceInterface<Long, 
      * Create a new Objective (with a new ID) and copy from a source Objective
      * (identified by id) the KeyResults. The CheckIns are not copied.
      *
-     * @param id
-     *            ID of the source Objective
-     * @param objective
-     *            New Objective with no KeyResults
-     * @param authorizationUser
-     *            AuthorizationUser
-     * @param keyResultIds
-     *            KeyResultIds to copy
-     *
+     * @param id                ID of the source Objective
+     * @param objective         New Objective with no KeyResults
+     * @param authorizationUser AuthorizationUser
+     * @param keyResultIds      KeyResultIds to copy
      * @return New Objective with copied KeyResults form the source Objective
      */
     @Transactional
@@ -128,8 +124,8 @@ public class ObjectiveBusinessService implements BusinessServiceInterface<Long, 
         Objective duplicatedObjective = createEntity(objective, authorizationUser);
         for (Long keyResult : keyResultIds) {
             duplicateKeyResult(authorizationUser,
-                               keyResultBusinessService.getEntityById(keyResult),
-                               duplicatedObjective);
+                    keyResultBusinessService.getEntityById(keyResult),
+                    duplicatedObjective);
         }
         return duplicatedObjective;
     }
@@ -146,19 +142,7 @@ public class ObjectiveBusinessService implements BusinessServiceInterface<Long, 
     }
 
     private KeyResult makeCopyOfKeyResultMetric(KeyResult keyResult, Objective duplicatedObjective) {
-
-        List<Action> actionList = keyResult
-                .getActionList()
-                .stream()
-                .map(e -> Action.Builder
-                        .builder()
-                        .withKeyResult(e.getKeyResult())
-                        .isChecked(e.isChecked())
-                        .withAction(e.getActionPoint())
-                        .withPriority(e.getPriority())
-                        .build())
-                .toList();
-        return KeyResultMetric.Builder
+        KeyResult copyOfMetricKeyResult = KeyResultMetric.Builder
                 .builder() //
                 .withObjective(duplicatedObjective) //
                 .withTitle(keyResult.getTitle()) //
@@ -167,24 +151,24 @@ public class ObjectiveBusinessService implements BusinessServiceInterface<Long, 
                 .withUnit(((KeyResultMetric) keyResult).getUnit()) //
                 .withBaseline(((KeyResultMetric) keyResult).getBaseline()) //
                 .withStretchGoal(((KeyResultMetric) keyResult).getStretchGoal()) //
-                .withActionList(actionList)
                 .build();
-    }
-
-    private KeyResult makeCopyOfKeyResultOrdinal(KeyResult keyResult, Objective duplicatedObjective) {
-
         List<Action> actionList = keyResult
                 .getActionList()
                 .stream()
                 .map(e -> Action.Builder
                         .builder()
-                        .withKeyResult(e.getKeyResult())
+                        .withKeyResult(copyOfMetricKeyResult)
                         .isChecked(e.isChecked())
                         .withAction(e.getActionPoint())
                         .withPriority(e.getPriority())
                         .build())
                 .toList();
-        return KeyResultOrdinal.Builder
+        copyOfMetricKeyResult.setActionList(actionList);
+        return copyOfMetricKeyResult;
+    }
+
+    private KeyResult makeCopyOfKeyResultOrdinal(KeyResult keyResult, Objective duplicatedObjective) {
+        KeyResult copyOfOrdinalKeyResult = KeyResultOrdinal.Builder
                 .builder() //
                 .withObjective(duplicatedObjective) //
                 .withTitle(keyResult.getTitle()) //
@@ -193,8 +177,20 @@ public class ObjectiveBusinessService implements BusinessServiceInterface<Long, 
                 .withCommitZone(((KeyResultOrdinal) keyResult).getCommitZone()) //
                 .withTargetZone(((KeyResultOrdinal) keyResult).getTargetZone()) //
                 .withStretchZone(((KeyResultOrdinal) keyResult).getStretchZone()) //
-                .withActionList(actionList)
                 .build();
+        List<Action> actionList = keyResult
+                .getActionList()
+                .stream()
+                .map(e -> Action.Builder
+                        .builder()
+                        .withKeyResult(copyOfOrdinalKeyResult)
+                        .isChecked(e.isChecked())
+                        .withAction(e.getActionPoint())
+                        .withPriority(e.getPriority())
+                        .build())
+                .toList();
+        copyOfOrdinalKeyResult.setActionList(actionList);
+        return copyOfOrdinalKeyResult;
     }
 
     @Transactional

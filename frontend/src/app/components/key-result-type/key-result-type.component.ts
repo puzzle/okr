7bd8fe26d80d02ea435e61e153727e9aca9d7fb1
Yanick Minder <minder@puzzle.ch>
2025-01-16 13:30:29 +0100

    implement logic for autocompletion

--- frontend/src/app/components/key-result-type/key-result-type.component.ts
+++ frontend/src/app/components/key-result-type/key-result-type.component.ts
@@ -1,12 +1,30 @@
 import { AfterContentInit, Component, Input } from '@angular/core';
 import { KeyResult } from '../../shared/types/model/key-result';
-import { ControlContainer, FormGroup, FormGroupDirective } from '@angular/forms';
+import { ControlContainer, FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
 import { KeyResultMetric } from '../../shared/types/model/key-result-metric';
 import { KeyResultOrdinal } from '../../shared/types/model/key-result-ordinal';
 import { Unit } from '../../shared/types/enums/unit';
 import { formInputCheck, hasFormFieldErrors } from '../../shared/common';
 import { getFullNameOfUser, User } from '../../shared/types/model/user';
 import { Observable, Subject } from 'rxjs';
+
+enum KeyResultMetricField {
+  BASELINE,
+  TARGETGOAL,
+  STRETCHGOAL,
+  NONE
+}
+
+interface MetricControl {
+  identifier: KeyResultMetricField;
+  control: FormControl<any> | null;
+}
+
+interface MetricValue {
+  baseline: number;
+  targetGoal: number;
+  stretchGoal: number;
+}
 
 @Component({
   selector: 'app-key-result-type',
@@ -54,13 +72,87 @@ export class KeyResultTypeComponent implements AfterContentInit {
     return this.keyResultForm?.get('keyResultType')?.value;
   }
 
-  updateMetricValue(value: any) {
-    console.log(value);
+  updateMetricValue(changed: KeyResultMetricField, value: any) {
+    const formGroupMetric = this.keyResultForm.get('metric');
+    let formGroupValue = { ...formGroupMetric?.value,
+      ...value };
+    formGroupValue = { baseline: +formGroupValue.baseline,
+      targetGoal: +formGroupValue.targetGoal,
+      stretchGoal: +formGroupValue.stretchGoal } as MetricValue;
+    const controls = this.getMetricControls();
+    const changedControlsAmount = controls.filter((obj) => obj.control?.dirty).length;
+    if (this.isCreating() && changedControlsAmount == 2) {
+      const unchangedControl = controls.find((obj) => obj.control?.dirty === false);
+      const identifier: KeyResultMetricField = unchangedControl?.identifier as KeyResultMetricField || KeyResultMetricField.NONE;
+      const newMetricValue = this.calculateValueForField(formGroupValue, identifier);
+      formGroupMetric?.patchValue(newMetricValue, { emitEvent: false });
+    } else {
+      const newMetricValue = this.calculateValueAfterChanged(formGroupValue, changed);
+      formGroupMetric?.patchValue(newMetricValue, { emitEvent: false });
+    }
+  }
+
+  calculateValueAfterChanged(values: MetricValue, changed: KeyResultMetricField) {
+    switch (changed) {
+      case KeyResultMetricField.STRETCHGOAL:
+      case KeyResultMetricField.BASELINE: {
+        return this.calculateValueForField(values, KeyResultMetricField.TARGETGOAL);
+      }
+      case KeyResultMetricField.TARGETGOAL: {
+        return this.calculateValueForField(values, KeyResultMetricField.STRETCHGOAL);
+      }
+      case KeyResultMetricField.NONE: {
+        return {};
+      }
+    }
+  }
+
+  calculateValueForField(values: MetricValue, field: KeyResultMetricField) {
+    console.log(values);
+    switch (field) {
+      case KeyResultMetricField.BASELINE: {
+        return { baseline: (values.targetGoal - (values.stretchGoal - values.baseline)) / 0.3 };
+      }
+
+      case KeyResultMetricField.TARGETGOAL: {
+        return { targetGoal: (values.stretchGoal - values.baseline) * 0.7 + values.baseline };
+      }
+
+      case KeyResultMetricField.STRETCHGOAL: {
+        return { stretchGoal: (values.targetGoal - values.baseline) / 0.7 + values.baseline };
+      }
+
+      case KeyResultMetricField.NONE: {
+        return {};
+      }
+    }
   }
 
   protected readonly getFullNameOfUser = getFullNameOfUser;
 
   ngAfterContentInit(): void {
-    this.keyResultForm.get('metric')?.valueChanges.subscribe((value: any) => this.updateMetricValue(value));
+    const formGroupMetric = this.keyResultForm.get('metric');
+    const formGroupValue = formGroupMetric?.value;
+    delete formGroupValue['unit'];
+    formGroupMetric?.get('baseline')?.valueChanges.subscribe((value: any) => this.updateMetricValue(KeyResultMetricField.BASELINE, { baseline: value }));
+    formGroupMetric?.get('targetGoal')?.valueChanges.subscribe((value) => this.updateMetricValue(KeyResultMetricField.TARGETGOAL, { targetGoal: value }));
+    formGroupMetric?.get('stretchGoal')?.valueChanges.subscribe((value) => this.updateMetricValue(KeyResultMetricField.STRETCHGOAL, { stretchGoal: value }));
+  }
+
+
+  getMetricControls(): MetricControl[] {
+    const formGroupMetric: FormGroup = this.keyResultForm.get('metric') as FormGroup;
+    const controls = formGroupMetric?.controls;
+    delete controls['unit'];
+    return Object.keys(controls)
+      .map((key: string) => {
+        return { identifier: KeyResultMetricField[key.toUpperCase() as keyof typeof KeyResultMetricField],
+          control: formGroupMetric?.get(key) } as MetricControl;
+      });
+  }
+
+  isCreating() {
+    return this.keyResult?.id == null;
   }
 }
+

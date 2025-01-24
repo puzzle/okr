8510d73e1de9016df32a9940f3bec67fb2e77344
Yanick Minder <minder@puzzle.ch>
2025-01-24 10:54:39 +0100

    remove complexity of SpringSecurityAuditorAware

--- backend/src/main/java/ch/puzzle/okr/SpringSecurityAuditorAware.java
+++ backend/src/main/java/ch/puzzle/okr/SpringSecurityAuditorAware.java
@@ -1,36 +1,23 @@
 package ch.puzzle.okr;
 
 import ch.puzzle.okr.models.User;
-import ch.puzzle.okr.security.JwtHelper;
 import ch.puzzle.okr.service.authorization.AuthorizationService;
-import java.util.Optional;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 class SpringSecurityAuditorAware implements AuditorAware<User> {
 
-    private final JwtHelper jwtHelper;
     private final AuthorizationService authorizationService;
 
-    public SpringSecurityAuditorAware(JwtHelper jwtHelper, AuthorizationService authorizationService) {
-        this.jwtHelper = jwtHelper;
+    public SpringSecurityAuditorAware(AuthorizationService authorizationService) {
         this.authorizationService = authorizationService;
     }
 
     @Override
     public Optional<User> getCurrentAuditor() {
-        return Optional
-                .ofNullable(SecurityContextHolder.getContext())
-                .map(SecurityContext::getAuthentication)
-                .filter(Authentication::isAuthenticated)
-                .map(Authentication::getPrincipal)
-                .map(e -> (Jwt) e)
-                .map(jwtHelper::getUserFromJwt)
-                .map(e -> authorizationService.updateOrAddAuthorizationUser().user());
+        return Optional.ofNullable(authorizationService.updateOrAddAuthorizationUser().user());
     }
 }

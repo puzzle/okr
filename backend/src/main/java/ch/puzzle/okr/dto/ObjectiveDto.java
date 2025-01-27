9723e65ea28038fa77dd2fd29f957a5d2785bb2a
Manuel <moeri@puzzle.ch>
2025-01-27 15:05:54 +0100

    Remove unwanted attribute on ObjectiveDto and mapper

--- backend/src/main/java/ch/puzzle/okr/dto/ObjectiveDto.java
+++ backend/src/main/java/ch/puzzle/okr/dto/ObjectiveDto.java
@@ -3,7 +3,6 @@ package ch.puzzle.okr.dto;
 import ch.puzzle.okr.models.State;
 import java.time.LocalDateTime;
 
-public record ObjectiveDto(Long id, int version, String title, Long teamId, Long quarterId, String quarterLabel,
-        boolean isBacklogQuarter, String description, State state, LocalDateTime createdOn, LocalDateTime modifiedOn,
+public record ObjectiveDto(Long id, int version, String title, Long teamId, Long quarterId, String description, State state, LocalDateTime createdOn, LocalDateTime modifiedOn,
         boolean isWriteable) {
 }

26bc444bf25ff4703219a61713cd58262dac6079
Yanick Minder <minder@puzzle.ch>
2025-01-30 11:21:18 +0100

    format

--- backend/src/test/java/ch/puzzle/okr/test/WithMockAuthUserSecurityContextFactory.java
+++ backend/src/test/java/ch/puzzle/okr/test/WithMockAuthUserSecurityContextFactory.java
@@ -1,10 +1,9 @@
 package ch.puzzle.okr.test;
 
 import ch.puzzle.okr.models.User;
+import ch.puzzle.okr.service.persistence.UserPersistenceService;
 import java.util.List;
 import java.util.Optional;
-
-import ch.puzzle.okr.service.persistence.UserPersistenceService;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -22,19 +21,17 @@ public class WithMockAuthUserSecurityContextFactory implements WithSecurityConte
     @Override
     public SecurityContext createSecurityContext(WithMockAuthUser annotation) {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
-        User  user = User.Builder
+        User user = User.Builder
                 .builder()
                 .withEmail(annotation.email())
                 .withFirstName("mocked user first name for test")
                 .withLastName("mocked user last name for test")
                 .build();
         Optional<User> byEmail = this.userPersistenceService.findByEmail(annotation.email());
-        byEmail.ifPresentOrElse(
-                (u) -> {
-                   user.setFirstName(u.getFirstName());
-                   user.setLastName(u.getLastName());
-                }, ()->{}
-        );
+        byEmail.ifPresentOrElse((u) -> {
+            user.setFirstName(u.getFirstName());
+            user.setLastName(u.getLastName());
+        }, () -> {});
         context.setAuthentication(new JwtAuthenticationToken(TestHelper.mockJwtToken(user), List.of()));
         return context;
     }

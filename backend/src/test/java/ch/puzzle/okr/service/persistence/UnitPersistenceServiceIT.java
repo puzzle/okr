df6b7f7bdf42a96d27a218f6e646d83b39a51a03
Yanick Minder <minder@puzzle.ch>
2025-02-27 20:35:05 +0100

    apply formatter

--- backend/src/test/java/ch/puzzle/okr/service/persistence/UnitPersistenceServiceIT.java
+++ backend/src/test/java/ch/puzzle/okr/service/persistence/UnitPersistenceServiceIT.java
@@ -5,10 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import ch.puzzle.okr.models.Unit;
 import ch.puzzle.okr.models.User;
-import ch.puzzle.okr.multitenancy.TenantContext;
 import ch.puzzle.okr.test.SpringIntegrationTest;
 import ch.puzzle.okr.test.WithMockAuthUser;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;

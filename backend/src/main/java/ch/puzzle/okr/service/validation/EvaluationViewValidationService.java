21d1b7e6de11d4e5aedf0ab50291b6518c4c3df5
Yanick Minder <minder@puzzle.ch>
2025-03-18 10:38:28 +0100

    Add validationservice for EvaluationView #1467

---
+++ backend/src/main/java/ch/puzzle/okr/service/validation/EvaluationViewValidationService.java
@@ -0,0 +1,41 @@
+package ch.puzzle.okr.service.validation;
+
+import ch.puzzle.okr.models.evaluation.EvaluationView;
+import ch.puzzle.okr.models.evaluation.EvaluationViewId;
+import ch.puzzle.okr.repository.EvaluationViewRepository;
+import ch.puzzle.okr.service.persistence.EvaluationViewPersistenceService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EvaluationViewValidationService extends
+                                             ValidationBase<EvaluationView, EvaluationViewId, EvaluationViewRepository, EvaluationViewPersistenceService> {
+
+    private final QuarterValidationService quarterValidationService;
+    private final TeamValidationService teamValidationService;
+
+    EvaluationViewValidationService(EvaluationViewPersistenceService persistenceService,
+                                    QuarterValidationService quarterValidationService,
+                                    TeamValidationService teamValidationService) {
+        super(persistenceService);
+        this.quarterValidationService = quarterValidationService;
+        this.teamValidationService = teamValidationService;
+    }
+
+    public void validateOnGet(List<Long> teamIds, Long quarterId) {
+
+        teamIds.forEach(teamValidationService::validateOnGet);
+        quarterValidationService.validateOnGet(quarterId);
+    }
+
+    @Override
+    public void validateOnCreate(EvaluationView model) {
+        throw new UnsupportedOperationException("EvaluationView is for get Operations only.");
+    }
+
+    @Override
+    public void validateOnUpdate(EvaluationViewId evaluationViewId, EvaluationView model) {
+        throw new UnsupportedOperationException("EvaluationView is for get Operations only.");
+    }
+}

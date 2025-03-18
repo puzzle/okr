0249166308282b7fb5315efe5bf1fb0d4798f6e0
Yanick Minder <minder@puzzle.ch>
2025-03-18 10:58:10 +0100

    Add dto, mapper and controller for Evaluation #1464

---
+++ backend/src/main/java/ch/puzzle/okr/dto/EvaluationDto.java
@@ -0,0 +1,6 @@
+package ch.puzzle.okr.dto;
+
+public record EvaluationDto(int objectiveAmount, int completedObjectivesAmount, int successfullyCompletedObjectivesAmount,
+                            int keyResultAmount, int keyResultsOrdinalAmount, int keyResultsMetricAmount , int keyResultsInTargetOrStretchAmount,
+                            int keyResultsInFailAmount, int keyResultsInCommitAmount, int keyResultsInTargetAmount, int keyResultsInStretchAmount) {
+}

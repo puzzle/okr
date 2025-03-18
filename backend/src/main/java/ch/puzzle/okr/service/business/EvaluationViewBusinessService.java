44d99a67c1336166f552b7cbb96096513c6bc7c8
Yanick Minder <minder@puzzle.ch>
2025-03-18 10:38:28 +0100

    Add validationservice for EvaluationView #1467

--- backend/src/main/java/ch/puzzle/okr/service/business/EvaluationViewBusinessService.java
+++ backend/src/main/java/ch/puzzle/okr/service/business/EvaluationViewBusinessService.java
@@ -2,6 +2,7 @@ package ch.puzzle.okr.service.business;
 
 import ch.puzzle.okr.models.evaluation.EvaluationView;
 import ch.puzzle.okr.service.persistence.EvaluationViewPersistenceService;
+import ch.puzzle.okr.service.validation.EvaluationViewValidationService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,12 +10,16 @@ import java.util.List;
 @Service
 public class EvaluationViewBusinessService {
     private final EvaluationViewPersistenceService evaluationViewPersistenceService;
+    private final EvaluationViewValidationService evaluationViewValidationService;
 
-    public EvaluationViewBusinessService(EvaluationViewPersistenceService evaluationViewPersistenceService) {
+    public EvaluationViewBusinessService(EvaluationViewPersistenceService evaluationViewPersistenceService,
+                                         EvaluationViewValidationService evaluationViewValidationService) {
         this.evaluationViewPersistenceService = evaluationViewPersistenceService;
+        this.evaluationViewValidationService = evaluationViewValidationService;
     }
 
     public List<EvaluationView> findByTeamIdsAndQuarterId(List<Long> teamIds, Long quarterId) {
+        evaluationViewValidationService.validateOnGet(teamIds, quarterId);
         return evaluationViewPersistenceService.findByTeamIdsAndQuarterId(teamIds, quarterId);
     }
 }

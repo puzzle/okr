f361b13a4a8c8f0ef960304455bd672f84cf1f0b
Yanick Minder <minder@puzzle.ch>
2025-03-20 16:07:27 +0100

    Add base structure of statistics page #1478

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { RefreshDataService } from '../services/refresh-data.service';
 
 @Component({
   selector: 'app-statistics',
@@ -6,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss'
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+  overviewPadding = new Subject<number>();
 
+  constructor(private refreshDataService: RefreshDataService, private changeDetector: ChangeDetectorRef) {
+  }
+
+  ngOnInit(): void {
+    this.refreshDataService.okrBannerHeightSubject.subscribe((e) => {
+      this.overviewPadding.next(e);
+      this.changeDetector.detectChanges();
+    });
+  }
 }

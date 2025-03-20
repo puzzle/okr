2b60a2bc2a290ade5fbe55f2d7a9aaa04c4daefa
Yanick Minder <minder@puzzle.ch>
2025-03-20 13:34:55 +0100

    Update swagger documentation #1474

--- backend/src/main/java/ch/puzzle/okr/controller/EvaluationViewController.java
+++ backend/src/main/java/ch/puzzle/okr/controller/EvaluationViewController.java
@@ -4,6 +4,7 @@ import ch.puzzle.okr.dto.EvaluationDto;
 import ch.puzzle.okr.mapper.EvaluationViewMapper;
 import ch.puzzle.okr.service.business.EvaluationViewBusinessService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -29,15 +30,17 @@ public class EvaluationViewController {
         this.evaluationViewBusinessService = evaluationViewBusinessService;
     }
 
-    @Operation(summary = "Get statisics data for multiple teams in a specific quarter", description = "Get a List of teams with their objectives")
+    @Operation(summary = "Get statistics data for multiple teams in a specific quarter", description = "Get a List of teams with their objectives")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Returned the statistics data for the requested teams and quarter", content = {
                     @Content(mediaType = "application/json", schema = @Schema(implementation = EvaluationDto.class)) }),
             @ApiResponse(responseCode = "404", description = "The quarter or one of the teams were not found", content = @Content),
             @ApiResponse(responseCode = "401", description = "Not Authorized", content = @Content) })
     @GetMapping("")
-    public ResponseEntity<EvaluationDto> getOverview(@RequestParam(name = "team") List<Long> teamIds,
-                                                     @RequestParam(name = "quarter") Long quarterId) {
+    public ResponseEntity<EvaluationDto> getOverview(@RequestParam(name = "team")
+    @Parameter(description = "List of Team ids the statistics are requested for") List<Long> teamIds,
+                                                     @RequestParam(name = "quarter")
+                                                     @Parameter(description = "Quarter id the statistics are requested for ") Long quarterId) {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(evaluationViewMapper

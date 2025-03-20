e78fef19b1e66de8ff87535f27dc66ac6abde1fc
Yanick Minder <minder@puzzle.ch>
2025-03-20 14:21:25 +0100

    Add statistics module and component #1478

---
+++ frontend/src/app/statistics/statistics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: false,
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.scss'
+})
+export class StatisticsComponent {
+
+}

8468b48c3e111ba4a00943f98e4ed119e395da6b
Yanick Minder <minder@puzzle.ch>
2025-03-21 15:18:19 +0100

    Add tests for application-page #1478

--- frontend/src/app/shared/application-page/application-page.component.spec.ts
+++ frontend/src/app/shared/application-page/application-page.component.spec.ts
@@ -1,9 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplicationPageComponent } from './application-page.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { RefreshDataService } from '../../services/refresh-data.service';
+
+const refreshDataServiceMock = {
+  teamFilterReady: new Subject<any>(),
+  quarterFilterReady: new Subject<any>(),
+  reloadOverviewSubject: new Subject<any>(),
+  okrBannerHeightSubject: new BehaviorSubject(5)
+};
 
 describe('ApplicationPageComponent', () => {
   let component: ApplicationPageComponent;
@@ -11,20 +20,41 @@ describe('ApplicationPageComponent', () => {
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule],
       declarations: [ApplicationPageComponent],
-      providers: [provideRouter([]),
+      providers: [
+        provideRouter([]),
         provideHttpClient(),
-        provideHttpClientTesting()]
+        provideHttpClientTesting(),
+        {
+          provide: RefreshDataService,
+          useValue: refreshDataServiceMock
+        }
+      ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ApplicationPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    jest.spyOn(component.reloadPage, 'next');
   });
 
   it('should create', () => {
     expect(component)
       .toBeTruthy();
+  });
+
+  it('should emit page reloader if all filters are ready', () => {
+    refreshDataServiceMock.quarterFilterReady.next(true);
+    refreshDataServiceMock.teamFilterReady.next(true);
+    expect(component.reloadPage.next)
+      .toHaveBeenCalled();
+  });
+
+  it('should emit page reloader reloadOverviewSubject is called', () => {
+    refreshDataServiceMock.reloadOverviewSubject.next(true);
+    expect(component.reloadPage.next)
+      .toHaveBeenCalled();
   });
 });

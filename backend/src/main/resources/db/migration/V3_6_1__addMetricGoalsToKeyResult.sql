992d7693fd26acff2496bc7c910d3af89850d6f1
Livio Canobbio <canobbio@puzzle.ch>
2025-08-12 15:59:36 +0200

    test: Update integration and unit tests and a bit of formatting

--- backend/src/main/resources/db/migration/V3_6_1__addMetricGoalsToKeyResult.sql
+++ backend/src/main/resources/db/migration/V3_6_1__addMetricGoalsToKeyResult.sql
@@ -1,6 +1,6 @@
 ALTER TABLE key_result
     ADD COLUMN IF NOT EXISTS wording_commit_value DOUBLE PRECISION,
-    add COLUMN IF NOT EXISTS wording_target_value DOUBLE PRECISION;
+    ADD COLUMN IF NOT EXISTS wording_target_value DOUBLE PRECISION;
 
 UPDATE key_result
 SET wording_commit_value = ROUND(((COALESCE(stretch_goal, 0) - COALESCE(baseline, 0)) * 0.3 + COALESCE(baseline, 0))::numeric, 2),

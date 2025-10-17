d8a62a63e7af9edfeb19d74d99fe6d88f4792e7b
Miguel Lehmann <lehmann@puzzle.ch>
2025-10-17 10:35:08 +0200

    test: Fix frontend and backend unit tests #1675

--- frontend/src/app/statistics/statistics-information/statistics-information.component.ts
+++ frontend/src/app/statistics/statistics-information/statistics-information.component.ts
@@ -20,6 +20,7 @@ export class StatisticsInformationComponent {
   @Input() maxHeightInPx = 50;
 
   get chartHeight(): string {
+    // Chart value is already a percentage. We can expect it to never be higher than 1.
     const heightInPx = this.chartValue * this.maxHeightInPx;
     return heightInPx + 'px';
   }

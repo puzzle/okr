131b4df4d367ed7cf4ea7c77d89ada7274ea4275
nevio18324 <141240169+nevio18324@users.noreply.github.com>
2025-10-28 10:04:52 +0100

    fix: recreate correct view in new migration #1680
    * fix: keyresult type check failing because of wrong case  #1680
    
    * fix: recreate correct view in new migration #1680

---
+++ backend/src/main/resources/db/migration/V3_6_5__fix_case_in_evaluation_view.sql
@@ -0,0 +1,32 @@
+DROP VIEW IF EXISTS EVALUATION_VIEW;
+
+CREATE VIEW EVALUATION_VIEW AS
+SELECT
+    row_number() OVER () AS row_id,
+    o.id              AS objective_id,
+    o.team_id,
+    o.quarter_id,
+    o.state           AS objective_state,
+    kr.id             AS key_result_id,
+    kr.key_result_type,
+    kr.baseline,
+    kr.commit_value,
+    kr.target_value,
+    kr.stretch_goal,
+    ci.value_metric,
+    CASE
+        WHEN UPPER(kr.key_result_type) = 'ORDINAL' THEN NULLIF(ci.zone, '')
+        ELSE NULL
+        END              AS zone,
+    ci.modified_on    AS latest_check_in_date
+FROM objective o
+         LEFT JOIN key_result kr
+                   ON kr.objective_id = o.id
+         LEFT JOIN LATERAL (
+    SELECT ci2.*
+    FROM check_in ci2
+    WHERE ci2.key_result_id = kr.id
+    ORDER BY ci2.modified_on DESC
+    LIMIT 1
+    ) ci ON TRUE
+WHERE o.state <> 'DRAFT';

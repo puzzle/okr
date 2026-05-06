2dce1fc259a3b8c03371ba7b68bc73289c409c3c
Manuel <moeri@puzzle.ch>
2026-05-06 14:24:35 +0200

    style(backend): Format code

--- backend/src/test/java/ch/puzzle/okr/test/TestHelper.java
+++ backend/src/test/java/ch/puzzle/okr/test/TestHelper.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.springframework.security.oauth2.jwt.Jwt;
-import tools.jackson.core.JsonParser;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 

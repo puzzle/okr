358b596f7dc2641e7fdcbf0db6b941e736ff2593
Manuel <moeri@puzzle.ch>
2026-06-24 11:37:34 +0200

    refactor(router): Change statistics component to filtered teams as well  #1736

--- frontend/src/app/team-management/member-list/member-list-table/member-list-table.component.spec.ts
+++ frontend/src/app/team-management/member-list/member-list-table/member-list-table.component.spec.ts
@@ -4,7 +4,7 @@ import { team1, testUser } from '../../../shared/test-data';
 import { of } from 'rxjs';
 import { UserTableEntry } from '../../../shared/types/model/user-table-entry';
 import { UserService } from '../../../services/user.service';
-import { TeamStateService } from '../../../services/team.state.service'; // Updated Import
+import { TeamStateService } from '../../../services/team.state.service';
 import { MatTableModule } from '@angular/material/table';
 import { DialogService } from '../../../services/dialog.service';
 

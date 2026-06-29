b1e19fa8f277a81a9e29198ef3182bdc84299860
Manuel <moeri@puzzle.ch>
2026-06-29 09:34:49 +0200

    fix(team-filter): Archived teams are correctly filtered out by date  #1736

--- frontend/src/app/resolvers/team-filter.resolver.ts
+++ frontend/src/app/resolvers/team-filter.resolver.ts
@@ -4,7 +4,7 @@ import { TeamStateService } from '../services/team.state.service';
 
 export const teamFilterResolver: ResolveFn<void> = (route) => {
   const teamStateService = inject(TeamStateService);
-  const quarterIdParam = route.queryParams['quarterId'];
+  const quarterIdParam = route.queryParams['quarter'];
 
   const filters = quarterIdParam ? { quarterId: parseInt(quarterIdParam, 10) } : {};
   teamStateService.loadTeams(filters);

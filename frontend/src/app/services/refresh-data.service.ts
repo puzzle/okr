d8d70b012e4b5c5bce1b04b21a8827bd5d8825ba
schiltpzzle <schilt@puzzle.ch>
2026-08-13 16:21:41 +0200

    fix: remove statistics reload after check-in method #1736

--- frontend/src/app/services/refresh-data.service.ts
+++ frontend/src/app/services/refresh-data.service.ts
@@ -2,15 +2,12 @@ import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { DEFAULT_HEADER_HEIGHT_PX } from '../shared/constant-library';
 import { OverviewService } from './overview.service';
-import { StatisticsService } from './statistics.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RefreshDataService {
   overviewService = inject(OverviewService);
-
-  statisticsService = inject(StatisticsService);
 
   public reloadOverviewSubject = new Subject<void>();
 
@@ -21,6 +18,5 @@ export class RefreshDataService {
   markDataRefresh() {
     this.reloadOverviewSubject.next();
     this.overviewService.reload();
-    this.statisticsService.reload();
   }
 }
